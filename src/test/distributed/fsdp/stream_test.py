4078dfd3477a15c0a2853044492128864a9413f2
epwalsh <petew@allenai.org>
2024-03-27 13:57:50 -0700

    add to stream test

--- src/test/distributed/fsdp/stream_test.py
+++ src/test/distributed/fsdp/stream_test.py
@@ -7,10 +7,24 @@ from olmo_core.distributed.fsdp.stream import CudaStream, Stream
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="Requires a GPU")
 @pytest.mark.gpu
 def test_cuda_stream():
-    default_stream = Stream.default(torch.device("cuda"))
+    device = torch.device("cuda")
+
+    default_stream = Stream.default(device)
     assert isinstance(default_stream, CudaStream)
     assert isinstance(default_stream.base_stream, torch.cuda.Stream)
 
-    current_stream = Stream.current(torch.device("cuda"))
+    current_stream = Stream.current(device)
     assert isinstance(current_stream, CudaStream)
     assert isinstance(current_stream.base_stream, torch.cuda.Stream)
+
+    other_stream = Stream.new(device)
+    assert isinstance(other_stream, CudaStream)
+
+    x = torch.empty((100, 100), device=device).normal_(0.0, 1.0)
+    other_stream.wait_stream(default_stream)
+    with other_stream:
+        assert torch.cuda.current_stream(device) == other_stream.base_stream
+        y = torch.sum(x)
+
+    default_stream.wait_stream(other_stream)
+    del x
