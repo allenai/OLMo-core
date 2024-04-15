2b837fc9bb9550b60bf69b045a70ada16ab37966
epwalsh <petew@allenai.org>
2024-04-15 15:40:18 -0700

    optimize how we flatten params

--- src/olmo_core/distributed/fsdp/flat_param_handle.py
+++ src/olmo_core/distributed/fsdp/flat_param_handle.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
 
+import logging
+import math
 from dataclasses import dataclass, field
-from typing import Dict, Iterable, List, Optional, Tuple
+from typing import Iterable, List, Optional, Tuple
 
 import torch
 import torch.distributed as dist
+import torch.nn as nn
+import torch.nn.functional as F
 
 from olmo_core.distributed.tensors import (
     ShardedFlatParameter,
@@ -13,6 +17,8 @@ from olmo_core.distributed.tensors import (
 )
 from olmo_core.distributed.utils import get_rank, get_world_size
 from olmo_core.utils import get_default_device
+
+log = logging.getLogger(__name__)
 
 
 @dataclass
@@ -39,13 +45,7 @@ class FlatParamHandle:
 
     params_data: ShardedFlatTensor = field(default_factory=lambda: ShardedFlatTensor(torch.empty(0)))
     """
-    Consolidated data for all of the local sharded data of the parameters.
-    """
-
-    params_offsets_per_rank: List[Dict[int, Tuple[int, int]]] = field(default_factory=list)
-    """
-    For each parameter, provides a mapping of rank to the offsets into the rank's local `params_data`
-    for that parameter.
+    Consolidated data for all of the local sharded data of the parameters including padding.
     """
 
     process_group: Optional[dist.ProcessGroup] = None
@@ -53,77 +53,150 @@ class FlatParamHandle:
     device: Optional[torch.device] = None
 
     @classmethod
-    def collate_flat_params(
+    def shard_params(
         cls,
-        params: Iterable[ShardedFlatParameter],
+        params: Iterable[nn.Parameter],
         param_fqns: Iterable[str],
         process_group: Optional[dist.ProcessGroup] = None,
         device: Optional[torch.device] = None,
     ) -> FlatParamHandle:
         """
-        Collate the data from a group of sharded flat parameters into a single flat param handle.
+        Shard and flatten parameters and collect into a :class:`FlatParamHandle`.
         """
         device = device or get_default_device()
-        params = list(params)
         world_size = get_world_size(process_group)
         local_rank = get_rank(process_group)
+        params = list(params)
 
         if not params:
-            return cls(process_group=process_group)
+            params_data = ShardedFlatTensor(torch.empty(0, device=device))
+            params_data.mark_as_sharded(
+                ShardingSpec(unsharded_shape=(0,), unsharded_flattened_offsets=tuple([(0, 0)] * world_size))
+            )
+            return FlatParamHandle(process_group=process_group, device=device)
 
-        # Find max numel of all sharded flat params across the process group to determine padding.
-        # All ranks will have the same sized `params_data` at the end to avoid needed padding at runtime.
-        numel_total_per_rank: List[int] = [0] * world_size
+        total_numel = sum(p.numel() for p in params)
+        padded_sharded_numel = math.ceil(total_numel / world_size)
+        padded_unsharded_numel = padded_sharded_numel * world_size
+
+        # The idea is to flatten and concatenate all (unsharded) original parameters together to form
+        # the unsharded, unpadded flat parameter.
+        #
+        # For example, suppose we have 3 parameters that, when flattened, look like this:
+        #
+        #  x = (x1, x2, x3, x4)
+        #  y = (y1, y2, y3)
+        #  z = (z1, z2, z3, z4)
+        #
+        # And suppose we have a world size of 4. Then the padded unsharded flat parameter would look
+        # like this:
+        #
+        #   |x1 x2 x3|x4 y1 y2|y3 z1 z2|z3 z4 0 |
+        #   | rank 0 | rank 1 | rank 2 | rank 3 |
+        #    1  2  3  4  5  6  7  8  9  10 11 12
+        #
+        # Now first we need to initialize a flat parameter to take the place of each regular parameter.
+        flat_params: List[ShardedFlatParameter] = []
+        numel_running_total = 0
         for param in params:
-            if not param.is_sharded:
-                raise ValueError("All sharded flat params should be sharded at this point!")
-            if param.dtype != torch.float32:
-                raise NotImplementedError("Only float32 params are supported at this time")
-            for rank, n in enumerate(param.sharding_spec.sharded_numels):
-                numel_total_per_rank[rank] += n
-        max_numel = max(numel_total_per_rank)
+            flat_param_global_offsets = (numel_running_total, numel_running_total + param.numel())
 
-        # Initialize local data for all params.
-        params_data = ShardedFlatTensor(torch.empty(max_numel, device=device))
+            # First we need to determine which ranks will have a slice of the data.
+            unsharded_flattened_offsets: List[Tuple[int, int]] = []
+            for rank in range(world_size):
+                rank_global_start = rank * padded_sharded_numel
+                rank_global_end = rank_global_start + padded_sharded_numel
+                if (rank_global_end <= flat_param_global_offsets[0]) or (
+                    flat_param_global_offsets[1] <= rank_global_start
+                ):
+                    # No overlap with this rank.
+                    unsharded_flattened_offsets.append((0, 0))
+                elif (
+                    rank_global_start <= flat_param_global_offsets[0]
+                    and flat_param_global_offsets[1] <= rank_global_end
+                ):
+                    # Param is completely contained by this rank.
+                    unsharded_flattened_offsets.append((0, param.numel()))
+                elif (
+                    rank_global_start <= flat_param_global_offsets[0]
+                    and rank_global_end < flat_param_global_offsets[1]
+                ):
+                    # Param starts in this rank and ends in a subsequent rank.
+                    unsharded_flattened_offsets.append((0, rank_global_end - flat_param_global_offsets[0]))
+                elif (
+                    flat_param_global_offsets[0] < rank_global_start
+                    and flat_param_global_offsets[1] <= rank_global_end
+                ):
+                    # Param starts in a previous rank and ends in this one.
+                    unsharded_flattened_offsets.append(
+                        (rank_global_start - flat_param_global_offsets[0], param.numel())
+                    )
+                elif (
+                    flat_param_global_offsets[0] < rank_global_start
+                    and rank_global_end < flat_param_global_offsets[1]
+                ):
+                    # Param spans this rank and overflows into other ranks.
+                    unsharded_flattened_offsets.append(
+                        (
+                            rank_global_start - flat_param_global_offsets[0],
+                            rank_global_end - flat_param_global_offsets[0],
+                        )
+                    )
+
+            sharding_spec = ShardingSpec(
+                unsharded_shape=tuple(param.shape), unsharded_flattened_offsets=tuple(unsharded_flattened_offsets)
+            )
+            flat_param: ShardedFlatParameter
+            if (local_rank_numel := sharding_spec.sharded_numels[local_rank]) > 0:
+                if param.device == torch.device("meta"):
+                    flat_param = ShardedFlatParameter(torch.empty(local_rank_numel, device=device))
+                else:
+                    flat_param = ShardedFlatParameter(
+                        param.data.flatten()[
+                            unsharded_flattened_offsets[local_rank][0] : unsharded_flattened_offsets[local_rank][1]
+                        ].to(device)
+                    )
+            else:
+                flat_param = ShardedFlatParameter(torch.empty(0, device=device))
+            flat_param.mark_as_sharded(sharding_spec, process_group=process_group)
+
+            flat_params.append(flat_param)
+
+            numel_running_total += param.numel()
+            param.data = torch.empty(0, device=param.device)
+
+        # Now that we have all of the flat parameters we need to collate all of their data into a single
+        # sharded flat tensor, then set the data for each flat parameter as a view into that flat tensor.
+        local_flat_sharded_data = torch.cat([flat_param.data for flat_param in flat_params])
+        params_data = ShardedFlatTensor(
+            F.pad(local_flat_sharded_data, (0, padded_sharded_numel - local_flat_sharded_data.numel()))
+        )
+        del local_flat_sharded_data
         params_data.mark_as_sharded(
             ShardingSpec(
-                unsharded_shape=(world_size, max_numel),
+                unsharded_shape=(padded_unsharded_numel,),
                 unsharded_flattened_offsets=tuple(
                     [
                         (start_idx, end_idx)
                         for start_idx, end_idx in zip(
-                            range(0, max_numel * world_size, max_numel),
-                            range(max_numel, max_numel * world_size + 1, max_numel),
+                            range(0, padded_unsharded_numel, padded_sharded_numel),
+                            range(padded_sharded_numel, padded_unsharded_numel + 1, padded_sharded_numel),
                         )
                     ]
                 ),
             ),
             process_group=process_group,
         )
-
-        # Consolidate the sharded data from each param into `params_data` and collect offsets.
-        params_offsets_per_rank: List[Dict[int, Tuple[int, int]]] = []
-        offset_start_per_rank = {rank: 0 for rank in range(world_size)}
-        for param in params:
-            param_offsets: Dict[int, Tuple[int, int]] = {}
-            for rank in range(world_size):
-                offset_start = offset_start_per_rank[rank]
-                offset_end = offset_start + param.sharding_spec.sharded_numels[rank]
-                param_offsets[rank] = (offset_start, offset_end)
-                offset_start_per_rank[rank] = offset_end
-            params_offsets_per_rank.append(param_offsets)
-
-            # Set data for param as a view into `params_data`.
-            offset_start, offset_end = param_offsets[local_rank]
-            params_data.data[offset_start:offset_end].copy_(param.data)
-            param.data = params_data.data[offset_start:offset_end]
+        offset = 0
+        for flat_param in flat_params:
+            flat_param.data = params_data[offset : offset + flat_param.numel()]
+            offset += flat_param.numel()
 
         return cls(
-            params=params,
+            params=flat_params,
             param_fqns=list(param_fqns),
-            grads_cache=[None] * len(params),
+            grads_cache=[None] * len(flat_params),
             params_data=params_data,
-            params_offsets_per_rank=params_offsets_per_rank,
             process_group=process_group,
             device=device,
         )
@@ -136,7 +209,6 @@ class FlatParamHandle:
             return
 
         local_rank = get_rank(self.process_group)
-        world_size = get_world_size(self.process_group)
 
         # Gather full, padded, unsharded data for all params.
         all_params_unsharded_data: torch.Tensor
@@ -153,29 +225,21 @@ class FlatParamHandle:
                 self.params_data.data.to(dtype or self.params_data.dtype),
                 group=self.process_group,
             )
+        self.params_data.unshard_(unsharded_data=all_params_unsharded_data, dtype=dtype, rank0_only=rank0_only)
 
         # Set the data for each param as a view into `all_params_unsharded_data`.
-        for i, (param, param_offsets) in enumerate(zip(self.params, self.params_offsets_per_rank)):
+        numel_running_total = 0
+        for i, param in enumerate(self.params):
             if rank0_only and local_rank != 0:
                 param.unshard_(
                     unsharded_data=torch.empty_like(all_params_unsharded_data), dtype=dtype, rank0_only=rank0_only
                 )
             else:
-                unsharded_data = torch.empty(
-                    param.sharding_spec.unsharded_flattened_shape,
-                    dtype=all_params_unsharded_data.dtype,
-                    device=self.device,
-                )
-                for rank in range(world_size):
-                    rank_local_data = all_params_unsharded_data[rank][
-                        param_offsets[rank][0] : param_offsets[rank][1]
-                    ]
-                    unsharded_data[
-                        param.sharding_spec.unsharded_flattened_offsets[rank][
-                            0
-                        ] : param.sharding_spec.unsharded_flattened_offsets[rank][1]
-                    ] = rank_local_data
-                param.unshard_(unsharded_data=unsharded_data, dtype=dtype)
+                unsharded_data = all_params_unsharded_data[
+                    numel_running_total : numel_running_total + param.unsharded_numel
+                ]
+                param.unshard_(unsharded_data, dtype=dtype, rank0_only=rank0_only)
+            numel_running_total += param.unsharded_numel
 
             if cache_grads and param.grad is not None:
                 # We should only be caching these between the pre-backward and post-backward
@@ -191,13 +255,17 @@ class FlatParamHandle:
         """
         Reshard the handle's managed flat parameters in-place.
         """
-        local_rank = get_rank(self.process_group)
-        for param, param_offsets in zip(self.params, self.params_offsets_per_rank):
-            param.reshard_(writeback=writeback)
+        if not self.params:
+            return
+
+        self.params_data.reshard_(writeback=writeback)
+        offset = 0
+        for flat_param in self.params:
+            flat_param.reshard_(writeback=False)
             if writeback:
-                offset_start, offset_end = param_offsets[local_rank]
-                self.params_data.data[offset_start:offset_end].copy_(param.data)
-                param.data = self.params_data.data[offset_start:offset_end]
+                # Reset the view into the new `params_data`.
+                flat_param.data = self.params_data[offset : offset + flat_param.numel()]
+            offset += flat_param.numel()
 
     def reduce_scatter_grads(
         self, grad_dtype: Optional[torch.dtype] = None, grad_reduce_dtype: Optional[torch.dtype] = None
