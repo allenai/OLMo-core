f3164076e6307b1a1e7474b9ae2e040c7c86a819
epwalsh <petew@allenai.org>
2024-05-07 10:07:31 -0700

    fix debug logging

--- src/olmo_core/distributed/checkpoint.py
+++ src/olmo_core/distributed/checkpoint.py
@@ -354,7 +354,7 @@ class Checkpointer:
 
         # Load each tensor from the slices in each file.
         for key in state_dict.keys():
-            log.debug("Loading tensor '%s' from state dict...")
+            log.debug("Loading tensor '%s' from state dict...", key)
             tensor_storage_metadata = metadata.tensors[key]
             tensor = state_dict[key]
             flat_view = self._get_flat_view(tensor)
