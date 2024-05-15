084cbbbde0766fed60891dc4a784b4e38f13e288
epwalsh <petew@allenai.org>
2024-05-15 09:25:21 -0700

    clean up

--- src/olmo_core/distributed/tensors/sharded_flat_parameter.py
+++ src/olmo_core/distributed/tensors/sharded_flat_parameter.py
@@ -15,6 +15,7 @@ class ShardedFlatParameter(ShardedFlatTensor, nn.Parameter):
     A :class:`~torch.nn.parameter.Parameter` version of :class:`ShardedFlatTensor`.
     """
 
+    @staticmethod
     def __new__(cls, data: Optional[torch.Tensor] = None, requires_grad: bool = True) -> ShardedFlatParameter:
         if data is not None and data.ndim != 1:
             raise ValueError(f"{cls.__name__} requires flat data! Got {data.shape}")
