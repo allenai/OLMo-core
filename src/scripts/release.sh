badf93d50110fd91187cd58f5a341b29d6046a63
epwalsh <petew@allenai.org>
2024-06-11 13:43:11 -0700

    add

---
+++ src/scripts/release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+TAG=$(python -c 'from olmo_core.version import VERSION; print("v" + VERSION)')
+
+git pull
+python src/scripts/prepare_changelog.py
+
+read -p "Creating new release for $TAG. Do you want to continue? [Y/n] " prompt
+
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
+    git add -A
+    git commit -m "(chore) prepare for release $TAG" || true && git push
+    echo "Creating new git tag $TAG"
+    git tag "$TAG" -m "$TAG"
+    git push --tags
+else
+    echo "Canceled"
+    git checkout CHANGELOG.md
+    exit 1
+fi
