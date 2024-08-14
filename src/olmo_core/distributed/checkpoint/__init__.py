dd561a0fd1fc99343c3f424d1dc931aa87b8d513
epwalsh <petew@allenai.org>
2024-08-14 13:26:44 -0700

    clean up

--- src/olmo_core/distributed/checkpoint/__init__.py
+++ src/olmo_core/distributed/checkpoint/__init__.py
@@ -31,15 +31,8 @@ from typing import Any, Dict, Optional
 import torch
 import torch.distributed as dist
 import torch.distributed.checkpoint as dist_cp
+import torch.distributed.checkpoint.state_dict as dist_cp_sd
 import torch.nn as nn
-from torch.distributed.checkpoint.state_dict import (
-    StateDictOptions,
-    get_model_state_dict,
-    get_state_dict,
-    set_model_state_dict,
-    set_state_dict,
-)
-from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 
 from olmo_core.distributed.utils import barrier, get_fs_local_rank
 from olmo_core.io import (
@@ -69,6 +62,7 @@ def save_model_and_optim_state(
     dir: PathOrStr,
     model: nn.Module,
     optim: Optional[torch.optim.Optimizer] = None,
+    *,
     process_group: Optional[dist.ProcessGroup] = None,
     save_overwrite: bool = False,
 ) -> None:
@@ -96,14 +90,12 @@ def save_model_and_optim_state(
     :raises FileExistsError: If the checkpoint dir exists and is non-empty unless ``save_overwrite=True``.
     """
     dir = _prepare_env_for_save(dir, process_group=process_group, save_overwrite=save_overwrite)
-
-    with _checkpoint_context(model):
-        state_dict = _prepare_state_dict(model, optim=optim, process_group=process_group)
-        dist_cp.state_dict_saver.save(
-            state_dict,
-            storage_writer=RemoteFileSystemWriter(dir),
-            process_group=process_group,
-        )
+    state_dict = _prepare_state_dict(model, optim=optim, process_group=process_group)
+    dist_cp.state_dict_saver.save(
+        state_dict,
+        storage_writer=RemoteFileSystemWriter(dir),
+        process_group=process_group,
+    )
 
 
 @torch.no_grad()
@@ -111,6 +103,7 @@ def async_save_model_and_optim_state(
     dir: PathOrStr,
     model: nn.Module,
     optim: Optional[torch.optim.Optimizer] = None,
+    *,
     process_group: Optional[dist.ProcessGroup] = None,
     save_overwrite: bool = False,
 ) -> Future[None]:
@@ -120,14 +113,12 @@ def async_save_model_and_optim_state(
     This code first de-stages the state dict on the CPU, then writes it in a separate thread.
     """
     dir = _prepare_env_for_save(dir, process_group=process_group, save_overwrite=save_overwrite)
-
-    with _checkpoint_context(model):
-        state_dict = _prepare_state_dict(model, optim=optim, process_group=process_group)
-        return dist_cp.state_dict_saver.async_save(
-            state_dict,
-            storage_writer=RemoteFileSystemWriter(dir),
-            process_group=process_group,
-        )
+    state_dict = _prepare_state_dict(model, optim=optim, process_group=process_group)
+    return dist_cp.state_dict_saver.async_save(
+        state_dict,
+        storage_writer=RemoteFileSystemWriter(dir),
+        process_group=process_group,
+    )
 
 
 @torch.no_grad()
@@ -135,6 +126,7 @@ def load_model_and_optim_state(
     dir: PathOrStr,
     model: nn.Module,
     optim: Optional[torch.optim.Optimizer] = None,
+    *,
     process_group: Optional[dist.ProcessGroup] = None,
 ):
     """
@@ -157,34 +149,24 @@ def load_model_and_optim_state(
     :param process_group: The process group to use for distributed collectives.
     """
     dir = normalize_path(dir)
+    state_dict = _prepare_state_dict(model, optim, process_group=process_group)
+    dist_cp.load(
+        state_dict,
+        checkpoint_id=dir,
+        storage_reader=RemoteFileSystemReader(dir),
+        process_group=process_group,
+    )
 
-    with _checkpoint_context(model):
-        state_dict = _prepare_state_dict(model, optim, process_group=process_group)
+    dist_cp_sd.set_model_state_dict(
+        model, state_dict["model"], options=dist_cp_sd.StateDictOptions(strict=True)
+    )
+    gc_cuda()
 
-        dist_cp.load(
-            state_dict,
-            checkpoint_id=dir,
-            storage_reader=RemoteFileSystemReader(dir),
-            process_group=process_group,
+    if optim is not None:
+        dist_cp_sd.set_optimizer_state_dict(
+            model, optim, state_dict["optim"], options=dist_cp_sd.StateDictOptions(strict=True)
         )
-
-        if optim is not None:
-            set_state_dict(
-                model,
-                optim,
-                model_state_dict=state_dict["model"],
-                optim_state_dict=state_dict["optim"],
-                options=StateDictOptions(strict=True),
-            )
-        else:
-            set_model_state_dict(model, state_dict["model"], options=StateDictOptions(strict=True))
-
-        #  # Load model state.
-        #  model_state_dict = _load_model_state(dir, model, process_group=process_group)
-
-        #  # Maybe load optim state.
-        #  if optim is not None:
-        #      _load_optim_state(dir, model, optim, model_state_dict, process_group=process_group)
+        gc_cuda()
 
 
 def _prepare_env_for_save(
@@ -218,131 +200,12 @@ def _prepare_state_dict(
     process_group: Optional[dist.ProcessGroup] = None,
 ) -> Dict[str, Any]:
     del process_group  # I feel like these torch functions should take a process group argument.
-    sd_options = StateDictOptions(full_state_dict=False, cpu_offload=True)
+    sd_options = dist_cp_sd.StateDictOptions(full_state_dict=False, cpu_offload=True)
 
+    state_dict: Dict[str, Any] = {
+        "model": dist_cp_sd.get_model_state_dict(model, options=sd_options)
+    }
     if optim is not None:
-        model_state, optim_state = get_state_dict(model, optim, options=sd_options)
-        return {"model": model_state, "optim": optim_state}
-    else:
-        return {"model": get_model_state_dict(model, options=sd_options)}
+        state_dict["optim"] = dist_cp_sd.get_optimizer_state_dict(model, optim, options=sd_options)
 
-    #  state_dict: Dict[str, Any]
-    #  if isinstance(model, FSDP):
-    #      state_dict = {"model": model.state_dict()}
-    #  else:
-    #      state_dict = {"model": model}
-
-    #  if optim is not None:
-    #      state_dict["optim"] = _prepare_optimizer_state_to_save(
-    #          model, optim, process_group=process_group
-    #      )
-
-    #  return state_dict
-
-
-#  def _prepare_optimizer_state_to_save(
-#      model: nn.Module,
-#      optim: torch.optim.Optimizer,
-#      process_group: Optional[dist.ProcessGroup] = None,
-#  ) -> Any:
-#      _init_optimizer_state(optim)
-#      if isinstance(model, FSDP):
-#          return FSDP.optim_state_dict(model, optim, group=process_group)
-#      else:
-#          return optim
-
-
-@torch.no_grad()
-def _init_optimizer_state(optim: torch.optim.Optimizer):
-    """
-    Ensure optimizer state is initialized for checkpointing.
-    """
-    if optim.state:
-        return
-    for group in optim.param_groups:
-        for p in group["params"]:
-            # Some parameters may be empty for sharded models, in which case the state does not need
-            # to be initialized.
-            if p.numel() > 0:
-                p.grad = torch.zeros_like(p, memory_format=torch.preserve_format)
-    optim.step()
-    optim.zero_grad()
-
-
-def _checkpoint_context(model: nn.Module):
-    if isinstance(model, FSDP):
-        from torch.distributed.fsdp.api import StateDictType
-
-        return FSDP.state_dict_type(model, StateDictType.SHARDED_STATE_DICT)
-    else:
-        from contextlib import nullcontext
-
-        return nullcontext()
-
-
-def _prepare_model_state_dict_to_load(model: nn.Module) -> Dict[str, Any]:
-    if isinstance(model, FSDP):
-        return {"model": model.state_dict()}
-    else:
-        return {"model": model}
-
-
-def _load_model_state(
-    dir: str, model: nn.Module, process_group: Optional[dist.ProcessGroup] = None
-) -> Dict[str, Any]:
-    model_state = _prepare_model_state_dict_to_load(model)
-    dist_cp.load(
-        model_state,
-        checkpoint_id=dir,
-        storage_reader=RemoteFileSystemReader(dir),
-        process_group=process_group,
-    )
-    if isinstance(model_state["model"], dict):
-        model.load_state_dict(model_state["model"])
-    return model_state
-
-
-def _load_optim_state(
-    dir: str,
-    model: nn.Module,
-    optim: torch.optim.Optimizer,
-    model_state_dict: Dict[str, Any],
-    process_group: Optional[dist.ProcessGroup] = None,
-):
-    _init_optimizer_state(optim)
-    if isinstance(model, FSDP):
-        from torch.distributed.checkpoint.optimizer import (
-            load_sharded_optimizer_state_dict,
-        )
-
-        optim_state = load_sharded_optimizer_state_dict(
-            model_state_dict=model_state_dict["model"],
-            optimizer_key="optim",
-            storage_reader=RemoteFileSystemReader(dir),
-        )
-        del model_state_dict
-        # Make sure tensors are on CPU! PyTorch puts them on GPU even if we have `offload_to_cpu=True`.
-        _move_optim_state_to_cpu(optim_state["optim"])
-
-        flattened_osd = FSDP.optim_state_dict_to_load(model, optim, optim_state["optim"])
-        del optim_state
-        # Put flattened optim state on CPU since `Optimizer.load_state_dict()` will create a deepcopy
-        # of the whole state dict which takes up unnecessary GPU memory.
-        _move_optim_state_to_cpu(flattened_osd)
-
-        optim.load_state_dict(flattened_osd)
-    else:
-        del model_state_dict
-        dist_cp.load(
-            {"optim": optim},
-            checkpoint_id=dir,
-            storage_reader=RemoteFileSystemReader(dir),
-            process_group=process_group,
-        )
-
-
-def _move_optim_state_to_cpu(optim_state: Dict[str, Any]):
-    for state in optim_state["state"].values():
-        for k in state.keys():
-            state[k] = state[k].cpu()
-    gc_cuda()
+    return state_dict
