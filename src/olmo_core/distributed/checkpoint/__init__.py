6ca2f0a7fe5f8581090245e3664316cdf7429f44
epwalsh <petew@allenai.org>
2024-08-14 10:52:07 -0700

    fix

--- src/olmo_core/distributed/checkpoint/__init__.py
+++ src/olmo_core/distributed/checkpoint/__init__.py
@@ -276,7 +276,7 @@ def _load_optim_state(
         )
 
         optim_state = load_sharded_optimizer_state_dict(
-            model_state_dict=model_state_dict,
+            model_state_dict=model_state_dict["model"],
             optimizer_key="optim",
             storage_reader=RemoteFileSystemReader(dir),
         )
