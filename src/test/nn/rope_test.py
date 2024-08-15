68dd0192b3cd07d438771ee8eb5b65d5cc01a25c
epwalsh <petew@allenai.org>
2024-08-15 08:22:00 -0700

    Add inference tests

--- src/test/nn/rope_test.py
+++ src/test/nn/rope_test.py
@@ -26,19 +26,83 @@ def test_rope_head_first_vs_seq_first(device):
 
 
 @pytest.mark.parametrize("device", DEVICES)
+@pytest.mark.parametrize(
+    "head_first",
+    [
+        pytest.param(True, id="head_first"),
+        pytest.param(False, id="seq_first"),
+    ],
+)
+def test_rope_with_past_key_values(device, head_first):
+    B, T, d_model, n_heads = 2, 12, 16, 4
+    rope = RotaryEmbedding(head_shape=d_model // n_heads)
+
+    with torch.no_grad():
+        q = torch.rand(B, n_heads, T, d_model // n_heads, device=device)
+        k = torch.rand(B, n_heads, T, d_model // n_heads, device=device)
+        q_last = q[:, :, -1:, :]
+
+        if not head_first:
+            q, k = q.transpose(1, 2), k.transpose(1, 2)
+            q_last = q_last.transpose(1, 2)
+
+        q_full, k_full = rope(q, k, head_first=head_first)
+        q_part, k_part = rope(q_last, k, head_first=head_first)
+
+        if not head_first:
+            q_full, k_full = q_full.transpose(1, 2), k_full.transpose(1, 2)
+            q_part, k_part = q_part.transpose(1, 2), k_part.transpose(1, 2)
+
+        torch.testing.assert_close(q_full[:, :, -1:, :], q_part)
+        torch.testing.assert_close(k_full, k_part)
+
+
+@pytest.mark.parametrize("device", DEVICES)
 def test_complex_rope_head_first_vs_seq_first(device):
     B, T, d_model, n_heads = 2, 12, 16, 4
-    c_rope = ComplexRotaryEmbedding(head_shape=d_model // n_heads)
+    rope = ComplexRotaryEmbedding(head_shape=d_model // n_heads)
 
     with torch.no_grad():
         q = torch.rand(B, n_heads, T, d_model // n_heads, device=device)
         k = torch.rand(B, n_heads, T, d_model // n_heads, device=device)
 
-        q1, k1 = c_rope(q, k, head_first=True)
+        q1, k1 = rope(q, k, head_first=True)
 
-        q2, k2 = c_rope(q.transpose(1, 2), k.transpose(1, 2), head_first=False)
+        q2, k2 = rope(q.transpose(1, 2), k.transpose(1, 2), head_first=False)
         q2 = q2.transpose(1, 2)
         k2 = k2.transpose(1, 2)
 
         torch.testing.assert_close(q1, q2)
         torch.testing.assert_close(k1, k2)
+
+
+@pytest.mark.parametrize("device", DEVICES)
+@pytest.mark.parametrize(
+    "head_first",
+    [
+        pytest.param(True, id="head_first"),
+        pytest.param(False, id="seq_first"),
+    ],
+)
+def test_complex_rope_with_past_key_values(device, head_first):
+    B, T, d_model, n_heads = 2, 12, 16, 4
+    rope = ComplexRotaryEmbedding(head_shape=d_model // n_heads)
+
+    with torch.no_grad():
+        q = torch.rand(B, n_heads, T, d_model // n_heads, device=device)
+        k = torch.rand(B, n_heads, T, d_model // n_heads, device=device)
+        q_last = q[:, :, -1:, :]
+
+        if not head_first:
+            q, k = q.transpose(1, 2), k.transpose(1, 2)
+            q_last = q_last.transpose(1, 2)
+
+        q_full, k_full = rope(q, k, head_first=head_first)
+        q_part, k_part = rope(q_last, k, head_first=head_first)
+
+        if not head_first:
+            q_full, k_full = q_full.transpose(1, 2), k_full.transpose(1, 2)
+            q_part, k_part = q_part.transpose(1, 2), k_part.transpose(1, 2)
+
+        torch.testing.assert_close(q_full[:, :, -1:, :], q_part)
+        torch.testing.assert_close(k_full, k_part)
