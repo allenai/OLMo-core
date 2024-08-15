9264feb5af21b87422279fad6dcea0c6db9ea220
epwalsh <petew@allenai.org>
2024-08-15 10:44:11 -0700

    update values

--- src/test/nn/functional/cross_entropy_loss_test.py
+++ src/test/nn/functional/cross_entropy_loss_test.py
@@ -9,7 +9,7 @@ from ...utils import DEVICES, requires_flash_attn, requires_gpu
 @pytest.mark.parametrize("device", DEVICES)
 @pytest.mark.parametrize("reduction", ["sum", "mean"])
 def test_cross_entropy_loss(device, reduction):
-    vocab_size = 4096
+    vocab_size = 50257
     N = 32
 
     logits = torch.randn(N, vocab_size, device=device)
@@ -37,14 +37,14 @@ def test_cross_entropy_loss(device, reduction):
 @pytest.mark.parametrize("reduction", ["sum", "mean"])
 def test_fused_cross_entropy_loss(reduction):
     device = torch.device("cuda")
-    vocab_size = 4096
+    vocab_size = 50257
     N = 32
 
     logits = torch.randn(N, vocab_size, device=device)
     labels = torch.randint(0, vocab_size, (N,), device=device)
 
     # Make sure the outputs match those from the non-fused cross entropy loss function.
-    ce_loss, z_loss = cross_entropy_loss(logits, labels, reduction=reduction, compute_z_loss=True)
+    ce_loss, z_loss = cross_entropy_loss(logits.clone(), labels.clone(), reduction=reduction, compute_z_loss=True)
     assert z_loss is not None
     ce_loss_fused, z_loss_fused = fused_cross_entropy_loss(
         logits, labels, reduction=reduction, compute_z_loss=True
