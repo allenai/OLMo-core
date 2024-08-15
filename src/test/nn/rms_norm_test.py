6d6de54bfbb4ae59cebd6eeac0d9e29f8f443c8e
epwalsh <petew@allenai.org>
2024-08-15 13:02:28 -0700

    add test without bias

--- src/test/nn/rms_norm_test.py
+++ src/test/nn/rms_norm_test.py
@@ -1,3 +1,4 @@
+import pytest
 import torch
 
 from olmo_core.nn.rms_norm import FusedRMSNorm, RMSNorm
@@ -7,10 +8,11 @@ from ..utils import requires_flash_attn, requires_gpu
 
 @requires_gpu
 @requires_flash_attn
-def test_fused_rms_norm():
+@pytest.mark.parametrize("bias", [pytest.param(True, id="bias"), pytest.param(False, id="no-bias")])
+def test_fused_rms_norm(bias):
     dim = 64
-    norm = RMSNorm(size=dim, init_device="cuda")
-    norm_fused = FusedRMSNorm(size=dim, init_device="cuda")
+    norm = RMSNorm(size=dim, init_device="cuda", bias=bias)
+    norm_fused = FusedRMSNorm(size=dim, init_device="cuda", bias=bias)
 
     x = torch.randn(4, dim, device="cuda")
     y1 = norm(x)
