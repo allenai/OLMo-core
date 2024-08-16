d68400823120c043c01cb19660518db4331c7671
epwalsh <petew@allenai.org>
2024-08-16 13:13:41 -0700

    add more tests

--- src/test/nn/attention_test.py
+++ src/test/nn/attention_test.py
@@ -1,11 +1,72 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 import pytest
 import torch
 
 from olmo_core.nn.attention import Attention, FusedAttention
+from olmo_core.nn.layer_norm import LayerNormConfig
+from olmo_core.nn.rope import RoPEConfig
 
-from ..utils import requires_flash_attn, requires_gpu
+from ..utils import DEVICES, GPU_MARKS, requires_flash_attn, requires_gpu
+
+
+@pytest.mark.parametrize("device", DEVICES)
+@pytest.mark.parametrize(
+    "dtype",
+    [
+        pytest.param(torch.bfloat16, id="bf16", marks=GPU_MARKS),
+        pytest.param(torch.float32, id="fp16"),
+    ],
+)
+@pytest.mark.parametrize(
+    "n_kv_heads",
+    [pytest.param(None, id="MHA"), pytest.param(1, id="MQA"), pytest.param(4, id="GQA")],
+)
+@pytest.mark.parametrize(
+    "use_flash", [pytest.param(True, id="flash"), pytest.param(False, id="torch-SDPA")]
+)
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        pytest.param({"clip_qkv": 8.0}, id="QKV-clip"),
+        pytest.param({"rope": RoPEConfig()}, id="rope"),
+        pytest.param({"rope": RoPEConfig(name="complex")}, id="complex-rope"),
+        pytest.param({"qk_norm": LayerNormConfig()}, id="qk-norm"),
+    ],
+)
+def test_attention(
+    dtype: torch.dtype,
+    device: torch.device,
+    n_kv_heads: Optional[int],
+    use_flash: bool,
+    kwargs: Dict[str, Any],
+):
+    torch.random.manual_seed(0)
+
+    d_model = 128
+    seq_len = 32
+
+    attention = Attention(
+        d_model=d_model,
+        n_heads=8,
+        n_kv_heads=n_kv_heads,
+        use_flash=use_flash,
+        init_device=device.type,
+        **kwargs,
+    )
+
+    x1 = torch.randn(1, seq_len, d_model, dtype=dtype, device=device)
+    x2 = torch.randn(1, seq_len, d_model, dtype=dtype, device=device)
+    x = torch.cat([x1, x2])
+
+    # Make sure batch outputs match individual outputs.
+    with torch.no_grad():
+        y1 = attention(x1)
+        y2 = attention(x2)
+        y = attention(x)
+
+    torch.testing.assert_close(y[0:1, :, :], y1)
+    torch.testing.assert_close(y[1:, :, :], y2)
 
 
 @requires_gpu
@@ -14,7 +75,7 @@ from ..utils import requires_flash_attn, requires_gpu
 @pytest.mark.parametrize(
     "use_flash", [pytest.param(True, id="flash"), pytest.param(False, id="torch-SDPA")]
 )
-def test_fused_attention_against_non_fused(dtype, use_flash):
+def test_fused_attention_against_non_fused(dtype: torch.dtype, use_flash: bool):
     torch.random.manual_seed(0)
 
     d_model = 128
