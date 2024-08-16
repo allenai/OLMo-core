be41d2abde18edf4b4c8ae048ceb713b4515d776
epwalsh <petew@allenai.org>
2024-08-16 12:49:05 -0700

    fix, run in bf16

--- src/test/nn/attention_test.py
+++ src/test/nn/attention_test.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict
 
+import pytest
 import torch
 
 from olmo_core.nn.attention import Attention, FusedAttention
@@ -9,7 +10,8 @@ from ..utils import requires_flash_attn, requires_gpu
 
 @requires_gpu
 @requires_flash_attn
-def test_fused_attention_against_non_fused():
+@pytest.mark.parametrize("dtype", [pytest.param(torch.bfloat16, id="bf16")])
+def test_fused_attention_against_non_fused(dtype):
     torch.random.manual_seed(0)
 
     d_model = 128
@@ -24,9 +26,11 @@ def test_fused_attention_against_non_fused():
     attention = Attention(**kwargs)
     fused_att = FusedAttention(**kwargs)
 
-    x1 = torch.randn(batch_size, seq_len, d_model, device="cuda")
+    x1 = torch.randn(batch_size, seq_len, d_model, dtype=dtype, device="cuda")
     x2 = x1.clone()
-    y1 = attention(x1)
-    y2 = fused_att(x2)
+
+    with torch.autocast("cuda", dtype=dtype, enabled=True):
+        y1 = attention(x1)
+        y2 = fused_att(x2)
 
     torch.testing.assert_close(y1, y2)
