4dd668cf4c8ce1336d93d86afa46dbdcaffc0ed5
epwalsh <petew@allenai.org>
2024-08-20 16:39:35 -0700

    don't record speed for the first batch

--- src/olmo_core/train/callbacks/speed_monitor.py
+++ src/olmo_core/train/callbacks/speed_monitor.py
@@ -12,22 +12,33 @@ class SpeedMonitorCallback(Callback):
     _total_steps: int = 0
     _total_tokens: int = 0
     _start_time: float = 0.0
+    _first_step: bool = True
 
     _step_start_time: float = 0.0
     _step_tokens: int = 0
 
     def pre_train(self):
-        self._total_steps = 0
-        self._total_tokens = 0
-        self._start_time = time.monotonic()
+        self._first_step = True
 
     def pre_step(self, batch: Dict[str, Any]):
+        if self._first_step:
+            # We don't record the first batch since the first one tends to take
+            # unusually long.
+            return
+
         self._step_start_time = time.monotonic()
         self._step_tokens = batch["input_ids"].numel()
         self._total_steps += 1
         self._total_tokens += self._step_tokens
 
     def post_step(self):
+        if self._first_step:
+            # Now we can start recording.
+            self._total_steps = 0
+            self._total_tokens = 0
+            self._start_time = time.monotonic()
+            self._first_step = False
+
         step_time = time.monotonic() - self._step_start_time
         total_time = time.monotonic() - self._start_time
         self.trainer.record_metric(
