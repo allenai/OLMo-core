1d075ffba04bf173450dc63a94b934d03217402b
epwalsh <petew@allenai.org>
2024-08-26 10:14:16 -0700

    don't set those now

--- src/olmo_core/distributed/utils.py
+++ src/olmo_core/distributed/utils.py
@@ -60,8 +60,8 @@ def init_distributed(backend: str = "nccl", timeout: timedelta = timedelta(minut
     if "jupiter" in get_node_hostname():
         set_env_var("NCCL_SOCKET_IFNAME", "ib")
         set_env_var("NCCL_IB_HCA", "^=mlx5_bond_0")
-    elif "pluto" in get_node_hostname():
-        set_env_var("NCCL_IB_HCA", "^=mlx5_1,mlx5_2")
+    #  elif "pluto" in get_node_hostname():
+    #      set_env_var("NCCL_IB_HCA", "^=mlx5_1,mlx5_2")
 
     validate_env_vars()
 
