dede0e6d348155f8e13245e475ccc6a5f98c8adb
epwalsh <petew@allenai.org>
2024-08-27 09:11:59 -0700

    ignore error messages when removing old image

--- src/scripts/create_beaker_image.sh
+++ src/scripts/create_beaker_image.sh
@@ -8,5 +8,5 @@ beaker_workspace=$3
 
 beaker_user=$(beaker account whoami --format=json | jq -r '.[0].name')
 beaker image create "${source_image}" --name "${beaker_image}-tmp" --workspace "${beaker_workspace}"
-beaker image delete "${beaker_user}/${beaker_image}" || true
+beaker image delete "${beaker_user}/${beaker_image}" 2>/dev/null || true
 beaker image rename "${beaker_user}/${beaker_image}-tmp" "${beaker_image}"
