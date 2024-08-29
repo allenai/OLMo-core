3ad72ada27166e5c3b725e690b393a327c50a9e0
epwalsh <petew@allenai.org>
2024-08-29 10:36:51 -0700

    collect existing ephemeral checkpoints

--- src/olmo_core/train/callbacks/checkpointer.py
+++ src/olmo_core/train/callbacks/checkpointer.py
@@ -1,7 +1,7 @@
 import logging
 from concurrent.futures import Future
 from dataclasses import dataclass, field
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 import torch.distributed as dist
 
@@ -12,6 +12,7 @@ from olmo_core.distributed.utils import (
 )
 from olmo_core.io import clear_directory
 
+from ..checkpoint import Checkpointer
 from .callback import Callback
 
 log = logging.getLogger(__name__)
@@ -41,6 +42,14 @@ class CheckpointerCallback(Callback):
     _checkpoints: List[str] = field(default_factory=list)
     _ephemeral_checkpoints: List[str] = field(default_factory=list)
 
+    @property
+    def checkpointer(self) -> Checkpointer:
+        return self.trainer.checkpointer
+
+    @property
+    def save_folder(self) -> str:
+        return self.trainer.save_folder
+
     def _await_last_checkpoint(self, blocking: bool = True) -> Optional[Future]:
         if (fut := self._future) is not None:
             # Wait for last async checkpoint to finish.
@@ -54,11 +63,11 @@ class CheckpointerCallback(Callback):
     def _save_checkpoint(self) -> str:
         self._await_last_checkpoint()
         self._latest_checkpoint = self.step
-        dirname = self.trainer.checkpointer.checkpoint_dirname(self.step)
-        path = f"{self.trainer.save_folder}/{dirname}"
+        dirname = self.checkpointer.checkpoint_dirname(self.step)
+        path = f"{self.save_folder}/{dirname}"
         if self.save_async:
             log.info(f"Saving checkpoint for step {self.step} to '{path}' asynchronously...")
-            self._future = self.trainer.checkpointer.save_async(
+            self._future = self.checkpointer.save_async(
                 path,
                 self.trainer.model,
                 self.trainer.optim,
@@ -66,7 +75,7 @@ class CheckpointerCallback(Callback):
             )
         else:
             log.info(f"Saving checkpoint for step {self.step} to '{path}'...")
-            self.trainer.checkpointer.save(
+            self.checkpointer.save(
                 path,
                 self.trainer.model,
                 self.trainer.optim,
@@ -75,22 +84,46 @@ class CheckpointerCallback(Callback):
             log.info("Checkpoint saved")
         return path
 
+    def _remove_checkpoint(self, path: str):
+        if get_fs_local_rank() == 0:
+            self.trainer.thread_pool.submit(clear_directory, path)
+
     def pre_train(self):
-        if is_distributed() and self.save_async and self.trainer.checkpointer.process_group is None:
+        # Maybe create a new process group for async checkpointing.
+        if is_distributed() and self.save_async and self.checkpointer.process_group is None:
             if not backend_supports_cpu():
                 raise RuntimeError("a CPU-capable backend is required for async checkpointing")
 
             log.info(
                 "Creating new process group for checkpointing (needed for async checkpointing)"
             )
-            self.trainer.checkpointer.process_group = dist.new_group()
+            self.checkpointer.process_group = dist.new_group()
 
-        if (
-            self.step == 0
-            and self.pre_train_checkpoint is not False
-            and not self.trainer.checkpoint_loaded
+        # Maybe save a pre-train checkpoint.
+        if self.step == 0 and (
+            self.pre_train_checkpoint
+            or (self.pre_train_checkpoint is None and not self.trainer.checkpoint_loaded)
         ):
             self._checkpoints.append(self._save_checkpoint())
+
+        # Collect existing ephemeral checkpoints from previous runs.
+        if self.ephemeral_save_interval is not None:
+            ephemeral_checkpoints: List[Tuple[int, str]] = []
+            for step_num, path in self.checkpointer.find_checkpoints(self.save_folder):
+                if step_num == 0 and step_num % self.save_interval == 0:
+                    continue
+                elif step_num % self.ephemeral_save_interval == 0:
+                    ephemeral_checkpoints.append((step_num, path))
+                    log.info(
+                        f"Collected existing ephemeral checkpoint for step {step_num} at '{path}'"
+                    )
+
+            # TODO: handle this if we ever restore callback state.
+            assert not self._ephemeral_checkpoints
+
+            self._ephemeral_checkpoints = [
+                path for _, path in sorted(ephemeral_checkpoints, key=lambda x: x[0])
+            ]
 
     def post_train_batch(self):
         self._await_last_checkpoint(blocking=False)
@@ -104,8 +137,7 @@ class CheckpointerCallback(Callback):
             while len(self._ephemeral_checkpoints) > 1:
                 oldest_path = self._ephemeral_checkpoints.pop(0)
                 log.info(f"Removing old ephemeral checkpoint at '{oldest_path}'...")
-                if get_fs_local_rank() == 0:
-                    clear_directory(oldest_path)
+                self._remove_checkpoint(oldest_path)
 
     def post_train(self):
         if self.step > self._latest_checkpoint:
