d850ed55a838503ab8aeb981c556f273b0fc7369
epwalsh <petew@allenai.org>
2024-08-30 18:26:35 -0700

    try without env var

--- src/olmo_core/distributed/utils.py
+++ src/olmo_core/distributed/utils.py
@@ -58,7 +58,7 @@ def init_distributed(backend: str = "nccl", timeout: timedelta = timedelta(minut
     # Host-specific env vars.
     # See https://beaker-docs.apps.allenai.org/experiments/distributed-training.html#ai2pluto-cirrascale.
     if "jupiter" in get_node_hostname():
-        set_env_var("NCCL_SOCKET_IFNAME", "ib")
+        #  set_env_var("NCCL_SOCKET_IFNAME", "ib")
         set_env_var("NCCL_IB_HCA", "^=mlx5_bond_0")
     elif "pluto" in get_node_hostname():
         set_env_var("NCCL_IB_HCA", "^=mlx5_1,mlx5_2")
