5a52c47f9349a7ce43b4e9aa52d970fc2e97492f
epwalsh <petew@allenai.org>
2024-09-06 14:02:49 -0700

    Add Lion optimizer and a "skip step" version of it

--- src/olmo_core/optim/__init__.py
+++ src/olmo_core/optim/__init__.py
@@ -1,11 +1,16 @@
 from .adamw import AdamWConfig
 from .config import OptimConfig, OptimGroupOverride
+from .lion import Lion, SkipStepLion
 from .scheduler import ConstantScheduler, CosWithWarmup, Scheduler
+from .skip_step_optimizer import SkipStepOptimizer
 
 __all__ = [
     "OptimConfig",
     "OptimGroupOverride",
+    "SkipStepOptimizer",
     "AdamWConfig",
+    "Lion",
+    "SkipStepLion",
     "Scheduler",
     "CosWithWarmup",
     "ConstantScheduler",
---
+++ src/olmo_core/optim/skip_step_optimizer.py
@@ -0,0 +1,80 @@
+from typing import Any, Dict, Iterable, List, Union
+
+import torch
+from torch.optim.optimizer import Optimizer
+from typing_extensions import TypeAlias
+
+ParamsT: TypeAlias = Union[Iterable[torch.Tensor], Iterable[Dict[str, Any]]]
+
+
+class SkipStepOptimizer(Optimizer):
+    """
+    A :class:`SkipStepOptimizer` is an optimizer that can skip updates when the loss or gradient
+    norm for a step is above a certain threshold of standard deviations computed over a rolling
+    interval.
+
+    .. important::
+        When using a :class:`SkipStepOptimizer` you must always set :data:`latest_loss` and
+        :data:`latest_grad_norm` to the current loss and grad norm, respectively, *before* calling
+        :meth:`step()`.
+
+    .. tip::
+        When implementing a :class:`SkipStepOptimizer` you should be careful to avoid host-device
+        syncs. You can use :meth:`get_step_factor()` within your :meth:`step()` method to do this.
+        See the implementation of :class:`SkipStepLion` for an example.
+    """
+
+    def __init__(
+        self,
+        params: ParamsT,
+        defaults: Dict[str, Any],
+        rolling_interval_length: int = 128,
+        sigma_factor: int = 6,
+    ) -> None:
+        super().__init__(params, defaults)
+        self.rolling_interval_length = rolling_interval_length
+        self.sigma_factor = sigma_factor
+        self._losses: List[torch.Tensor] = []
+        self._grad_norms: List[torch.Tensor] = []
+
+    @property
+    def latest_loss(self) -> torch.Tensor:
+        if not self._losses:
+            raise RuntimeError("'latest_loss' has not been set yet")
+        return self._losses[-1]
+
+    @latest_loss.setter
+    def latest_loss(self, loss: torch.Tensor):
+        self._losses.append(loss)
+        while len(self._losses) > self.rolling_interval_length + 1:
+            self._losses.pop(0)
+
+    @property
+    def latest_grad_norm(self) -> torch.Tensor:
+        if not self._grad_norms:
+            raise RuntimeError("'latest_grad_norm' has not been set yet")
+        return self._grad_norms[-1]
+
+    @latest_grad_norm.setter
+    def latest_grad_norm(self, grad_norm: torch.Tensor):
+        self._grad_norms.append(grad_norm)
+        while len(self._grad_norms) > self.rolling_interval_length + 1:
+            self._grad_norms.pop(0)
+
+    def get_step_factor(self) -> torch.Tensor:
+        """
+        Returns a float tensor which will be `1.0` if the optimizer should proceed with the step
+        and `0.0` if the optimizer should skip the step.
+
+        The tensor can be used within the optimizer's step computation to essentially skip a step
+        without a host-device sync.
+        """
+        if len(self._losses) < max(20, self.rolling_interval_length // 2):
+            return torch.tensor(1.0).to(device=self.latest_loss.device, non_blocking=True)
+
+        loss_std = torch.std(torch.stack(self._losses[:-1]))
+        grad_norm_std = torch.std(torch.stack(self._grad_norms[:-1]))
+        return (
+            self.latest_loss <= self.sigma_factor * loss_std
+            and self.latest_grad_norm <= self.sigma_factor * grad_norm_std
+        )
