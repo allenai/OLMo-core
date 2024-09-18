5e3100b71767daff14e47c0b9b5713832742b824
epwalsh <petew@allenai.org>
2024-09-18 15:08:41 -0700

    Add option to change model init seed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Added support for sequence length warm-up during training via the callback `SequenceLengthSchedulerCallback`.
 - Added support for variable sequence length (VSL) datasets and VSL curriculums as introduced in ["Dataset Decomposition: Faster LLM Training with Variable Sequence Length Curriculum"](https://arxiv.org/pdf/2405.13226).
 - Added `Lion` and `SkipStepLion` optimizers.
+- Added `init_seed` argument to `Transformer` and `TransformerConfig`.
 
 ### Changed
 
--- src/olmo_core/nn/transformer/init.py
+++ src/olmo_core/nn/transformer/init.py
@@ -1,5 +1,6 @@
-from typing import Union
+from typing import Optional, Union
 
+import torch
 import torch.nn as nn
 
 from olmo_core.config import StrEnum
@@ -27,25 +28,38 @@ class InitMethod(StrEnum):
     dependent on either ``d_model`` or the layer index.
     """
 
-    def _init_linear(self, m: nn.Linear, *, std: float = 0.02):
-        nn.init.trunc_normal_(m.weight, mean=0.0, std=std, a=-3 * std, b=3 * std)
+    def _init_linear(
+        self, m: nn.Linear, *, std: float = 0.02, generator: Optional[torch.Generator] = None
+    ):
+        nn.init.trunc_normal_(
+            m.weight, mean=0.0, std=std, a=-3 * std, b=3 * std, generator=generator
+        )
         if m.bias is not None:
             nn.init.zeros_(m.bias)
 
-    def init_embeddings(self, m: nn.Embedding):
+    def init_embeddings(self, m: nn.Embedding, generator: Optional[torch.Generator] = None):
         if self in (InitMethod.llama, InitMethod.llama_depth):
-            nn.init.normal_(m.weight)
+            nn.init.normal_(m.weight, generator=generator)
         else:
-            nn.init.trunc_normal_(m.weight, mean=0.0, std=0.02, a=-3 * 0.02, b=3 * 0.02)
+            nn.init.trunc_normal_(
+                m.weight, mean=0.0, std=0.02, a=-3 * 0.02, b=3 * 0.02, generator=generator
+            )
 
-    def init_final_w_out(self, m: nn.Linear, *, d_model: int):
+    def init_final_w_out(
+        self, m: nn.Linear, *, d_model: int, generator: Optional[torch.Generator] = None
+    ):
         std = 0.02
         if self in (InitMethod.llama, InitMethod.llama_depth):
             std = d_model**-0.05
-        self._init_linear(m, std=std)
+        self._init_linear(m, std=std, generator=generator)
 
     def init_attention(
-        self, m: Union[Attention, FusedAttention], *, block_idx: int, num_blocks: int
+        self,
+        m: Union[Attention, FusedAttention],
+        *,
+        block_idx: int,
+        num_blocks: int,
+        generator: Optional[torch.Generator] = None,
     ):
         std = 0.02
         if self == InitMethod.llama:
@@ -55,21 +69,28 @@ class InitMethod(StrEnum):
 
         if isinstance(m, Attention):
             for w in (m.w_q, m.w_k, m.w_v):
-                self._init_linear(w, std=0.02)
+                self._init_linear(w, std=0.02, generator=generator)
         elif isinstance(m, FusedAttention):
-            self._init_linear(m.w_qkv, std=0.02)
+            self._init_linear(m.w_qkv, std=0.02, generator=generator)
         else:
             raise NotImplementedError(m)
 
-        self._init_linear(m.w_out, std=std)
+        self._init_linear(m.w_out, std=std, generator=generator)
 
-    def init_feed_forward(self, m: FeedForward, *, block_idx: int, num_blocks: int):
+    def init_feed_forward(
+        self,
+        m: FeedForward,
+        *,
+        block_idx: int,
+        num_blocks: int,
+        generator: Optional[torch.Generator] = None,
+    ):
         std = 0.02
         if self == InitMethod.llama:
             std = 0.02 / (2 * num_blocks) ** 0.5
         elif self == InitMethod.llama_depth:
             std = 0.02 / (2 * (block_idx + 1)) ** 0.5
 
-        self._init_linear(m.w1, std=0.02)
-        self._init_linear(m.w2, std=std)
-        self._init_linear(m.w3, std=std)
+        self._init_linear(m.w1, std=0.02, generator=generator)
+        self._init_linear(m.w2, std=std, generator=generator)
+        self._init_linear(m.w3, std=std, generator=generator)
