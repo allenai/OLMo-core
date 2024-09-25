84b72c98ca0a53ad575c752c8e1678d3a2dc9b9d
epwalsh <petew@allenai.org>
2024-09-25 10:24:59 -0700

    (chore) prepare for release v1.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## [v1.2.0](https://github.com/allenai/OLMo-core/releases/tag/v1.2.0) - 2024-09-25
+
 ### Added
 
 - Added support for wildcards in `OptimGroupOverride.params`.
--- src/olmo_core/version.py
+++ src/olmo_core/version.py
@@ -1,10 +1,6 @@
 _MAJOR = "1"
-_MINOR = "1"
-# On main and in a nightly release the patch should be one ahead of the last
-# released build.
+_MINOR = "2"
 _PATCH = "0"
-# This is mainly for nightly builds which have the suffix ".dev$DATE". See
-# https://semver.org/#is-v123-a-semantic-version for the semantics.
 _SUFFIX = ""
 
 VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
