2fcc23c082a133739dcb1f60df04125645dd73ec
Shane A <shanea@allenai.org>
2024-10-02 10:15:47 -0700

    Fix layer norm eps kwarg

--- src/scripts/train/peteish-1B.py
+++ src/scripts/train/peteish-1B.py
@@ -54,7 +54,7 @@ def build_config(run_name: str, platform: Platform, overrides: List[str]) -> Exp
 
     model_config = TransformerConfig.olmo_1B(
         vocab_size=tokenizer_config.padded_vocab_size(),
-        norm_eps=1e-6,
+        layer_norm_eps=1e-6,
         compile=True,
         dp_config=DataParallelConfig(
             name=DataParallelType.fsdp, param_dtype=DType.bfloat16, reduce_dtype=DType.float32
