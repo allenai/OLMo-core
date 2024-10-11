d90f5da0168f00b3c3f655645cbff37e0cdbeae1
epwalsh <petew@allenai.org>
2024-10-11 09:15:07 -0700

    Add comet API key to launch config

--- src/olmo_core/internal/experiment.py
+++ src/olmo_core/internal/experiment.py
@@ -144,6 +144,7 @@ def build_common_components(
         env_secrets=[
             BeakerEnvSecret(name="BEAKER_TOKEN", secret=f"{beaker_user}_BEAKER_TOKEN"),
             BeakerEnvSecret(name="WANDB_API_KEY", secret=f"{beaker_user}_WANDB_API_KEY"),
+            BeakerEnvSecret(name="COMET_API_KEY", secret=f"{beaker_user}_COMET_API_KEY"),
             BeakerEnvSecret(name="AWS_CONFIG", secret=f"{beaker_user}_AWS_CONFIG"),
             BeakerEnvSecret(name="AWS_CREDENTIALS", secret=f"{beaker_user}_AWS_CREDENTIALS"),
             BeakerEnvSecret(name="R2_ENDPOINT_URL", secret="R2_ENDPOINT_URL"),
