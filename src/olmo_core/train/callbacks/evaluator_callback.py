91678a4651ab2948a69f92b150dfb313e5291552
epwalsh <petew@allenai.org>
2024-10-28 15:35:30 -0700

    Add callback for downstream evals

--- src/olmo_core/train/callbacks/evaluator_callback.py
+++ src/olmo_core/train/callbacks/evaluator_callback.py
@@ -1,18 +1,24 @@
 import logging
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING, List, Optional
+from typing import TYPE_CHECKING, Any, Dict, List, Optional
 
-from olmo_core.data import NumpyDatasetConfig, NumpyPaddedFSLDataset
-from olmo_core.distributed.utils import get_world_size
+import torch
+import torch.distributed as dist
+from torch.utils.data import DataLoader, DistributedSampler
+
+from olmo_core.data import NumpyDatasetConfig, NumpyPaddedFSLDataset, TokenizerConfig
+from olmo_core.distributed.utils import get_rank, get_world_size, is_distributed
 from olmo_core.eval import Evaluator
 from olmo_core.eval.lm_evaluator import LMEvaluator
 from olmo_core.exceptions import OLMoConfigurationError
-from olmo_core.utils import format_float, move_to_device
+from olmo_core.utils import format_float, get_default_device, move_to_device
 
 from ..common import Duration
 from .callback import Callback, CallbackConfig
 
 if TYPE_CHECKING:
+    from olmo_eval import HFTokenizer
+
     from ..trainer import Trainer
 
 log = logging.getLogger(__name__)
@@ -124,9 +130,119 @@ class LMEvaluatorCallbackConfig(CallbackConfig):
             global_batch_size=eval_batch_size,
             collator=trainer.data_loader.collator,
             device=trainer.device,
+            dp_process_group=trainer.dp_process_group,
         )
         return EvaluatorCallback(
             evaluators=[evaluator],
+            eval_interval=self.eval_interval,
+            log_interval=self.log_interval,
+            eval_duration=self.eval_duration,
+        )
+
+
+class DownstreamEvaluator(Evaluator):
+    metric_type_to_label = {
+        "f1": "F1 score",
+        "acc": "accuracy",
+        "len_norm": "length-normalized accuracy",
+        "pmi_dc": "PMI-DC accuracy",
+        "ce_loss": "CE loss",
+        "bpb": "BPB",
+    }
+
+    def __init__(
+        self,
+        *,
+        name: str,
+        task: str,
+        rank_batch_size: int,
+        tokenizer: "HFTokenizer",
+        device: Optional[torch.device] = None,
+        dp_process_group: Optional[dist.ProcessGroup] = None,
+    ):
+        from olmo_eval import ICLMetric, build_task
+
+        self.label = task
+        self.task = build_task(task, tokenizer)
+        self.metric = ICLMetric(metric_type=self.task.metric_type).to(
+            device or get_default_device()
+        )
+        sampler: Optional[DistributedSampler] = None
+        if is_distributed():
+            sampler = DistributedSampler(
+                self.task,  # type: ignore
+                drop_last=True,
+                shuffle=False,
+                num_replicas=get_world_size(dp_process_group),
+                rank=get_rank(dp_process_group),
+            )
+
+        data_loader = DataLoader(
+            self.task,  # type: ignore
+            batch_size=rank_batch_size,
+            collate_fn=self.task.collate_fn,
+            num_workers=0,
+            sampler=sampler,
+        )
+
+        super().__init__(
+            name=name, batches=data_loader, device=device, dp_process_group=dp_process_group
+        )
+
+    def update_metrics(
+        self, batch: Dict[str, Any], ce_loss: torch.Tensor, logits: torch.Tensor
+    ) -> None:
+        del ce_loss
+        self.metric.update(batch, logits)
+
+    def compute_metrics(self) -> Dict[str, torch.Tensor]:
+        value = self.metric.compute()
+        label = f"{self.label} ({self.metric_type_to_label[self.task.metric_type]})"
+        return {label: value}
+
+    def reset_metrics(self) -> None:
+        self.metric.reset()
+
+
+@dataclass
+class DownstreamEvaluatorCallbackConfig(CallbackConfig):
+    tasks: List[str]
+    tokenizer: TokenizerConfig
+    eval_rank_batch_size_instances: int
+    eval_interval: int = 1000
+    eval_duration: Duration = field(default_factory=lambda: Duration.epochs(1))
+    log_interval: int = 5
+
+    def build(self, trainer: "Trainer") -> Callback:
+        from olmo_eval import HFTokenizer
+
+        if self.tokenizer.identifier is None:
+            raise OLMoConfigurationError(
+                "Tokenizer 'identifier' required to build a concrete tokenizer"
+            )
+
+        tokenizer = HFTokenizer(
+            self.tokenizer.identifier,
+            pad_token_id=self.tokenizer.pad_token_id,
+            eos_token_id=self.tokenizer.eos_token_id,
+            bos_token_id=self.tokenizer.bos_token_id,
+        )
+
+        evaluators: List[Evaluator] = []
+        for task in self.tasks:
+            evaluators.append(
+                DownstreamEvaluator(
+                    name="downstream",
+                    task=task,
+                    rank_batch_size=self.eval_rank_batch_size_instances,
+                    tokenizer=tokenizer,
+                    device=self.trainer.device,
+                    dp_process_group=trainer.dp_process_group,
+                )
+            )
+
+        return EvaluatorCallback(
+            evaluators=evaluators,
             eval_interval=self.eval_interval,
             log_interval=self.log_interval,
             eval_duration=self.eval_duration,
