00d34f6931f890cc814f394fcad3c7dcff0498d1
Pete Walsh <petew@allenai.org>
2024-11-01 12:31:37 -0700

    Add option to compile loss function, move logits FP32 casting into loss function (#77)
    This change gives a small but not insignificant improvement to
    throughput and GPU memory usage by allowing the casting of logits to
    FP32 to be fused into the loss function via `torch.compile()`.

--- src/scripts/train/OLMoE-1B-7B.py
+++ src/scripts/train/OLMoE-1B-7B.py
@@ -77,6 +77,7 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
             metrics_collect_interval=10,
             cancel_check_interval=1,
             z_loss_multiplier=1e-5,
+            compile_loss=True,
         )
         .with_callback(
             "checkpointer",
