dd741337f17c28676bef390a916ebe91241ffdd4
epwalsh <petew@allenai.org>
2024-11-04 15:02:01 -0800

    try exporting a different way

--- src/scripts/train/all_reduce_bench.py
+++ src/scripts/train/all_reduce_bench.py
@@ -114,6 +114,7 @@ def build_config(script: str, run_name: str, cluster: str, overrides: List[str])
             "pip install -e '.[all]'",
             "pip freeze",
             "ls -lh /var/lib/tcpxo/lib64",
+            "export LD_LIBRARY_PATH=/var/lib/tcpxo/lib64:$LD_LIBRARY_PATH",
             # Move AWS credentials from env to relevant files
             #  "mkdir -p ~/.aws",
             #  "printenv AWS_CONFIG > ~/.aws/config",
