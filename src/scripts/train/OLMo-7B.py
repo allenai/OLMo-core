93d53ccc78dd977927549b9a888d92690ceb99d9
epwalsh <petew@allenai.org>
2024-11-06 11:09:33 -0800

    add compile back

--- src/scripts/train/OLMo-7B.py
+++ src/scripts/train/OLMo-7B.py
@@ -18,7 +18,7 @@ log = logging.getLogger(__name__)
 def build_model_config(common: CommonComponents) -> TransformerConfig:
     return TransformerConfig.olmo_7B(
         vocab_size=common.tokenizer.padded_vocab_size(),
-        compile=False,
+        compile=True,
         fused_ops=False,
         use_flash=True,
         dp_config=TransformerDataParallelConfig(
