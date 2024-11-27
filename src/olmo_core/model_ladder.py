b550c185770e36976f2d0dc3a1a2cc0a7b728a1d
epwalsh <petew@allenai.org>
2024-11-27 10:54:48 -0800

    clean up again

--- src/olmo_core/model_ladder.py
+++ src/olmo_core/model_ladder.py
@@ -3,7 +3,6 @@ Configuration classes for defining model ladder scaling ablations.
 """
 
 import logging
-import math
 from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass, field
 from typing import Optional
