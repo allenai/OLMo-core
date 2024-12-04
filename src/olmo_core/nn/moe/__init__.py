6135725058b6395bda0cdd1d898f77760791146f
epwalsh <petew@allenai.org>
2024-12-03 16:35:31 -0800

    Move MoE callback logic to a new `MoEHandler` class

--- src/olmo_core/nn/moe/__init__.py
+++ src/olmo_core/nn/moe/__init__.py
@@ -3,6 +3,7 @@ MoE layers. Requires `megablocks <https://github.com/databricks/megablocks>`_.
 """
 
 from .config import MoEActivationFn, MoEConfig, MoEMLPImplementation, MoEType
+from .handler import MoEHandler
 from .layers import MoE
 
-__all__ = ["MoE", "MoEConfig", "MoEType", "MoEActivationFn", "MoEMLPImplementation"]
+__all__ = ["MoE", "MoEConfig", "MoEType", "MoEActivationFn", "MoEMLPImplementation", "MoEHandler"]
