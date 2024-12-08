c277d54da18799a4d3de21c816876b9745c4a079
Dirk Groeneveld <dirkg@allenai.org>
2024-12-08 00:00:07 -0800

    Make sure we use the same CE loss that we used for the 13B

--- src/scripts/train/OLMo2-26B.py
+++ src/scripts/train/OLMo2-26B.py
@@ -60,7 +60,8 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
             metrics_collect_interval=10,
             cancel_check_interval=10,
             z_loss_multiplier=1e-5,
-            compile_loss=True,
+            fused_loss=True,
+            compile_loss=False,
         )
         .with_callback(
             "checkpointer",
