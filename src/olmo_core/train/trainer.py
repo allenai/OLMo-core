d568fc296ea099fbec420480cc5854a39f022cc9
epwalsh <petew@allenai.org>
2024-12-10 16:05:36 -0800

    log callback order

--- src/olmo_core/train/trainer.py
+++ src/olmo_core/train/trainer.py
@@ -482,6 +482,10 @@ class Trainer:
             elif self.load_strategy == LoadStrategy.if_available:
                 self.maybe_load_checkpoint(load_path)
 
+        log.info("Callback order:")
+        for i, callback_name in enumerate(self.callbacks.keys()):
+            log.info(f"  - Callback {i+1}: {callback_name}")
+
         log.info(f"Training for {self.max_steps:,d} steps")
 
         for callback in self.callbacks.values():
