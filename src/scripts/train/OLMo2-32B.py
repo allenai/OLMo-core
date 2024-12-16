1f79446c5e4a27f753cbf7e278abd61587c6904d
Dirk Groeneveld <dirkg@allenai.org>
2024-12-16 01:46:22 -0800

    Revert "Async checkpointer may have problems with large checkpoints?"
    This reverts commit 4804004ed5b94db5ae0403beeff0190c7dd61183.

--- src/scripts/train/OLMo2-32B.py
+++ src/scripts/train/OLMo2-32B.py
@@ -82,7 +82,7 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
             CheckpointerCallback(
                 save_interval=1000,
                 ephemeral_save_interval=250,
-                save_async=False,
+                save_async=True,
             ),
         )
         .with_callback(
