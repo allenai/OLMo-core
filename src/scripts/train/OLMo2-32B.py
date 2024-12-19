d2a00a7469a89ed47fbcb5930b409be3a1271f9e
Shane A <shanea@allenai.org>
2024-12-19 15:46:27 -0800

    Increase microbatch size, increase activation checkpointing

--- src/scripts/train/OLMo2-32B.py
+++ src/scripts/train/OLMo2-32B.py
@@ -43,11 +43,11 @@ def build_model_config(common: CommonComponents) -> TransformerConfig:
             reduce_dtype=DType.float32,
             num_replicas=64 // 2,  # common.launch.num_nodes // 2,
         ),
-        # ac_config=TransformerActivationCheckpointingConfig(TransformerActivationCheckpointingMode.full),
-        ac_config=TransformerActivationCheckpointingConfig(
-            mode=TransformerActivationCheckpointingMode.selected_modules,
-            modules=[f"blocks.{i}.feed_forward" for i in range(64)],
-        ),
+        ac_config=TransformerActivationCheckpointingConfig(TransformerActivationCheckpointingMode.full),
+        # ac_config=TransformerActivationCheckpointingConfig(
+        #     mode=TransformerActivationCheckpointingMode.selected_modules,
+        #     modules=[f"blocks.{i}.feed_forward" for i in range(64)],
+        # ),
         float8_config=Float8Config(compile=compile, enabled=False),
     )
 
@@ -70,7 +70,7 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
     return (
         TrainerConfig(
             save_folder=f"gs://ai2-llm/checkpoints/{project_name}/",
-            rank_microbatch_size=1 * 4096,
+            rank_microbatch_size=2 * 4096,
             checkpointer=CheckpointerConfig(pre_download=True, thread_count=32),
             save_overwrite=True,
             metrics_collect_interval=10,
