1264c35f793c3b0b65f9a6ba12ccc1f71cfada59
Shane A <shanea@allenai.org>
2024-12-22 23:32:05 -0800

    Upload test

--- src/scripts/train/OLMo2-32B.py
+++ src/scripts/train/OLMo2-32B.py
@@ -85,7 +85,7 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
             "checkpointer",
             CheckpointerCallback(
                 save_interval=1000,
-                ephemeral_save_interval=250,
+                ephemeral_save_interval=5,
                 save_async=True,
             ),
         )
