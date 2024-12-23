d81cd12978c8482e1ad28b2bb60a3e92d7a5a6f4
Shane A <shanea@allenai.org>
2024-12-22 22:38:37 -0800

    Make gcs upload pass generation

--- src/olmo_core/io.py
+++ src/olmo_core/io.py
@@ -590,11 +590,17 @@ def _gcs_upload(source: Path, bucket_name: str, key: str, save_overwrite: bool =
     storage_client = _get_gcs_client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(key)
-    if not save_overwrite and blob.exists():
-        raise FileExistsError(
-            f"gs://{bucket_name}/{key} already exists. Use save_overwrite to overwrite it."
-        )
-    blob.upload_from_filename(source, retry=_get_gcs_conditional_retry())
+
+    generation: Optional[int] = None
+    if blob.exists():
+        if save_overwrite:
+            generation = blob.generation
+        else:
+            raise FileExistsError(
+                f"gs://{bucket_name}/{key} already exists. Use save_overwrite to overwrite it."
+            )
+
+    blob.upload_from_filename(source, if_generation_match=generation, retry=_get_gcs_conditional_retry())
 
 
 @retriable()
