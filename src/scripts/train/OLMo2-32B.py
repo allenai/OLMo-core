1558306eec2fddfb78ed4f59eec9957af2a3a2c8
Dirk Groeneveld <dirkg@allenai.org>
2024-12-29 22:11:53 -0800

    Go back to FSDP to see if we can survive more than 9 steps

--- src/scripts/train/OLMo2-32B.py
+++ src/scripts/train/OLMo2-32B.py
@@ -35,7 +35,7 @@ def build_model_config(common: CommonComponents) -> TransformerConfig:
         fused_ops=False,
         use_flash=not compile,
         dp_config=TransformerDataParallelConfig(
-            name=DataParallelType.hsdp,
+            name=DataParallelType.fsdp,
             param_dtype=DType.bfloat16,
             reduce_dtype=DType.float32,
             num_replicas=4,
