f47f6f54baa8d6b19742baac0a040abcd76576c0
epwalsh <petew@allenai.org>
2025-01-17 12:06:02 -0800

    minor clean up

--- src/olmo_core/launch/beaker.py
+++ src/olmo_core/launch/beaker.py
@@ -351,7 +351,7 @@ class BeakerLaunchConfig(Config):
                 leader_selection=self.num_nodes > 1,
                 host_networking=self.num_nodes > 1
                 or any(["augusta" in cluster for cluster in self.clusters]),
-                propagate_failure=False,
+                propagate_failure=False if self.num_nodes > 1 else None,
                 propagate_preemption=True if self.num_nodes > 1 else None,
                 synchronized_start_timeout="90m" if self.num_nodes > 1 else None,
                 resources=TaskResources(gpu_count=self.num_gpus, shared_memory="10GiB"),
