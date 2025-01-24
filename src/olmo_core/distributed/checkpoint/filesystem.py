f34fd94a2596da7f7f3288bdc4d7f69f194ff6ac
epwalsh <petew@allenai.org>
2025-01-24 09:30:17 -0800

    fix

--- src/olmo_core/distributed/checkpoint/filesystem.py
+++ src/olmo_core/distributed/checkpoint/filesystem.py
@@ -386,7 +386,9 @@ class RemoteFileSystemReader(dist_cp.StorageReader):
                 ) as metadata_file:
                     metadata = pickle.load(metadata_file)
             except FileNotFoundError as exc:
-                msg = f"'{dir}' does not appear to contain a distributed state dict/checkpoint."
+                msg = (
+                    f"'{self.path}' does not appear to contain a distributed state dict/checkpoint."
+                )
                 suggested_dir = join_path(self.path, "model_and_optim")
                 if file_exists(join_path(suggested_dir, ".metadata")):
                     msg += f" Did you mean to use '{suggested_dir}'?"
