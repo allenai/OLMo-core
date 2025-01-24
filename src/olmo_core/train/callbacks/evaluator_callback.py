cbf137d7328faed93fa6ed133c224acb104b729e
epwalsh <petew@allenai.org>
2025-01-24 13:09:11 -0800

    log eval time

--- src/olmo_core/train/callbacks/evaluator_callback.py
+++ src/olmo_core/train/callbacks/evaluator_callback.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from dataclasses import dataclass, field
 from typing import TYPE_CHECKING, Any, Dict, List, Optional
 
@@ -59,6 +60,8 @@ class EvaluatorCallback(Callback):
         if self.step <= 1 or self.step % self.eval_interval != 0:
             return
 
+        start_time = time.monotonic()
+
         # Put model in eval train mode.
         self.trainer.optim.zero_grad(set_to_none=True)
         self.trainer.model.eval()
@@ -66,6 +69,7 @@ class EvaluatorCallback(Callback):
 
         for evaluator in self.evaluators:
             log.info(f"Running {evaluator.name} evals...")
+            inner_start_time = time.monotonic()
             evaluator.reset_metrics()
             eval_step = 0
             eval_tokens = 0
@@ -101,10 +105,15 @@ class EvaluatorCallback(Callback):
                 for name, value in evaluator.compute_metrics().items():
                     metrics.append(f"    {name}={format_float(value.item())}")
                     self.trainer.record_metric(f"eval/{evaluator.name}/{name}", value)
-            log.info("Eval metrics:\n" + "\n".join(metrics))
+            log.info(
+                f"Finished {evaluator.name} evals in {time.monotonic() - inner_start_time:.1f} seconds. Metrics:\n"
+                + "\n".join(metrics)
+            )
 
         # Restore model to train mode.
         self.trainer.model.train()
+
+        log.info(f"Finished evals in {time.monotonic() - start_time:.1f} seconds")
 
     def _log_progress(self, evaluator: Evaluator, eval_step: int):
         if evaluator.total_batches is not None:
