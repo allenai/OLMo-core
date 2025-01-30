04811a48fb80eacbb91f9ef88ad926b07ce7fbbf
aman-17 <amanrangapur@gmail.com>
2025-01-30 15:30:09 -0800

    fix

--- src/scripts/train/OLMo2-7B.py
+++ src/scripts/train/OLMo2-7B.py
@@ -89,7 +89,7 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
 
 if __name__ == "__main__":
     main(
-        global_batch_size=4 * 2048,
+        global_batch_size=12 * 2048,
         model_config_builder=build_model_config,
         optim_config_builder=build_optim_config,
         trainer_config_builder=build_trainer_config,
