c8b30325cbb068713de2f331caea49266287077e
aman-17 <amanrangapur@gmail.com>
2025-01-30 14:24:44 -0800

    add-2g1

--- src/olmo_core/train/config.py
+++ src/olmo_core/train/config.py
@@ -31,9 +31,10 @@ class TrainerConfig(Config):
         See the :class:`Trainer` documentation for a description of the fields.
     """
 
-    save_folder: Optional[str] = None
+    
     rank_microbatch_size: int
-
+    
+    save_folder: Optional[str] = None
     work_dir: Optional[str] = None
     load_path: Optional[str] = None
     load_strategy: LoadStrategy = LoadStrategy.if_available
