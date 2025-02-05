0cc1c7891b7d5d814ea0133a9bc19bd2ca46a210
epwalsh <petew@allenai.org>
2025-02-04 16:45:19 -0800

    try this

--- src/olmo_core/nn/moe/parallel_mlp.py
+++ src/olmo_core/nn/moe/parallel_mlp.py
@@ -250,8 +250,7 @@ class ParallelMLP(ParallelMLPBase):
 
         # Start the cross-device permutation asynchronously so we can
         # overlap communication with computation.
-        # shape: (num_local_experts * ep_world_size, expert_capacity, d_model)
-        #      = (num_experts, expert_capacity, d_model)
+        # shape: (num_local_experts * ep_world_size, local_expert_capacity, d_model)
         parallel_x, parallel_x_handle = ops.all_to_all(
             x,
             group=self._ep_pg,
@@ -277,14 +276,15 @@ class ParallelMLP(ParallelMLPBase):
                 self.num_local_experts,
             )
 
-            # shape: (num_experts * expert_capacity,)
+            # shape: (num_local_experts * ep_world_size * local_expert_capacity,)
+            #      = (num_local_experts * expert_capacity,)
             parallel_top_expert = torch.repeat_interleave(
                 parallel_top_expert,
-                expert_capacity,
-                output_size=parallel_top_expert.numel() * expert_capacity,
+                local_expert_capacity,
+                output_size=parallel_top_expert.numel() * local_expert_capacity,
             )
 
-            # shape: (num_experts * expert_capacity,)
+            # shape: (num_local_experts * expert_capacity,)
             _, parallel_indices = torch.sort(parallel_top_expert)
 
             # Calculate the bins boundaries from the token counts.
@@ -302,10 +302,12 @@ class ParallelMLP(ParallelMLPBase):
         parallel_x_handle.wait()
         if dist.get_rank() == 0:
             print(f"C {parallel_x.shape=} {parallel_x=}")
+        # shape: (num_local_experts * ep_world_size, local_expert_capacity, d_model)
+        #     ~= (num_local_experts, expert_capacity, d_model)
         parallel_x = self.permute_and_compute(
             parallel_x,
             indices=parallel_indices.int(),
-            expert_weights=None,  # expert_weights
+            expert_weights=None,
             bins=parallel_bins,
             expert_capacity=expert_capacity,
             top_k=1,
