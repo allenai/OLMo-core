3994f4114ecfded4f5765ea37d106f7d0f836b58
epwalsh <petew@allenai.org>
2025-02-08 12:20:54 -0800

    start test for CE loss

--- src/olmo_core/nn/cross_entropy_loss.py
+++ src/olmo_core/nn/cross_entropy_loss.py
@@ -4,8 +4,12 @@ from typing import Literal, Optional, Tuple
 import torch
 import torch.nn as nn
 from torch.distributed import DeviceMesh
-from torch.distributed.tensor import Placement, Shard
-from torch.distributed.tensor.parallel import PrepareModuleInput, parallelize_module
+from torch.distributed.tensor import Placement, Replicate, Shard
+from torch.distributed.tensor.parallel import (
+    PrepareModuleInput,
+    PrepareModuleOutput,
+    parallelize_module,
+)
 
 from .functional import cross_entropy_loss, fused_cross_entropy_loss
 
@@ -27,12 +31,17 @@ class CrossEntropyLoss(nn.Module):
         self.reduction: Literal["mean", "sum", "none"] = reduction
         self.z_loss_multiplier = z_loss_multiplier
         self.base_loss_fn = fused_cross_entropy_loss if fused else cross_entropy_loss
+        self._tp_enabled: bool = False
         if compile:
             if torch.cuda.is_available():
                 log.info("Compiling loss function...")
                 self.base_loss_fn = torch.compile(self.base_loss_fn)
             else:
                 log.warning("Skipping loss compilation since CUDA is not available")
+
+    @property
+    def tp_enabled(self) -> bool:
+        return self._tp_enabled
 
     def forward(
         self,
@@ -62,6 +71,10 @@ class CrossEntropyLoss(nn.Module):
             ce_loss = ce_loss.view(logits.shape[:-1])
             if z_loss is not None:
                 z_loss = z_loss.view(logits.shape[:-1])
+        elif self.tp_enabled:
+            ce_loss = ce_loss.unsqueeze(0)
+            if z_loss is not None:
+                z_loss = z_loss.unsqueeze(0)
 
         return ce_loss, z_loss
 
@@ -70,8 +83,8 @@ class CrossEntropyLoss(nn.Module):
         tp_mesh: DeviceMesh,
         input_layout: Optional[Placement] = None,
         shard_dimension: int = 1,
-        #  output_layout: Optional[Placement] = None,
-        #  use_local_output: bool = True,
+        output_layout: Optional[Placement] = None,
+        use_local_output: bool = True,
     ):
         if self.reduction == "none":
             raise NotImplementedError(self.reduction)
@@ -86,19 +99,22 @@ class CrossEntropyLoss(nn.Module):
             ),
         )
 
-        #  output_layout = output_layout or Replicate()
-        #  parallelize_module(
-        #      self,
-        #      device_mesh=tp_mesh,
-        #      parallelize_plan=PrepareModuleOutput(
-        #          output_layouts=(  # type: ignore
-        #              Shard(0),
-        #              None if self.z_loss_multiplier is None else Shard(0),
-        #          ),
-        #          desired_output_layouts=(  # type: ignore
-        #              output_layout,
-        #              None if self.z_loss_multiplier is None else output_layout,
-        #          ),
-        #          use_local_output=use_local_output,
-        #      ),
-        #  )
+        expected_output_layout = Shard(shard_dimension) if self.reduction == "none" else Shard(0)
+        desired_output_layout = output_layout or Replicate()
+        parallelize_module(
+            self,
+            device_mesh=tp_mesh,
+            parallelize_plan=PrepareModuleOutput(
+                output_layouts=(  # type: ignore
+                    expected_output_layout,
+                    None if self.z_loss_multiplier is None else expected_output_layout,
+                ),
+                desired_output_layouts=(  # type: ignore
+                    desired_output_layout,
+                    None if self.z_loss_multiplier is None else desired_output_layout,
+                ),
+                use_local_output=use_local_output,
+            ),
+        )
+
+        self._tp_enabled = True
---
+++ src/test/nn/cross_entropy_loss_test.py
@@ -0,0 +1,53 @@
+from typing import Literal, Optional
+
+import pytest
+import torch
+
+from olmo_core.nn.cross_entropy_loss import CrossEntropyLoss
+
+from ..distributed.utils import requires_multi_gpu
+
+
+@pytest.mark.parametrize(
+    "fused, compile, reduction",
+    [
+        (pytest.param(False, id="default"), pytest.param(False, id="no-compile"), "sum"),
+    ],
+)
+@requires_multi_gpu
+def test_cross_entropy_loss_parallel(
+    fused: bool,
+    compile: bool,
+    reduction: Literal["sum", "mean", "none"],
+    z_loss_multiplier: Optional[float] = None,
+):
+    loss_fn = CrossEntropyLoss(
+        reduction=reduction, compile=compile, fused=fused, z_loss_multiplier=z_loss_multiplier
+    )
+
+    B, S, D = 4, 16, 64
+    input_ids = torch.randint(0, 256, (B, S), device="cuda")
+    logits = torch.randn(B, S, D, device="cuda", requires_grad=True)
+    labels = input_ids.clone()[..., 1:].contiguous()
+    labels[0][2] = -100
+    labels[2][9] = -100
+    labels[3][12] = -100
+
+    batch_num_tokens_for_loss = (labels != -100).sum()
+    ce_loss, z_loss = loss_fn(logits, labels)
+    ce_loss.div_(batch_num_tokens_for_loss)
+    if z_loss is not None:
+        z_loss.div_(batch_num_tokens_for_loss)
+
+    loss = ce_loss
+    if z_loss is not None:
+        loss += z_loss
+
+    if reduction != "none":
+        assert loss.shape == tuple()
+    else:
+        assert loss.shape == (B, S - 1)
+
+    # Trigger backward pass.
+    loss.backward()
+    assert logits.grad is not None
