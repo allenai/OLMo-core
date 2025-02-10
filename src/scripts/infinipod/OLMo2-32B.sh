4fc909ea0b5ea0526792ba1b29816eae63f7ce47
Shane A <shanea@allenai.org>
2025-02-10 15:43:50 -0800

    First attempt at 32B setup for Infinipod

---
+++ src/scripts/infinipod/OLMo2-32B.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+
+BASE_RUN_NAME=$1
+shift
+
+# Tell OLMo all ranks DO share the same filesystem for checkpoints.
+export OLMO_SHARED_FS=1
+# # Tell OLMo all ranks do NOT share the same filesystem for checkpoints.
+# unset OLMO_SHARED_FS
+
+# Job details
+RUN_NAME=${BASE_RUN_NAME}-$(date -u +"%Y%m%d_%H%M%S")
+SAVE_FOLDER=/mnt/checkpoints/shanea/checkpoints/OLMo-large/$RUN_NAME
+mkdir -p $SAVE_FOLDER
+
+torchrun \
+  --nnodes $SLURM_NNODES \
+  --nproc-per-node $SLURM_GPUS_PER_NODE \
+  --rdzv_id $SLURM_JOB_ID \
+  --node_rank $SLURM_PROCID \
+  --rdzv_backend c10d \
+  --rdzv_endpoint $MASTER_ADDR:$MASTER_PORT \
+  src/scripts/train/OLMo2-32B.py $RUN_NAME ${@}
---
+++ src/scripts/infinipod/run_with_environment.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+
+CONDA_ENV=$1
+shift
+
+# Redirect stdout and stderr so that we get a prefix with the node name
+export NODENAME=$(hostname -s)
+exec > >(trap "" INT TERM; sed -u "s/^/$NODENAME:$SLURM_LOCALID out: /")
+exec 2> >(trap "" INT TERM; sed -u "s/^/$NODENAME:$SLURM_LOCALID err: /" >&2)
+
+# Read secrets from env file
+set -a
+set +x
+source /home/common/shanea/.env
+set -x
+set +a
+
+# Set up conda
+eval "$(conda shell.bash hook)"
+conda activate $CONDA_ENV
+pip freeze
+
+# Infinipod specific environment
+export NCCL_FASTRAK_LLCM_DEVICE_DIRECTORY="/dev/aperture_devices"
+NCCL_LIB_DIR="/usr/local/nvidia/lib64" source /usr/local/nvidia/lib64/nccl-env-profile.sh
+export LD_LIBRARY_PATH=/usr/local/nvidia/lib64:/usr/lib/x86_64-linux-gnu:"$LD_LIBRARY_PATH"
+export NCCL_SHIMNET_SHIM_LAYERS="unused"
+export NCCL_TUNER_PLUGIN="none"
+export NVIDIA_PYTORCH_VERSION=24.03
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+# Setup for multi-node
+MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$MASTER_ADDR
+export MASTER_PORT=34126 # This can be any free port
+
+# Force processes to synchronize at init_process_group
+export TORCH_DIST_INIT_BARRIER=1
+# Better error handling from Python
+export PYTHONFAULTHANDLER=1
+
+exec ${@}
