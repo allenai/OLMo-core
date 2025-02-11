ec1581979f03c4da77e2aab86c92d91bac3d815f
Shane A <shanea@allenai.org>
2025-02-11 15:12:08 -0800

    Try 32B with checkpoint loading

--- src/scripts/infinipod/OLMo2-32B.sh
+++ src/scripts/infinipod/OLMo2-32B.sh
@@ -17,7 +17,7 @@ unset OLMO_SHARED_FS
 
 # Job details
 RUN_NAME=${BASE_RUN_NAME}-$(date -u +"%Y%m%d_%H%M%S")
-SAVE_FOLDER=gs://infinipod-checkpoints/shanea/checkpoints/OLMo-large/$BASE_RUN_NAME
+SAVE_FOLDER=gs://infinipod-checkpoints/shanea/checkpoints/peteish32/$BASE_RUN_NAME
 mkdir -p $SAVE_FOLDER
 
 torchrun \
