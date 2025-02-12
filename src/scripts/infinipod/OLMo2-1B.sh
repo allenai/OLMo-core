a658ec3b37dfc3c43e4e8f1c72135e1bed957d48
Shane A <shanea@allenai.org>
2025-02-12 11:20:04 -0800

    whoami test

--- src/scripts/infinipod/OLMo2-1B.sh
+++ src/scripts/infinipod/OLMo2-1B.sh
@@ -18,6 +18,8 @@ unset OLMO_SHARED_FS
 # Change triton locatin
 export TRITON_HOME=/mnt/localdisk
 
+whoami
+
 # Job details
 RUN_NAME=${BASE_RUN_NAME}-$(date -u +"%Y%m%d_%H%M%S")
 SAVE_FOLDER=gs://infinipod-checkpoints/shanea/checkpoints/OLMo-small/$BASE_RUN_NAME
