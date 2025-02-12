b0a446038a23adca9e6ff65a64a25bb5d9a34f10
Dustin <drschwenk@gmail.com>
2025-02-11 17:00:46 -0800

    change beaker image

--- src/olmo_core/launch/beaker.py
+++ src/olmo_core/launch/beaker.py
@@ -57,7 +57,7 @@ class OLMoCoreBeakerImage(StrEnum):
     includes *versioned* images that are published with each release of the OLMo-core package.
     """
 
-    stable = "olmo-core-tch260cu124"
+    stable = "petew/olmo-core-tch260cu124-v1.8.0"
     """
     Built with the latest compatible stable version of PyTorch.
     """
