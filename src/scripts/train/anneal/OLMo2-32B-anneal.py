9f766755645f5db2246184fb86c451fa0c3d83c0
Shane A <shanea@allenai.org>
2025-02-16 18:19:25 -0800

    Anneal 32B with microbatch size 2

--- src/scripts/train/anneal/OLMo2-32B-anneal.py
+++ src/scripts/train/anneal/OLMo2-32B-anneal.py
@@ -192,7 +192,7 @@ class AnnealingConfig(Config):
             trainer=TrainerConfig(
                 save_folder=f"gs://ai2-llm/checkpoints/peteish32-anneal/{run_name}",
                 load_strategy=LoadStrategy.always,
-                rank_microbatch_size=4 * 4096,  # NOTE: again this is specified in tokens.
+                rank_microbatch_size=2 * 4096,  # NOTE: again this is specified in tokens.
                 checkpointer=CheckpointerConfig(
                     save_thread_count=1, load_thread_count=32, throttle_uploads=True
                 ),
