388fa2969bae4277ef00ef5e9f7aea82184387ba
epwalsh <petew@allenai.org>
2025-02-19 13:16:31 -0800

    Add experimental big MoE config

--- src/olmo_core/nn/transformer/config.py
+++ src/olmo_core/nn/transformer/config.py
@@ -370,6 +370,30 @@ class TransformerConfig(Config):
         )
 
     @classmethod
+    def olmoe2_large(cls, vocab_size: int, **kwargs) -> "TransformerConfig":
+        d_model = kwargs.pop("d_model", 4096)
+        return cls.llama_like(
+            d_model=d_model,
+            vocab_size=vocab_size,
+            n_layers=kwargs.pop("n_layers", 32),
+            n_heads=kwargs.pop("n_heads", 32),
+            name=kwargs.pop("name", TransformerType.moe),
+            block_name=kwargs.pop("block_name", TransformerBlockType.moe_reordered_norm),
+            qk_norm=kwargs.pop("qk_norm", True),
+            rope_theta=kwargs.pop("rope_theta", 500_000),
+            layer_norm_eps=1e-6,
+            feed_forward_moe=MoEConfig(
+                name=MoEType.default,
+                num_experts=64,
+                hidden_size=int(0.5 * d_model),
+                router=MoERouterConfig(top_k=8, bias=False),
+                shared_mlp=SharedMLPConfig(hidden_size=d_model * 2, bias=False),
+                lb_loss_weight=0.01,
+                z_loss_weight=0.001,
+            ),
+        )
+
+    @classmethod
     def olmoe_1B_7B(cls, vocab_size: int, **kwargs) -> "TransformerConfig":
         d_model = kwargs.pop("d_model", 2048)
         return cls.llama_like(
