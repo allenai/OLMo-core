5bb480280d1ea074b27c19bf1ca1188baaf40c9d
epwalsh <petew@allenai.org>
2025-02-20 10:52:19 -0800

    run with FSDP

--- src/scripts/train/OLMo2-2x7B.py
+++ src/scripts/train/OLMo2-2x7B.py
@@ -58,7 +58,7 @@ def build_train_module_config(common: CommonComponents) -> TransformerTrainModul
         ),
         compile_model=True,
         dp_config=TransformerDataParallelConfig(
-            name=DataParallelType.hsdp,
+            name=DataParallelType.fsdp,
             param_dtype=DType.bfloat16,
             reduce_dtype=DType.float32,
             wrapping_strategy=TransformerDataParallelWrappingStrategy.fine_grained,
