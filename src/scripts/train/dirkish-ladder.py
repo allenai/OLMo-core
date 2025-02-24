d10ee8a006f3db8754f3a7cfac99d59eac16dd0c
Dirk Groeneveld <dirkg@allenai.org>
2025-02-23 22:00:10 -0800

    Monkey patch the cmd

--- src/scripts/train/dirkish-ladder.py
+++ src/scripts/train/dirkish-ladder.py
@@ -172,6 +172,11 @@ $ [i]python {sys.argv[0]} {SubCmd.launch} 1B 1xC ai2/pluto-cirrascale --launch.n
 
     # Build run config.
     config = build_config(ladder, script, size, run_duration, cmd, cluster, overrides)
+
+    # monkey patch the cmd
+    script, cmd, size, run_duration, cluster, *overrides = config.launch.cmd
+    config.launch.cmd = [script, cmd, name, size, run_duration, cluster, *overrides]
+
     config.ladder.validate()
 
     # Run the cmd.
