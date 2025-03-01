1e275e13dbd48c1f09a968064a682f2352bda543
Dirk Groeneveld <dirkg@allenai.org>
2025-02-28 22:32:53 -0800

    Does it need this path?

--- src/scripts/train/lc_cont_train/OLMo2-32B-lc_p1.py
+++ src/scripts/train/lc_cont_train/OLMo2-32B-lc_p1.py
@@ -309,7 +309,7 @@ $ [i]python {sys.argv[0]} launch run01  --launch.num_nodes=2[/]
         script=script,
         cmd="train",
         run_name=run_name,
-        load_path="checkpoints/peteish32-anneal/peteish32-from716000-peteish32-from716000-100Banneal-soup/step11921-olmocore",
+        load_path="checkpoints/peteish32-anneal/peteish32-from716000-peteish32-from716000-100Banneal-soup/step11921-olmocore/model_and_optim",
         cluster=cluster,
         overrides=overrides,
     )
