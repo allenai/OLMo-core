5e725997dc05f5ad0fadc3a095c3e0c1d9988bb4
epwalsh <petew@allenai.org>
2025-03-09 20:18:50 -0700

    clean up

--- src/olmo_core/distributed/parallel/pipeline_parallel.py
+++ src/olmo_core/distributed/parallel/pipeline_parallel.py
@@ -196,8 +196,5 @@ class PipelineSchedule:
         else:
             target = None
 
-        if not self.has_first_stage:
-            args = tuple()
-
         output = self.base_schedule.step(*args, target=target, losses=losses, **kwargs)
         return output, None if losses is None else torch.stack(losses)
