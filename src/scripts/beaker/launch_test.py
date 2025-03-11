27cfcce65676d8e40d21848352fe5909a807141d
epwalsh <petew@allenai.org>
2025-03-11 13:56:27 -0700

    try this

--- src/scripts/beaker/launch_test.py
+++ src/scripts/beaker/launch_test.py
@@ -32,7 +32,7 @@ def build_config(command: List[str], overrides: List[str]) -> BeakerLaunchConfig
             # Submodule must be cloned
             "git clone --recursive https://github.com/deepseek-ai/DeepGEMM.git .",
             # Make symbolic links for third-party (CUTLASS and CuTe) include directories
-            "python setup.py develop",
+            # "python setup.py develop",
             # Test JIT compilation
             # "python tests/test_jit.py",
             # Test all GEMM implements (normal, contiguous-grouped and masked-grouped)
