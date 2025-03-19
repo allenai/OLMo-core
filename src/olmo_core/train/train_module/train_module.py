d1007b0a9c1a71a876f71cb0112af1f37154153b
Shane A <shanea@allenai.org>
2025-03-19 13:06:55 -0700

    Allow train modules to not load/save optimizer state

--- src/olmo_core/train/train_module/train_module.py
+++ src/olmo_core/train/train_module/train_module.py
@@ -122,26 +122,32 @@ class TrainModule(Stateful, metaclass=ABCMeta):
         """
 
     @abstractmethod
-    def state_dict(self) -> Dict[str, Any]:
+    def state_dict(self, *, optim: bool = True) -> Dict[str, Any]:
         """
         Get the state dict to save or load.
+
+        :param optim: If set to ``False``, optimizer state is not returned in the state dict.
         """
         raise NotImplementedError
 
-    def state_dict_to_save(self) -> Dict[str, Any]:
+    def state_dict_to_save(self, *, optim: bool = True) -> Dict[str, Any]:
         """
         Can be overridden if the state dict to save should be different from the state dict to load.
         By default just returns :func:`state_dict()`.
-        """
-        return self.state_dict()
 
-    def state_dict_to_load(self, metadata: Metadata) -> Dict[str, Any]:
+        :param optim: If set to ``False``, optimizer state is not returned in the state dict.
+        """
+        return self.state_dict(optim=optim)
+
+    def state_dict_to_load(self, metadata: Metadata, *, optim: bool = True) -> Dict[str, Any]:
         """
         Can be overridden if the state dict to load should be different from the state dict to save.
         By default just returns :func:`state_dict()`.
+
+        :param optim: If set to ``False``, optimizer state is not returned in the state dict.
         """
         del metadata
-        return self.state_dict()
+        return self.state_dict(optim=optim)
 
     @abstractmethod
     def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
