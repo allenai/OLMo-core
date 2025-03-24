4d3fb89376c051bebb8422e87f0f378d696ba63e
epwalsh <petew@allenai.org>
2025-03-24 15:40:28 -0700

    lower mbz again

--- src/scripts/train/OLMoE2-medium.py
+++ src/scripts/train/OLMoE2-medium.py
@@ -46,7 +46,7 @@ def build_model_config(common: CommonComponents) -> TransformerConfig:
 
 def build_train_module_config(common: CommonComponents) -> TransformerTrainModuleConfig:
     return TransformerTrainModuleConfig(
-        rank_microbatch_size=4 * 4096,
+        rank_microbatch_size=2 * 4096,
         max_sequence_length=common.dataset.effective_sequence_length,
         optim=AdamWConfig(
             lr=3e-4,
