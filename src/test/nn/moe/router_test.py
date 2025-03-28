976ec570c3293f74df51e1767a3cac78726d8df7
epwalsh <petew@allenai.org>
2025-03-28 12:25:14 -0700

    add test

--- src/test/nn/moe/router_test.py
+++ src/test/nn/moe/router_test.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from olmo_core.nn.moe.router import MoELinearRouter
+from olmo_core.nn.moe.router import MoELinearRouter, MoERouterGatingFunction
 
 from ...utils import DEVICES
 
@@ -9,9 +9,21 @@ from ...utils import DEVICES
 @pytest.mark.parametrize("device", DEVICES)
 @pytest.mark.parametrize(
     "uniform_expert_assignment",
-    [pytest.param(True, id="uniform"), pytest.param(False, id="computed")],
+    [
+        pytest.param(True, id="uniform"),
+        pytest.param(False, id="computed"),
+    ],
 )
-def test_router(device: torch.device, uniform_expert_assignment: bool):
+@pytest.mark.parametrize(
+    "gating_function",
+    [
+        pytest.param(MoERouterGatingFunction.softmax, id="softmax"),
+        pytest.param(MoERouterGatingFunction.sigmoid, id="sigmoid"),
+    ],
+)
+def test_router(
+    device: torch.device, uniform_expert_assignment: bool, gating_function: MoERouterGatingFunction
+):
     router = MoELinearRouter(
         d_model=128,
         num_experts=4,
@@ -19,15 +31,22 @@ def test_router(device: torch.device, uniform_expert_assignment: bool):
         top_k=2,
         normalize_expert_weights=True,
         uniform_expert_assignment=uniform_expert_assignment,
+        gating_function=gating_function,
     ).to(device)
+
     x = torch.randn((2, 4, 128), device=device)
     logits, scores, weights, indices, bz_per_expert, batched_bz_per_expert = router(x)
+
     assert logits.shape == (2, 4, 4)
     assert scores.shape == (2, 4, 4)
     assert weights.shape == (2, 4, 2)
     assert indices.shape == (2, 4, 2)
     assert bz_per_expert.shape == (4,)
     assert batched_bz_per_expert.shape == (2, 4)
+
+    # Scores should be normalized, otherwise LB loss doesn't work.
+    scores_total = scores.sum(dim=-1)
+    torch.testing.assert_close(scores_total, torch.ones_like(scores_total))
 
 
 @pytest.mark.parametrize("device", DEVICES)
