b1bb8939920b22f653e947744d68e6dfc318b599
David Heineman <david@davidheineman.com>
2025-04-02 00:13:13 -0700

    seed 4932

--- src/olmo_core/model_ladder.py
+++ src/olmo_core/model_ladder.py
@@ -173,7 +173,8 @@ class ModelLadder(Config, metaclass=ABCMeta):
     Get the tokenizer config to use throughput the ladder.
     """
 
-    init_seed: int = 2352
+    # init_seed: int = 2352
+    init_seed: int = 4932
     """
     The seed to use when first initializing RNG states.
     """
