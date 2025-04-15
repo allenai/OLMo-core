74e2d5709014d8bc049954b41638a9fd3f04e305
Dustin <drschwenk@gmail.com>
2025-04-15 16:33:30 -0700

    take "lc anneal" to 50B tokens

--- src/scripts/train/lc_cont_train/OLMo2-7B-lc_anneal.py
+++ src/scripts/train/lc_cont_train/OLMo2-7B-lc_anneal.py
@@ -205,7 +205,7 @@ class LcContTrain(Config):
                 load_path=load_path,
                 metrics_collect_interval=10,
                 cancel_check_interval=10,
-                max_duration=Duration.tokens(int(20e9)),
+                max_duration=Duration.tokens(int(50e9)),
             )
             .with_callback(
                 "checkpointer",
