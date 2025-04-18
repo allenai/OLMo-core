cbdc8ba1464b741678a1edb80baeefe139cbcad4
Dustin <drschwenk@gmail.com>
2025-04-17 23:23:59 -0700

    add comma

--- src/scripts/train/lc_cont_train/OLMo2-7B-lc_anneal_cp.py
+++ src/scripts/train/lc_cont_train/OLMo2-7B-lc_anneal_cp.py
@@ -373,7 +373,7 @@ $ [i]python {sys.argv[0]} launch run01  --launch.num_nodes=2[/]
         cmd="train",
         run_name=run_name,
         # load_path="gs://ai2-llm/checkpoints/dustins/OLMo-2-1124-7B_pre_anneal_oc/",
-        load_path="gs://ai2-llm/checkpoints/shanea/OLMo-medium/peteish7/step928646/model_and_optim/"
+        load_path="gs://ai2-llm/checkpoints/shanea/OLMo-medium/peteish7/step928646/model_and_optim/",
         cluster=cluster,
         overrides=overrides,
     )
