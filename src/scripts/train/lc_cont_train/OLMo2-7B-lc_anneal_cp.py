38a1f8aaa488b1739db357fc837b82a4e6a59d96
Shane A <shanea@allenai.org>
2025-04-21 19:21:54 -0700

    Revert "Unfuse compiler"
    This reverts commit 1806d0e88221d11da4b3f32ca2c559dd8b4f3329.

--- src/scripts/train/lc_cont_train/OLMo2-7B-lc_anneal_cp.py
+++ src/scripts/train/lc_cont_train/OLMo2-7B-lc_anneal_cp.py
@@ -152,7 +152,7 @@ class LcContTrain(Config):
                         OptimGroupOverride(params=["embeddings.weight"], opts=dict(weight_decay=0.0))
                     ],
                     compile=True,
-                    fused=False,
+                    fused=True,
                 ),
                 max_sequence_length=CONTEXT_LENGTH,
                 compile_model=True,
