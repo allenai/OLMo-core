b61c38dd3697486007d0e0b26953399532d96be1
Shane A <shanea@allenai.org>
2025-04-21 22:39:25 -0700

    Enable async TP

--- src/scripts/train/lc_cont_train/OLMo2-7B-lc_anneal_cp.py
+++ src/scripts/train/lc_cont_train/OLMo2-7B-lc_anneal_cp.py
@@ -164,6 +164,7 @@ class LcContTrain(Config):
                 ),
                 tp_config=TransformerTensorParallelConfig(
                     degree=8,
+                    enable_async=True,
                     # loss_parallel=True,
                 ),
                 # cp_config=TransformerContextParallelConfig.llama3(degree=CP_DEGREE)
