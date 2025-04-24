602898986a62d2114731770bb8aa6ebc63f700a6
Shane A <shanea@allenai.org>
2025-04-24 15:07:17 -0700

    Switch to smaller downstream eval task list

--- src/olmo_core/model_ladder.py
+++ src/olmo_core/model_ladder.py
@@ -405,6 +405,43 @@ class ModelLadder(Config, metaclass=ABCMeta):
                 f"Size {size} not supported by this ladder (supported sizes: {self.SUPPORTED_MODEL_SIZES})."
             )
 
+        # For training runs where we don't expect the model to acquire MC (e.g., 1B-5xC, short 7B training runs)
+        tasks = [
+            # OLMES Core 9(-ish) RC
+            "arc_challenge_test_rc_5shot",
+            "arc_easy_test_rc_5shot",
+            "hellaswag_rc_5shot",  # 1K subset of HellaSwag
+            "winogrande_val_rc_5shot",  # Helpful after 750M-5xC scale
+            "csqa_val_rc_5shot",
+            "piqa_val_rc_5shot",
+            "socialiqa_val_rc_5shot",
+            # Too noisy to be worth tracking
+            # "boolq_val_rc_5shot",
+            # "openbookqa_test_rc_5shot",
+            # MMLU RC
+            "mmlu_stem_val_rc_5shot",
+            "mmlu_humanities_val_rc_5shot",
+            "mmlu_social_sciences_val_rc_5shot",
+            "mmlu_other_val_rc_5shot",
+            "mmlu_stem_test_rc_5shot",
+            "mmlu_humanities_test_rc_5shot",
+            "mmlu_social_sciences_test_rc_5shot",
+            "mmlu_other_test_rc_5shot",
+            # Gen tasks BPB
+            "gsm8k_gold_bpb_5shot",
+            "minerva_math_algebra_gold_bpb_0shot",
+            "minerva_math_counting_and_probability_gold_bpb_0shot",
+            "minerva_math_geometry_gold_bpb_0shot",
+            "minerva_math_intermediate_algebra_gold_bpb_0shot",
+            "minerva_math_number_theory_gold_bpb_0shot",
+            "minerva_math_prealgebra_gold_bpb_0shot",
+            "minerva_math_precalculus_gold_bpb_0shot",
+            "codex_humaneval_gold_bpb_0shot",
+            "codex_mbpp_gold_bpb_0shot",
+            # Sanity check for MCQA ability
+            "copycolors_10way",
+        ]
+
         config = TrainerConfig(
             save_folder=self.get_save_folder(size, run_duration),
             metrics_collect_interval=10,
@@ -432,7 +469,7 @@ class ModelLadder(Config, metaclass=ABCMeta):
         config = config.with_callback(
             "downstream_evaluator",
             DownstreamEvaluatorCallbackConfig(
-                tasks=[task for task in list_tasks() if "_mc" not in task and "_var" not in task],
+                tasks=tasks,
                 tokenizer=self.tokenizer,
                 eval_interval=500,
             ),
