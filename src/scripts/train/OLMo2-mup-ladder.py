501262fa695b7eeb03053709d9a83055271895f1
Shane A <shanea@allenai.org>
2025-04-24 14:40:13 -0700

    Change comet project to OLMo2-mup-ladder

--- src/scripts/train/OLMo2-mup-ladder.py
+++ src/scripts/train/OLMo2-mup-ladder.py
@@ -148,7 +148,7 @@ def build_ladder(root_dir: str) -> BaselineModelLadder:
         save_folder = str(join_path(root_dir, "checkpoints/ladder"))
     return BaselineModelLadder(
         name="OLMo2",
-        project="OLMo2-model-ladder",
+        project="OLMo2-mup-ladder",
         mix_base_dir=root_dir,
         work_dir=get_work_dir(root_dir),
         save_folder=save_folder,
