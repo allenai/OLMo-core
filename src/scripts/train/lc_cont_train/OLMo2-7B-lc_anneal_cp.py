13d9c93f399c3f457f66550bbf21ee54873f729b
Dustin <drschwenk@gmail.com>
2025-04-25 16:52:04 -0700

    change data mix to 50/50

--- src/scripts/train/lc_cont_train/OLMo2-7B-lc_anneal_cp.py
+++ src/scripts/train/lc_cont_train/OLMo2-7B-lc_anneal_cp.py
@@ -76,7 +76,7 @@ class AnnealingDataMix(DataMixBase):
     name (without the '.txt' extension) below.
     """
 
-    data_mix = "lc67_dolmino33_v1"
+    data_mix = "lc50_dolmino50_v1"
 
     def build(self, base_dir: str, tokenizer: str) -> Tuple[List[str], List[str]]:
         if not base_dir.endswith("/"):
