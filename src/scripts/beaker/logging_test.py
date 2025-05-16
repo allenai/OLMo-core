ac97b4c88a2370c5769484e8ab5b3659616e07fb
epwalsh <petew@allenai.org>
2025-05-16 13:06:06 -0700

    fix

--- src/scripts/beaker/logging_test.py
+++ src/scripts/beaker/logging_test.py
@@ -9,8 +9,8 @@ def main():
     print("Starting test...")
     log.debug("Debug message!")
     log.info("Info message!")
-    log.info("Warning message!")
-    log.info("Error message!")
+    log.warning("Warning message!")
+    log.error("Error message!")
     print("End test. If you didn't see any log messages, something is wrong.")
 
 
