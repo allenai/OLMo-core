a591ecde511a61473e922866db8a1ea362ddd051
aman-17 <amanrangapur@gmail.com>
2025-06-12 15:17:56 -0700

    reduced batch_size

--- src/scripts/train/OLMo2-1B.py
+++ src/scripts/train/OLMo2-1B.py
@@ -16,7 +16,7 @@ from olmo_core.train.train_module import (
 )
 
 SEQUENCE_LENGTH = 16384  
-GLOBAL_BATCH_SIZE = 128 * SEQUENCE_LENGTH  
+GLOBAL_BATCH_SIZE = 32 * SEQUENCE_LENGTH  
 MAX_DURATION = int(4e12)  
 
 def build_model_config(common: CommonComponents) -> TransformerConfig:
@@ -33,7 +33,7 @@ def build_model_config(common: CommonComponents) -> TransformerConfig:
 
 
 def build_train_module_config(common: CommonComponents) -> TransformerTrainModuleConfig:
-    rank_microbatch_size = 8 * SEQUENCE_LENGTH
+    rank_microbatch_size = 2 * SEQUENCE_LENGTH
     if common.launch is not None:
         gpus = {CLUSTER_TO_GPU_TYPE.get(c, "unknown") for c in common.launch.clusters}
         if all("B200" in g for g in gpus):
