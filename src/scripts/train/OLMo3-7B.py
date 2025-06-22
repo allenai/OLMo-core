85a2ee927e68f0ac3f68f86fafdc3700d2e125a5
Akshita Bhagia <akshita23bhagia@gmail.com>
2025-06-21 17:56:08 -0700

    hack to double gpus but retain batch size warmup

--- src/scripts/train/OLMo3-7B.py
+++ src/scripts/train/OLMo3-7B.py
@@ -2,6 +2,7 @@
 Train a 7B OLMo model. Run this script without any arguments to see usage info.
 """
 from datetime import datetime
+import math
 
 from olmo_core.config import DType
 from olmo_core.distributed.parallel import DataParallelType
@@ -34,7 +35,7 @@ MAX_DURATION = int(
     10e12
 )  # Setting this higher than 6T (expected run time), in case we get to run longer since 1) we're using WSD and 2) our anneal will use different data
 ANNEAL_TOKENS = int(100e9)
-LR = 4.4e-5  # Based on 6T tokens with 100B anneal, don't forget to adjust when max duration or anneal length changes.
+LR = 4.4e-5  * math.sqrt(2) # Based on 6T tokens with 100B anneal, don't forget to adjust when max duration or anneal length changes.
 EVAL_INTERVAL = 1000
 
 
@@ -160,13 +161,13 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
     # batch size warmup
     config.callbacks["batchwup"] = BatchSizeSchedulerCallback(
         batch_sizes=[
-            GLOBAL_BATCH_SIZE,
+            # GLOBAL_BATCH_SIZE,
             GLOBAL_BATCH_SIZE * 2,
             GLOBAL_BATCH_SIZE * 4,
         ],
         schedule=[
             Duration.tokens(0),
-            Duration.tokens(167_772_160_000),
+            # Duration.tokens(167_772_160_000),
             Duration.tokens(503_316_480_000),
         ],
         enabled=True,
