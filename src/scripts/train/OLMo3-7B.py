2d8e0f964df2fae97bf004875550e8c3ef3a9e2c
Akshita Bhagia <akshita23bhagia@gmail.com>
2025-06-22 20:53:48 -0700

    hack after 2nd batch size increase

--- src/scripts/train/OLMo3-7B.py
+++ src/scripts/train/OLMo3-7B.py
@@ -35,7 +35,7 @@ MAX_DURATION = int(
     10e12
 )  # Setting this higher than 6T (expected run time), in case we get to run longer since 1) we're using WSD and 2) our anneal will use different data
 ANNEAL_TOKENS = int(100e9)
-LR = 4.4e-5  * math.sqrt(2) # Based on 6T tokens with 100B anneal, don't forget to adjust when max duration or anneal length changes.
+LR = 4.4e-5  * math.sqrt(4) # Based on 6T tokens with 100B anneal, don't forget to adjust when max duration or anneal length changes.
 EVAL_INTERVAL = 1000
 
 
@@ -162,13 +162,13 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
     config.callbacks["batchwup"] = BatchSizeSchedulerCallback(
         batch_sizes=[
             # GLOBAL_BATCH_SIZE,
-            GLOBAL_BATCH_SIZE * 2,
+            # GLOBAL_BATCH_SIZE * 2,
             GLOBAL_BATCH_SIZE * 4,
         ],
         schedule=[
             Duration.tokens(0),
             # Duration.tokens(167_772_160_000),
-            Duration.tokens(503_316_480_000),
+            # Duration.tokens(503_316_480_000),
         ],
         enabled=True,
     )
