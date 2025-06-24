e4478fcb29b0721fb8a7df10f74d3d9efa8ddf06
Shane A <shanea@allenai.org>
2025-06-24 14:17:58 -0700

    Add sliding window conversion to HF config

--- src/olmo_core/nn/hf/config.py
+++ src/olmo_core/nn/hf/config.py
@@ -1,3 +1,5 @@
+import typing
+
 from transformers import Olmo2Config, PretrainedConfig
 
 from olmo_core.doc_utils import beta_feature
@@ -17,17 +19,18 @@ def get_hf_config(model: Transformer) -> PretrainedConfig:
             f"Building HF config not implemented for {model.__class__.__name__}"
         )
 
-    block = next(iter(model.blocks.values()))
-    if not isinstance(block, ReorderedNormTransformerBlock):
+    blocks = [block for _, block in sorted(model.blocks.items(), key=lambda item: int(item[0]))]
+    if not isinstance(blocks[0], ReorderedNormTransformerBlock):
         raise NotImplementedError(
             f"Block is not a {ReorderedNormTransformerBlock.__name__}, unable to build HF config for {model.__class__.__name__}"
         )
+    blocks = typing.cast(list[ReorderedNormTransformerBlock], blocks)
 
-    if not isinstance(block.attention, Attention):
+    if not isinstance(blocks[0].attention, Attention):
         raise NotImplementedError(
             f"Attention is not a {Attention.__name__}, unable to build HF config for {model.__class__.__name__}"
         )
-    if block.attention.rope is None:
+    if blocks[0].attention.rope is None:
         raise NotImplementedError(
             f"Attention does not use rope, unable to build HF config for {model.__class__.__name__}"
         )
@@ -35,17 +38,22 @@ def get_hf_config(model: Transformer) -> PretrainedConfig:
     return Olmo2Config(
         vocab_size=model.vocab_size,
         hidden_size=model.d_model,
-        intermediate_size=block.feed_forward.hidden_size,
+        intermediate_size=blocks[0].feed_forward.hidden_size,
         num_hidden_layers=model.n_layers,
-        num_attention_heads=block.attention.n_heads,
-        num_key_value_heads=block.attention.n_kv_heads,
+        num_attention_heads=blocks[0].attention.n_heads,
+        num_key_value_heads=blocks[0].attention.n_kv_heads,
         hidden_act="silu",
         max_position_embeddings=-1,
-        attention_bias=block.attention.w_out.bias is not None,
-        rope_theta=block.attention.rope.theta,
+        attention_bias=blocks[0].attention.w_out.bias is not None,
+        rope_theta=blocks[0].attention.rope.theta,
         pad_token_id=None,  # type: ignore
         bos_token_id=None,
         eos_token_id=None,  # type: ignore
-        rms_norm_eps=block.feed_forward_norm.eps,
+        rms_norm_eps=blocks[0].feed_forward_norm.eps,
         tie_word_embeddings=False,
+        sliding_window=max(block.attention.window_size[0] for block in blocks),  # type: ignore
+        layer_types=[
+            "sliding_attention" if block.attention.window_size[0] != -1 else "full_attention"  # type: ignore
+            for block in blocks
+        ],
     )
