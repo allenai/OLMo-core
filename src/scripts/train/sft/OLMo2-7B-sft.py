903f069a50c471051e699c41dd5e0239f3cbc89b
jacob-morrison <root@phobos-cs-aus-453.reviz.ai2.in>
2025-07-01 10:53:06 -0700

    comment out field

--- src/scripts/train/sft/OLMo2-7B-sft.py
+++ src/scripts/train/sft/OLMo2-7B-sft.py
@@ -106,7 +106,7 @@ def build_sft_dataset(
         # how to handle long docs?
         name=NumpyDatasetType.padded_fsl, # NumpyDatasetType.packed_fsl,  # concatenated short docs into a single sequence... (see also "padded_fsl")
         generate_doc_lengths=False, # INTRA_DOCUMENT_MASKING,  # ...and mask attention so that they don't attend to each other
-        long_doc_strategy=LongDocStrategy.truncate,  # truncate docs...
+        # long_doc_strategy=LongDocStrategy.truncate,  # truncate docs...
         sequence_length=SEQUENCE_LENGTH,  # ...that are over this length
     )
     return dataset, tokenizer_config
