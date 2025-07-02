ba0f84b28bb12a1e05c63319df74534a72c86781
jacob-morrison <root@phobos-cs-aus-453.reviz.ai2.in>
2025-07-01 22:50:31 -0700

    also padded

--- src/scripts/train/sft/OLMo2-7B-sft.py
+++ src/scripts/train/sft/OLMo2-7B-sft.py
@@ -105,11 +105,11 @@ def build_sft_dataset(
         paths=paths,
         label_mask_paths=label_mask_paths,
         # how to handle long docs?
-        # name=NumpyDatasetType.padded_fsl, # NumpyDatasetType.packed_fsl,  # concatenated short docs into a single sequence... (see also "padded_fsl")
-        # generate_doc_lengths=False, # INTRA_DOCUMENT_MASKING,  # ...and mask attention so that they don't attend to each other
-        name=NumpyDatasetType.packed_fsl,  # concatenated short docs into a single sequence... (see also "padded_fsl")
-        generate_doc_lengths=INTRA_DOCUMENT_MASKING,  # ...and mask attention so that they don't attend to each other
-        long_doc_strategy=LongDocStrategy.truncate,  # truncate docs...
+        name=NumpyDatasetType.padded_fsl, # NumpyDatasetType.packed_fsl,  # concatenated short docs into a single sequence... (see also "padded_fsl")
+        generate_doc_lengths=False, # INTRA_DOCUMENT_MASKING,  # ...and mask attention so that they don't attend to each other
+        #name=NumpyDatasetType.packed_fsl,  # concatenated short docs into a single sequence... (see also "padded_fsl")
+        #generate_doc_lengths=INTRA_DOCUMENT_MASKING,  # ...and mask attention so that they don't attend to each other
+        #long_doc_strategy=LongDocStrategy.truncate,  # truncate docs...
         sequence_length=SEQUENCE_LENGTH,  # ...that are over this length
     )
     return dataset, tokenizer_config
