f0f75a1191a6a5e28e887df7c1f5585b4beec41e
Tyler Romero <tylerr@allenai.org>
2025-07-04 11:39:14 -0700

    fixes

--- src/olmo_core/nn/lm_head.py
+++ src/olmo_core/nn/lm_head.py
@@ -245,6 +245,10 @@ class LMHead(nn.Module):
                 z_loss_multiplier=z_loss_multiplier
                 or 1e-4,  # note, this will take a z_loss_multiplier of 0 and make it 1e-4
             )
+            if z_loss is not None:
+                loss = ce_loss + z_loss
+            else:
+                loss = ce_loss
         elif self.loss_implementation == LMLossImplementation.shared_operations:
             logits = self.w_out(h)
             assert logits is not None
@@ -256,6 +260,10 @@ class LMHead(nn.Module):
                 compute_z_loss=z_loss_multiplier is not None,
                 z_loss_multiplier=z_loss_multiplier or 1e-4,
             )
+            if z_loss is not None:
+                loss = ce_loss + z_loss
+            else:
+                loss = ce_loss
         elif self.loss_implementation == LMLossImplementation.fused_linear:
             logits = None
             loss, z_loss = fused_linear_cross_entropy_loss(
@@ -268,15 +276,14 @@ class LMHead(nn.Module):
                 compute_z_loss=z_loss_multiplier is not None,
                 z_loss_multiplier=z_loss_multiplier or 1e-4,
             )
+            if z_loss is not None:
+                ce_loss = loss - z_loss
+            else:
+                ce_loss = loss
         else:
             raise NotImplementedError(
                 f"'{self.loss_implementation}' loss implementation is not supported by {self.__class__.__name__}"
             )
-
-        if z_loss is not None:
-            ce_loss = loss - z_loss
-        else:
-            ce_loss = loss
 
         if return_logits is False:
             logits = None
