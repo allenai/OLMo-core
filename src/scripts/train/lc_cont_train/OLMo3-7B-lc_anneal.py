08a2ecffdb08643d22475e2af3e875c9d9e1dd89
Dustin <drschwenk@gmail.com>
2025-07-14 13:49:19 -0700

    quick test of ntk by parts

--- src/scripts/train/lc_cont_train/OLMo3-7B-lc_anneal.py
+++ src/scripts/train/lc_cont_train/OLMo3-7B-lc_anneal.py
@@ -310,7 +310,9 @@ $ [i]python {sys.argv[0]} launch run01  --launch.num_nodes=2[/]
     model_config.block.attention.rope = RoPEConfig(
         theta= 0.5 * 10 ** 6,
         # scaling=RoPEScalingConfig()
-        scaling=YaRNRoPEScalingConfig()
+        scaling = RoPEScalingConfig(
+            old_context_len=8192
+        )
     )
 
     # Print the config for debugging and then execute the command.
