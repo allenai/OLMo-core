d4c1ba03ff4ca7f42b82b5995906bc3cf2bcc75d
epwalsh <petew@allenai.org>
2025-07-28 14:42:28 -0700

    fix

--- src/olmo_core/train/callbacks/checkpointer.py
+++ src/olmo_core/train/callbacks/checkpointer.py
@@ -153,14 +153,18 @@ class CheckpointerCallback(Callback):
 
     def _remove_checkpoint(self, path: str):
         log.info(f"Removing old checkpoint at '{path}'...")
+
+        # Remove metadata file first to invalidate the checkpoint.
+        if get_rank() == 0:
+            try:
+                remove_file(join_path(path, self.trainer.checkpointer.METADATA_FNAME))
+            except FileNotFoundError:
+                pass
+
         if is_url(path):
             if get_rank() == 0:
-                # Remove metadata file first to invalidate the checkpoint.
-                remove_file(join_path(path, self.trainer.checkpointer.METADATA_FNAME))
                 self.trainer.run_bookkeeping_op(clear_directory, path)
         elif get_fs_local_rank() == 0:
-            # Remove metadata file first to invalidate the checkpoint.
-            remove_file(join_path(path, self.trainer.checkpointer.METADATA_FNAME))
             self.trainer.run_bookkeeping_op(clear_directory, path)
 
     def _schedule_for_removal(self, path: str):
