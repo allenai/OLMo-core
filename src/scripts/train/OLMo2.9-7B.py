420b44783eca88b0e2d500b78f37d0c178abca22
Dirk Groeneveld <dirkg@allenai.org>
2025-08-01 23:32:34 -0700

    Job died already due to Gloo?

--- src/scripts/train/OLMo2.9-7B.py
+++ src/scripts/train/OLMo2.9-7B.py
@@ -98,8 +98,8 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
             "checkpointer",
             CheckpointerCallback(
                 save_interval=1000,
-                ephemeral_save_interval=None,
-                save_async=True,
+                ephemeral_save_interval=250,
+                save_async=False,
             ),
         )
         .with_callback(
