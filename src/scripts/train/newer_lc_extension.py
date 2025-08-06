f9ec804572a29ffc1f7177f140da16a61d130a1b
Amanda Bertsch <amandab@allenai.org>
2025-08-05 18:51:55 -0700

    swap ckpt

--- src/scripts/train/newer_lc_extension.py
+++ src/scripts/train/newer_lc_extension.py
@@ -292,7 +292,7 @@ $ [i]python {sys.argv[0]} launch run01  --launch.num_nodes=2[/]
         script=script,
         cmd="train",
         run_name=run_name,
-        load_path="gs://ai2-llm/checkpoints/OLMo3-7B-swafix/step289000/model_and_optim/",
+        load_path="gs://ai2-llm/checkpoints/shanea/OLMo3-integrationtest-6/step79605/model_and_optim",
         cluster=cluster,
         overrides=overrides,
     )
