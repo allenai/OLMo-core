Fix MFU Calculation

src/olmo_core/train/callbacks/speed_monitor.py
@@ -1,3 +1,4 @@
+import logging
 import time
 from dataclasses import dataclass
 from typing import Any, ClassVar, Dict, Optional
@@ -9,6 +10,8 @@ from olmo_core.distributed.utils import get_world_size
 from ..common import ReduceType
 from ..train_module import TransformerTrainModule
 from .callback import Callback
+
+log = logging.getLogger(__name__)
 
 
 @dataclass
@@ -68,22 +71,33 @@ class SpeedMonitorCallback(Callback):
             and isinstance(self.trainer.train_module, TransformerTrainModule)
         ):
             device_name = torch.cuda.get_device_name(self.trainer.device)
-            if self.trainer.train_module.autocast_precision == torch.bfloat16:
+
+            # Check if we're using half precision
+            train_module = self.trainer.train_module
+            using_half_precision = train_module.autocast_precision == torch.bfloat16 or (
+                train_module.dp_config is not None
+                and train_module.dp_config.param_dtype == torch.bfloat16
+            )
+            if using_half_precision:
+                dense_correction = 0.5  # listed specs are one-half lower without sparsity
                 if "A100" in device_name:
-                    self.device_peak_flops = int(312e12)
+                    # data from https://www.nvidia.com/content/dam/en-zz/Solutions/Data-Center/a100/pdf/nvidia-a100-datasheet-us-nvidia-1758950-r4-web.pdf
+                    self.device_peak_flops = int(312e12 * dense_correction)
                 elif "H100" in device_name:
                     # data from https://www.nvidia.com/en-us/data-center/h100/
-                    # NOTE: Specifications are one-half lower without sparsity.
+                    # and https://www.megware.com/fileadmin/user_upload/LandingPage%20NVIDIA/nvidia-h100-datasheet.pdf
                     if "NVL" in device_name:
-                        self.device_peak_flops = int(1979e12)
+                        self.device_peak_flops = int(1671e12 * dense_correction)
                     elif "PCIe" in device_name:
-                        self.device_peak_flops = int(756e12)
+                        self.device_peak_flops = int(1513e12 * dense_correction)
                     else:  # for SXM and other variants
-                        self.device_peak_flops = int(989e12)
+                        self.device_peak_flops = int(1979e12 * dense_correction)
                 elif "B200" in device_name:
-                    self.device_peak_flops = int(2200e12)
+                    # data from https://semianalysis.com/2024/04/10/nvidia-blackwell-perf-tco-analysis
+                    self.device_peak_flops = int(4500e12 * dense_correction)
                 else:  # for other GPU types, assume A100
                     self.device_peak_flops = int(312e12)
+            log.info(f"Device: {device_name}, Device peak FLOPS: {self.device_peak_flops}")
 
     def pre_load_batch(self):
         self._batch_load_start = time.perf_counter()