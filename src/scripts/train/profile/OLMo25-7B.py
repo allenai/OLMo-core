dd5593bd8c412818b2048d274fac1f4e49dfdb78
Tyler Romero <tylerr@allenai.org>
2025-08-13 13:06:25 -0700

    add olmo25 profiling config

---
+++ src/scripts/train/profile/OLMo25-7B.py
@@ -0,0 +1,131 @@
+"""
+Train a 7B OLMo2.5 model on long contexts. Configured for profiling.
+Run this script without any arguments to see usage info.
+"""
+
+from olmo_core.config import DType
+from olmo_core.distributed.parallel import DataParallelType
+from olmo_core.float8 import Float8Config
+from olmo_core.internal.common import CLUSTER_TO_GPU_TYPE
+from olmo_core.internal.experiment import CommonComponents, main
+from olmo_core.nn.attention import SlidingWindowAttentionConfig
+from olmo_core.nn.transformer import TransformerConfig
+from olmo_core.optim import OptimGroupOverride, SkipStepAdamWConfig
+from olmo_core.optim.scheduler import CosWithWarmup
+from olmo_core.train import Duration, TrainerConfig
+from olmo_core.train.callbacks import (
+    CheckpointerCallback,
+    WandBCallback,
+)
+from olmo_core.train.callbacks.console_logger import ConsoleLoggerCallback
+from olmo_core.train.callbacks.gpu_memory_monitor import GPUMemoryMonitorCallback
+from olmo_core.train.callbacks.profiler import ProfilerCallback
+from olmo_core.train.train_module import (
+    TransformerDataParallelConfig,
+    TransformerDataParallelWrappingStrategy,
+    TransformerTrainModuleConfig,
+)
+
+SEQUENCE_LENGTH = 8 * 1024
+GLOBAL_BATCH_SIZE = 4 * 1024 * 1024
+
+# 16 forward-backwards per step
+# 64 BS / 16 = 4 DP?
+# Then those 4 DP shards are
+
+
+def build_model_config(common: CommonComponents) -> TransformerConfig:
+    config = TransformerConfig.olmo2_7B(vocab_size=common.tokenizer.padded_vocab_size())
+    config.block.attention.sliding_window = SlidingWindowAttentionConfig(
+        force_full_attention_on_first_layer=False,
+        force_full_attention_on_last_layer=True,
+        pattern=[4096, 4096, 4096, -1],
+    )
+    config.block.attention.use_flash = True
+    return config
+
+
+def build_train_module_config(common: CommonComponents) -> TransformerTrainModuleConfig:
+    rank_microbatch_size = SEQUENCE_LENGTH
+    if common.launch is not None:
+        gpus = {CLUSTER_TO_GPU_TYPE.get(c, "unknown") for c in common.launch.clusters}
+        if all("B200" in g for g in gpus):
+            rank_microbatch_size *= 2
+
+    return TransformerTrainModuleConfig(
+        rank_microbatch_size=rank_microbatch_size,
+        max_sequence_length=common.dataset.effective_sequence_length,
+        optim=SkipStepAdamWConfig(
+            lr=3e-4,
+            weight_decay=0.1,
+            betas=(0.9, 0.95),
+            group_overrides=[
+                OptimGroupOverride(params=["embeddings.weight"], opts=dict(weight_decay=0.0))
+            ],
+        ),
+        compile_model=True,
+        dp_config=TransformerDataParallelConfig(
+            name=DataParallelType.hsdp,
+            param_dtype=DType.bfloat16,
+            reduce_dtype=DType.float32,
+            wrapping_strategy=TransformerDataParallelWrappingStrategy.blocks,
+        ),
+        float8_config=Float8Config(enabled=False),
+        z_loss_multiplier=1e-5,
+        max_grad_norm=1.0,
+        scheduler=CosWithWarmup(warmup_steps=2000),
+    )
+
+
+def build_trainer_config(common: CommonComponents) -> TrainerConfig:
+    return (
+        TrainerConfig(
+            save_folder=common.save_folder,
+            save_overwrite=True,
+            metrics_collect_interval=10,
+            cancel_check_interval=10,
+            max_duration=Duration.steps(22),
+        )
+        .with_callback(
+            "wandb",
+            WandBCallback(
+                name=common.run_name,
+                entity="ai2-llm",
+                project="OLMo-core-7B-profile-results",
+                enabled=False,
+                cancel_check_interval=10,
+            ),
+        )
+        .with_callback(
+            "profiler",
+            ProfilerCallback(
+                enabled=True,
+                skip_first=15,
+                wait=3,
+                warmup=1,
+                active=2,
+                repeat=1,
+                with_stack=True,
+                enable_cuda_sync_events=True,
+                ranks="cp",
+            ),
+        )
+        .with_callback("gpu_monitor", GPUMemoryMonitorCallback())
+        .with_callback(
+            "checkpointer",
+            CheckpointerCallback(enabled=False),  # no need to checkpoint here
+        )
+        .with_callback("console_logger", ConsoleLoggerCallback(metrics_log_interval=4))
+    )
+
+
+if __name__ == "__main__":
+    main(
+        global_batch_size=GLOBAL_BATCH_SIZE,
+        sequence_length=SEQUENCE_LENGTH,
+        model_config_builder=build_model_config,
+        train_module_config_builder=build_train_module_config,
+        trainer_config_builder=build_trainer_config,
+        include_instance_filter=False,  # We use SkipStepOptimizer for this problem.
+        include_default_evals=False,
+    )
