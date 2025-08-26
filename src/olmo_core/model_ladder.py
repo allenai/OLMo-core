352f1f7676e15499bb45a19c734b1aeffd6c32b1
Shane A <shanea@allenai.org>
2025-08-26 15:30:33 -0700

    Switch back to comet temporarily

--- src/olmo_core/model_ladder.py
+++ src/olmo_core/model_ladder.py
@@ -484,7 +484,7 @@ class ModelLadder(Config, metaclass=ABCMeta):
                 name=f"{self.name}-{size}-{run_duration}",
                 workspace="ai2",
                 project=self.project,
-                enabled=False,
+                enabled=True,
                 cancel_check_interval=5,
             ),
         )
@@ -494,7 +494,7 @@ class ModelLadder(Config, metaclass=ABCMeta):
                 name=f"{self.name}-{size}-{run_duration}",
                 entity="ai2",
                 project=self.project,
-                enabled=True,
+                enabled=False,
                 cancel_check_interval=5,
             ),
         )
