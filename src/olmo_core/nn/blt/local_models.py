fix eval with larger lookahead, simplify boundary offset

src/olmo_core/nn/blt/local_models.py
@@ -154,15 +154,14 @@ class HNetBoundaryPredictor(nn.Module):
     ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         cos_sim = torch.einsum(
             "b l d, b l d -> b l",
-            F.normalize(self.q_proj_layer(hidden_states[:, 1:-self.boundary_predictor_lookahead]), dim=-1),
-            F.normalize(self.k_proj_layer(hidden_states[:, 1 + self.boundary_predictor_lookahead:]), dim=-1),
+            F.normalize(self.q_proj_layer(hidden_states[:, :-self.boundary_predictor_lookahead]), dim=-1),
+            F.normalize(self.k_proj_layer(hidden_states[:, self.boundary_predictor_lookahead:]), dim=-1),
         )
         boundary_logprobs = torch.log1p(-cos_sim.float().clip(max=1.0 - epsilon)) - math.log(2)
         POSITIVE_LOGPROB = 0.0
         NEGATIVE_LOGPROB = -100_000
-        boundary_logprobs = F.pad(boundary_logprobs, (1, 0), "constant", POSITIVE_LOGPROB)
-        boundary_logprobs = F.pad(boundary_logprobs, (1, 0), "constant", NEGATIVE_LOGPROB)
-        boundary_logprobs = F.pad(boundary_logprobs, (0, self.boundary_predictor_lookahead - 1), "constant", NEGATIVE_LOGPROB)
+        boundary_logprobs[:, 0] = POSITIVE_LOGPROB
+        boundary_logprobs = F.pad(boundary_logprobs, (0, self.boundary_predictor_lookahead), "constant", NEGATIVE_LOGPROB)
 
         boundary_logprobs_for_loss, boundary_logprobs = _teacher_force_interpolate(
             boundary_logprobs,
@@ -536,7 +535,7 @@ class LocalEncoder(nn.Module):
                 torch.arange(L, device=pool_out.device)[None, :] + (~boundary_mask).long() * L  # type: ignore
             )
             seq_sorted_indices = torch.argsort(token_idx, dim=1)
-            index = (seq_sorted_indices[:, :patch_lens.shape[1], None] - 1).clip(min=0).expand(
+            index = seq_sorted_indices[:, :patch_lens.shape[1], None].expand(
                 -1, -1, pool_out.shape[-1]
             )
 
@@ -904,11 +903,7 @@ class LocalDecoder(nn.Module):
         prepool_out = cast(torch.Tensor, prepool_out)
 
         # TODO(benjaminm): clipping is problematic if it happens too much; track clip %.
-        plug_back_idx = (torch.cumsum(boundary_mask[:, 1:], dim=1) - 1).clip(max=prepool_out.shape[1] - 1)
-        plug_back_idx = torch.cat([
-            plug_back_idx,
-            plug_back_idx.max(1, keepdim=True).values
-        ], 1)
+        plug_back_idx = (torch.cumsum(boundary_mask, dim=1) - 1).clip(max=prepool_out.shape[1] - 1)
         depool_out = torch.gather(
             prepool_out,
             dim=1,
@@ -945,7 +940,7 @@ class LocalDecoder(nn.Module):
 
         non_b_indices = torch.arange(len(h[0]), device=h.device).unsqueeze(0).repeat(len(h), 1)
         non_b_indices += plug_back_idx
-        b_indices = seq_sorted_indices[:, 1:] - 1 + torch.arange(patch_embeds.shape[1] - 1, device=h.device).unsqueeze(0)
+        b_indices = seq_sorted_indices[:, 1:] + torch.arange(patch_embeds.shape[1] - 1, device=h.device).unsqueeze(0)
         b_indices = torch.where(
             patch_mask[:, 1:],
             b_indices,

src/olmo_core/nn/transformer/model.py
@@ -1776,7 +1776,7 @@ class BLTDistillTransformer(BLTTransformer):
         teacher_indices_to_select = torch.gather(
             true_patch_ids,
             dim=1,
-            index=seq_sorted_indices - 1,
+            index=seq_sorted_indices,
         )
         mask = patch_mask & (teacher_indices_to_select < teacher_embeds.shape[1])
         teacher_indices_to_select = torch.where(
@@ -1909,16 +1909,15 @@ class BLTDistillTransformer(BLTTransformer):
         )
         patch_mask = shifted_patch_lens != 0
 
-        patch_start_indices = torch.cumsum(local_encoder_kwargs["patch_lens"], dim=1)
-        patch_start_indices = torch.where(
-            patch_start_indices < byte_mask.shape[1],
-            patch_start_indices,
-            torch.zeros_like(patch_start_indices), # effectively mask out, index 0 is always start
+        patch_end_indices = torch.cumsum(local_encoder_kwargs["patch_lens"], dim=1) - 1
+        patch_end_indices = torch.where(
+            patch_end_indices < byte_mask.shape[1],
+            patch_end_indices,
+            torch.zeros_like(patch_end_indices), # effectively mask out, index 0 is always start
         )
 
         boundary_labels = torch.zeros_like(byte_mask, dtype=torch.float32)
-        boundary_labels[:, 0] = 0.0  # bos is never a boundary
-        boundary_labels.scatter_(1, patch_start_indices, 1.0)
+        boundary_labels.scatter_(1, patch_end_indices, 1.0)
 
         if blt_config.teacher_force_interpolation_steps != 0:
             teacher_force_interpolation_ratio = min(kwargs["step"] / blt_config.teacher_force_interpolation_steps, 1.0)
@@ -1955,19 +1954,16 @@ class BLTDistillTransformer(BLTTransformer):
         )
 
         # compute the boundary loss
-        boundary_byte_mask = byte_mask.clone()
-        if self.local_encoder.boundary_predictor_lookahead > 1:  # type: ignore
-            boundary_byte_mask[:, -self.local_encoder.boundary_predictor_lookahead - 1:] = 0  # type: ignore
         elementwise_boundary_loss = blt_utils.binary_cross_entropy_with_logprobs(
             boundary_logprobs_for_loss,
             boundary_labels,
         )
-        boundary_loss = (elementwise_boundary_loss * boundary_byte_mask).mean()
+        boundary_loss = (elementwise_boundary_loss * byte_mask).mean()
         elementwise_boundary_acc = boundary_mask == (boundary_labels > 0)
-        boundary_acc = (elementwise_boundary_acc * boundary_byte_mask).float().mean()
-        metrics["blt/boundary_loss"] = boundary_loss / boundary_byte_mask.float().mean()
-        metrics["blt/boundary_acc"] = boundary_acc / boundary_byte_mask.float().mean()
-        metrics["blt/boundary_mean"] = (boundary_mask * boundary_byte_mask).float().mean() / boundary_byte_mask.float().mean()
+        boundary_acc = (elementwise_boundary_acc * byte_mask).float().mean()
+        metrics["blt/boundary_loss"] = boundary_loss / byte_mask.float().mean()
+        metrics["blt/boundary_acc"] = boundary_acc / byte_mask.float().mean()
+        metrics["blt/boundary_mean"] = (boundary_mask * byte_mask).float().mean() / byte_mask.float().mean()
 
         # First, run the teacher.
         if not skip_teacher: