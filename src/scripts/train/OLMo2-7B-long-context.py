b7a87a4ed6c3c197ca5475f27792b88f62009e0a
aman-17 <amanrangapur@gmail.com>
2025-09-02 13:07:13 -0700

    load_strategy always

--- src/scripts/train/OLMo2-7B-long-context.py
+++ src/scripts/train/OLMo2-7B-long-context.py
@@ -95,9 +95,10 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
             save_overwrite=True,
             metrics_collect_interval=10,
             cancel_check_interval=1,
-            load_strategy=LoadStrategy.never,
             max_duration=Duration.steps(25),
-            # load_path='gs://ai2-llm/checkpoints/OLMo25-from476838/step500680',
+            load_path='gs://ai2-llm/checkpoints/OLMo25-from476838/step500680',
+            load_strategy=LoadStrategy.always,
+
         )
         .with_callback(
             "checkpointer",
