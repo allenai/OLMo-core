1839dc968c221095cc06eee491936c03b2a2e850
Will Merrill <willm@allenai.org>
2025-09-11 23:20:47 +0000

    Reduce batch size

--- src/scripts/train/linear-rnns/FLA-deltanet.py
+++ src/scripts/train/linear-rnns/FLA-deltanet.py
@@ -54,7 +54,7 @@ def build_model_config(common: CommonComponents) -> TransformerConfig:
     config.block.attention = AttentionConfig()  # not used
     # 6d^2 for GatedDeltaNet vs. 3d^2 for attention
     # config.block.d_model = 1024
-    config.block.d_model = 512  # CUDA memory issues
+    config.block.d_model = 256  # CUDA memory issues
     config.block.n_heads = 16
     config.block.fla = FLAConfig(
         name="GatedDeltaNet",
