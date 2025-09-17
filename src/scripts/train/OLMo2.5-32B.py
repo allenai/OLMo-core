eedbd5c49e582025cea009b7d8ac7b45fc32d638
Dirk Groeneveld <dirkg@allenai.org>
2025-09-16 20:37:33 -0700

    No more ephemeral checkpoints

--- src/scripts/train/OLMo2.5-32B.py
+++ src/scripts/train/OLMo2.5-32B.py
@@ -103,7 +103,7 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
             "checkpointer",
             CheckpointerCallback(
                 save_interval=1000,
-                ephemeral_save_interval=200,
+                ephemeral_save_interval=None,
                 save_async=False,
             ),
         )
