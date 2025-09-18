fc00c4f0ec79472a6b726a0743b467335ccd098b
Tyler Romero <tylerr@allenai.org>
2025-09-18 14:14:03 -0700

    clean

--- src/olmo_core/eval/task_groups.py
+++ src/olmo_core/eval/task_groups.py
@@ -133,52 +133,10 @@ FAST_TASKS = [
     "copycolors_10way_fast",
 ]
 
-# NOTE: preserved for backwards compatibility, OLMO2_DEV_1B_TASKS is the same as
-# FULL_TASKS_SMALL_COMPUTE but with "copycolors_10way" instead of "copycolors_10way_fast"
-OLMO2_DEV_1B_TASKS = [
-    # OLMES Core 9(-ish) RC
-    "arc_challenge_test_rc_5shot",
-    "arc_easy_test_rc_5shot",
-    "hellaswag_rc_5shot",  # 1K subset of HellaSwag
-    "winogrande_val_rc_5shot",  # Helpful after 750M-5xC scale
-    "csqa_val_rc_5shot",
-    "piqa_val_rc_5shot",
-    "socialiqa_val_rc_5shot",
-    # MMLU RC
-    "mmlu_stem_val_rc_5shot",
-    "mmlu_humanities_val_rc_5shot",
-    "mmlu_social_sciences_val_rc_5shot",
-    "mmlu_other_val_rc_5shot",
-    "mmlu_stem_test_rc_5shot",
-    "mmlu_humanities_test_rc_5shot",
-    "mmlu_social_sciences_test_rc_5shot",
-    "mmlu_other_test_rc_5shot",
-    # Gen tasks BPB
-    "gsm8k_gold_bpb_5shot",
-    "minerva_math_algebra_gold_bpb_0shot",
-    "minerva_math_counting_and_probability_gold_bpb_0shot",
-    "minerva_math_geometry_gold_bpb_0shot",
-    "minerva_math_intermediate_algebra_gold_bpb_0shot",
-    "minerva_math_number_theory_gold_bpb_0shot",
-    "minerva_math_prealgebra_gold_bpb_0shot",
-    "minerva_math_precalculus_gold_bpb_0shot",
-    "codex_humaneval_gold_bpb_0shot",
-    "codex_mbpp_gold_bpb_0shot",
-    # Sanity check for MCQA ability
-    "copycolors_10way",
-    # Basic Skills rc 5shot
-    "basic_skills_arithmetic_rc_5shot",
-    "basic_skills_coding_rc_5shot",
-    "basic_skills_common_knowledge_rc_5shot",
-    "basic_skills_logical_reasoning_rc_5shot",
-    "basic_skills_pattern_rc_5shot",
-    "basic_skills_string_operations_rc_5shot",
-]
 
 TASK_GROUPS: Dict[str, List[str]] = {
     "full__small_compute": FULL_TASKS_SMALL_COMPUTE,
     "full__large_compute": FULL_TASKS_LARGE_COMPUTE,
     "full": FULL_TASKS,
     "fast": FAST_TASKS,
-    "olmo2_dev_1b": OLMO2_DEV_1B_TASKS,
 }
