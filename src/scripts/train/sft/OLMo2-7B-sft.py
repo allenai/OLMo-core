cba48aec45ef486e545ba50fd85c0461ec4f91d0
saumyamalik <saumyam@allenai.org>
2025-09-26 01:11:25 -0700

    seed #4

--- src/scripts/train/sft/OLMo2-7B-sft.py
+++ src/scripts/train/sft/OLMo2-7B-sft.py
@@ -245,7 +245,7 @@ class SFTConfig(Config):
     data_loader: NumpyDataLoaderConfig
     train_module: TransformerTrainModuleConfig
     trainer: TrainerConfig
-    init_seed: int = 33333
+    init_seed: int = 78899
 
     @classmethod
     def build(
