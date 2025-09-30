6b2c5c8c1be315f74df5b9e3b8a8cdf65a0cfbe5
jacob-morrison <jacobm@allenai.org>
2025-09-30 12:25:15 -0700

    fix

--- src/scripts/train/sft/OLMo2-7B-sft.py
+++ src/scripts/train/sft/OLMo2-7B-sft.py
@@ -17,7 +17,7 @@ from rich import print
 
 from olmo_core.config import Config, DType
 from olmo_core.data import NumpyDataLoaderConfig, NumpyPackedFSLDatasetConfig, TokenizerConfig
-from olmo_core.data.types import LongDocStrategy, NumpyDatasetType
+from olmo_core.data.types import LongDocStrategy
 from olmo_core.distributed.parallel import DataParallelType
 from olmo_core.distributed.utils import get_local_rank
 from olmo_core.exceptions import OLMoConfigurationError
