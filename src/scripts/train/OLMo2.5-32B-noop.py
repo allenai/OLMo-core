e1dff31f06d42710d7cbd90923c9c9040a17c7d7
Dirk Groeneveld <dirkg@allenai.org>
2025-10-04 23:52:43 -0700

    Optimizers cannot be set in overrides

---
+++ src/scripts/train/OLMo2.5-32B-noop.py
@@ -0,0 +1,160 @@
+from datetime import datetime
+
+from olmo_core.config import DType
+from olmo_core.data import DataMix
+from olmo_core.distributed.parallel import DataParallelType
+from olmo_core.float8 import Float8Config
+from olmo_core.internal.common import CLUSTER_TO_GPU_TYPE
+from olmo_core.internal.experiment import (
+    CommonComponents,
+    build_common_components,
+    main,
+)
+from olmo_core.nn.attention import SlidingWindowAttentionConfig
+from olmo_core.nn.transformer import (
+    TransformerActivationCheckpointingMode,
+    TransformerConfig,
+)
+from olmo_core.optim import CosWithWarmup, OptimGroupOverride, SkipStepAdamWConfig, NoOpConfig
+from olmo_core.train import Duration, TrainerConfig
+from olmo_core.train.callbacks import (
+    CheckpointerCallback,
+    CometCallback,
+    SlackNotifierCallback,
+    WandBCallback,
+)
+from olmo_core.train.train_module import (
+    TransformerActivationCheckpointingConfig,
+    TransformerDataParallelConfig,
+    TransformerDataParallelWrappingStrategy,
+    TransformerTrainModuleConfig,
+)
+
+SEQUENCE_LENGTH = 8 * 1024
+GLOBAL_BATCH_SIZE = 8 * 1024 * 1024
+
+
+def build_model_config(common: CommonComponents) -> TransformerConfig:
+    config = TransformerConfig.olmo2_32B(vocab_size=common.tokenizer.padded_vocab_size())
+    config.block.attention.sliding_window = SlidingWindowAttentionConfig(
+        force_full_attention_on_first_layer=False,
+        force_full_attention_on_last_layer=True,
+        pattern=[4096, 4096, 4096, -1],
+    )
+    config.block.attention.use_flash = True
+    return config
+
+
+def build_train_module_config(common: CommonComponents) -> TransformerTrainModuleConfig:
+    rank_microbatch_size = SEQUENCE_LENGTH
+    if common.launch is not None:
+        gpus = {CLUSTER_TO_GPU_TYPE.get(c, "unknown") for c in common.launch.clusters}
+        if all("B200" in g for g in gpus):
+            rank_microbatch_size *= 2
+
+    return TransformerTrainModuleConfig(
+        rank_microbatch_size=rank_microbatch_size,
+        max_sequence_length=common.dataset.effective_sequence_length,
+        optim=NoOpConfig(),
+        compile_model=True,
+        dp_config=TransformerDataParallelConfig(
+            name=DataParallelType.hsdp,
+            param_dtype=DType.bfloat16,
+            reduce_dtype=DType.float32,
+            wrapping_strategy=TransformerDataParallelWrappingStrategy.full,
+            shard_degree=64,
+        ),
+        ac_config=TransformerActivationCheckpointingConfig(
+            mode=TransformerActivationCheckpointingMode.budget, activation_memory_budget=0.5
+        ),
+        float8_config=Float8Config(enabled=False),
+        z_loss_multiplier=1e-5,
+        max_grad_norm=1.0,
+        scheduler=CosWithWarmup(warmup_steps=2000),
+    )
+
+
+def build_trainer_config(common: CommonComponents) -> TrainerConfig:
+    cancel_check_interval = 10
+
+    assert common.launch is not None
+    assert len(common.launch.clusters) == 1
+    cluster = common.launch.clusters[0]
+
+    run_name = f"{common.run_name}-{datetime.now().astimezone().strftime('%Y%m%dT%H%M%z')}"
+
+    return (
+        TrainerConfig(
+            save_folder=f"gs://ai2-llm/checkpoints/{common.run_name}/",
+            save_overwrite=True,
+            metrics_collect_interval=50,
+            cancel_check_interval=cancel_check_interval,
+            max_duration=Duration.epochs(1),
+        )
+        .with_callback(
+            "checkpointer",
+            CheckpointerCallback(
+                save_interval=1000,
+                ephemeral_save_interval=None,
+                save_async=False,
+            ),
+        )
+        .with_callback(
+            "comet",
+            CometCallback(
+                name=run_name,
+                workspace="ai2",
+                project="olmo3",
+                enabled=False,
+                cancel_check_interval=cancel_check_interval,
+            ),
+        )
+        .with_callback(
+            "wandb",
+            WandBCallback(
+                name=run_name,
+                group=common.run_name,
+                entity="ai2-llm",
+                project="olmo3",
+                enabled=True,
+                cancel_check_interval=cancel_check_interval,
+            ),
+        )
+        .with_callback(
+            "slack_notifier",
+            SlackNotifierCallback(
+                name=run_name,
+                enabled=True,
+            ),
+        )
+        .with_recommended_evals(
+            common.tokenizer, SEQUENCE_LENGTH, cluster, task_set="fast", eval_interval=1000
+        )
+    )
+
+
+def build_common_config(*args, **kwargs):
+    components = build_common_components(*args, **kwargs)
+    components.data_loader.num_workers = 8
+
+    # components.dataset.max_target_sequence_length = 32 * 1024
+    # This setting would be tempting, so that we can seamlessly extend the
+    # context length at the end, but it decreases diversity in the batch.
+
+    components.dataset.mix = DataMix.OLMo_mix_0925
+    return components
+
+
+if __name__ == "__main__":
+    main(
+        global_batch_size=GLOBAL_BATCH_SIZE,
+        sequence_length=SEQUENCE_LENGTH,
+        common_config_builder=build_common_config,
+        model_config_builder=build_model_config,
+        train_module_config_builder=build_train_module_config,
+        trainer_config_builder=build_trainer_config,
+        beaker_image="petew/olmo-core-tch270cu128-2025-05-16",
+        include_instance_filter=True,
+        include_default_evals=False,
+        flight_recorder=True,
+    )
