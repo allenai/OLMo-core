161bdad13eb6cf2830101c8be98bac2e768c6bd0
Allyson Ettinger <allysone@allenai.org>
2025-10-27 17:55:39 -0400

    load 32B checkpoint

--- src/scripts/train/OLMo3/OLMo3-32B-midtraining.py
+++ src/scripts/train/OLMo3/OLMo3-32B-midtraining.py
@@ -81,7 +81,7 @@ def build_experiment_config(cli_context: CliContext) -> ExperimentConfig:
     )
 
     trainer_config = cookbook.configure_trainer(
-        load_path="gs://ai2-llm/checkpoints/OLMo25/step1413814", #TO BE UPDATED
+        load_path="gs://ai2-llm/checkpoints/stego32-highlr-filter3/step555000", #TO BE UPDATED
         load_trainer_state=False,
         load_optim_state=True,
         max_duration=Duration.tokens(MAX_TOKENS),
