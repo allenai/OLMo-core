43693301e9cbf85e6d7713b43fe73435240e84f8
Tyler Romero <tylerr@allenai.org>
2025-10-31 11:37:25 -0700

    NCCL_NVLSTREE_MAX_CHUNKSIZE=131072

--- src/olmo_core/distributed/utils.py
+++ src/olmo_core/distributed/utils.py
@@ -99,6 +99,9 @@ def init_distributed(backend: str = "nccl", timeout: timedelta = timedelta(minut
             )
             set_env_var("NCCL_SOCKET_IFNAME", "enp0s12")
             set_env_var("NCCL_DEBUG_SUBSYS", "INIT,NET,COLL")
+            set_env_var(
+                "NCCL_NVLSTREE_MAX_CHUNKSIZE", "131072"
+            )  # set b/c enforced by the new library versions google recommended to us.
 
     if backend_supports_cuda(backend):
         # Set CUDA device.
