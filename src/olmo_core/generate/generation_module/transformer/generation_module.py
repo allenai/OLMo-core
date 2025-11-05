fix loglikelihood impl for fused boundaries, restore max_patch_length_decode

src/olmo_core/generate/generation_module/transformer/generation_module.py
@@ -915,7 +915,7 @@ class BLTTransformerGenerationModule(TransformerGenerationModule):
         stream: bool = False,
         verify: bool = False,
         force_boundary_every: Optional[int] = None,
-        max_patch_length_decode: Optional[int] = None, # 128 is longest dolma2 tokenizer token
+        max_patch_length_decode: Optional[int] = 128, # 128 is longest dolma2 tokenizer token
         stop_on_exceed_patch_length: bool = True, # chances are slim to produce something useful
         profile: bool = False,
         **generation_kwargs,
@@ -1244,11 +1244,16 @@ class BLTTransformerGenerationModule(TransformerGenerationModule):
                     next_tokens[:] = 36
 
             if max_patch_length_decode is not None:
-                if fuse_boundaries:
-                    raise NotImplementedError("max_patch_length_decode not implemented with fuse_boundaries=True")
-
                 stop_mask = bytes_since_boundary >= max_patch_length_decode
-                next_tokens[stop_mask] = self.model.end_of_subword_token_blt # type: ignore
+
+                if fuse_boundaries:
+                    next_tokens = torch.where(
+                        (next_tokens < boundary_offset) & stop_mask,
+                        next_tokens + boundary_offset,
+                        next_tokens,
+                    )
+                else:
+                    next_tokens[stop_mask] = self.model.end_of_subword_token_blt  # type: ignore
 
                 if stop_on_exceed_patch_length:
                     finished |= stop_mask

src/olmo_core/nn/transformer/model.py
@@ -2468,16 +2468,10 @@ class BLTDistillTransformer(BLTTransformer):
             ce_loss, _ = cross_entropy_loss(logits.view(-1, logits.shape[-1]), labels.view(-1))  # type: ignore
 
             # replace plain (single byte) logits with byte + boundary logits where boundaries occur
-            main_path_logprobs = torch.gather(F.log_softmax(logits[:, :-1].float(), dim=-1), -1, labels[:, :-1].clip(0).unsqueeze(-1)).squeeze(-1)
-            remaining_logpmass = log1mexp(main_path_logprobs)
-            remaining_logp_uniform = remaining_logpmass - math.log(logits.shape[2] - 1)  # -1 to skip the main path token
-            logits.zero_()
-            logits[:, :-1, :] = remaining_logp_uniform.unsqueeze(-1)
-            logits.scatter_(
-                -1,
-                input_ids[:, 1:].unsqueeze(-1),
-                main_path_logprobs.to(logits.dtype).unsqueeze(-1),
-            )
+            probs = F.softmax(logits.float(), dim=-1)
+            probs[..., :self.vocab_size_blt] += probs[..., self.vocab_size_blt:self.vocab_size_blt*2]
+            logits = torch.log(probs)
+            logits[..., self.vocab_size_blt:self.vocab_size_blt*2] = -100_000
         else:
             logits = torch.concatenate([
                 logits,