3f98f9bce4acb9055914a178ed0ce11b3d0bec96
Tyler Romero <tylerr@allenai.org>
2025-11-04 17:03:35 -0800

    style

--- src/olmo_core/nn/transformer/config.py
+++ src/olmo_core/nn/transformer/config.py
@@ -1069,11 +1069,9 @@ class TransformerConfig(Config):
                 hidden_size=expert_hidden_size,
                 capacity_factor=capacity_factor,
                 router=MoERouterConfig(top_k=top_k),
-                shared_mlp=(
-                    None
-                    if shared_expert_hidden_size is None
-                    else FeedForwardConfig(hidden_size=shared_expert_hidden_size, bias=False)
-                ),
+                shared_mlp=None
+                if shared_expert_hidden_size is None
+                else FeedForwardConfig(hidden_size=shared_expert_hidden_size, bias=False),
                 lb_loss_weight=lb_loss_weight,
                 z_loss_weight=z_loss_weight,
             ),
