aecaabd8d438ebb7e3678ebe59015284f6770527
Amanda Bertsch <amandab@allenai.org>
2025-11-11 08:20:02 -0800

    add comma

--- src/scripts/train/OLMo2.5-7B-no_qk.py
+++ src/scripts/train/OLMo2.5-7B-no_qk.py
@@ -89,7 +89,7 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
             metrics_collect_interval=10,
             cancel_check_interval=cancel_check_interval,
             max_duration=Duration.tokens(int(5e12)),
-            hard_stop=Duration.tokens(int(145e9)) # stop at 145B tokens for this run,
+            hard_stop=Duration.tokens(int(145e9)), # stop at 145B tokens for this run,
             keys_to_ignore=[r'q_norm', r'k_norm'], # don't want qk norm params loaded
         )
         .with_callback(
