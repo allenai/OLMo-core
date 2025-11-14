6c7badb73a04dbec8ccd70a1de8991ef8d3186b0
Dirk Groeneveld <dirkg@allenai.org>
2025-11-14 13:08:21 -0800

    I this did wrong.

--- src/olmo_core/generate/generation_module/transformer/generation_module.py
+++ src/olmo_core/generate/generation_module/transformer/generation_module.py
@@ -555,8 +555,8 @@ class TransformerGenerationModule(GenerationModule):
 
             # Average the weights
             for key in merged_state_dict["model"].keys():
+                target_tensor = merged_state_dict["model"][key]
                 if torch.is_tensor(target_tensor) and torch.is_floating_point(target_tensor):
-                    target_tensor = merged_state_dict["model"][key]
                     source_tensor = next_state_dict["model"].pop(key)
                     assert target_tensor.shape == source_tensor.shape
                     # in-place operations for better memory consumption
