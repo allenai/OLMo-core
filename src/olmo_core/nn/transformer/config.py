67f244e546a37f41f8239717c4c96072f06a7bfc
Amanda Bertsch <amandab@allenai.org>
2025-11-25 20:31:28 -0800

    v0 of gqa free llama, not fixed yet

--- src/olmo_core/nn/transformer/config.py
+++ src/olmo_core/nn/transformer/config.py
@@ -858,6 +858,23 @@ class TransformerConfig(Config):
         )
     
     @classmethod
+    def llama3_8B_no_gqa(cls, vocab_size: int, **kwargs) -> "TransformerConfig":
+        """
+        An 8B Llama3-like model config.
+        """
+        return cls.llama_like(
+            d_model=4096,
+            vocab_size=vocab_size,
+            n_layers=kwargs.pop("n_layers", 32),
+            n_heads=kwargs.pop("n_heads", 32),
+            n_kv_heads=kwargs.pop("n_kv_heads", 8),
+            rope_theta=kwargs.pop("rope_theta", 500_000),
+            hidden_size_multiplier=1.3,
+            hidden_size_multiple_of=1024,
+            **kwargs,
+    )
+
+    @classmethod
     def llama3_8B_qknorm_reordered(cls, vocab_size: int, **kwargs) -> "TransformerConfig":
         """
         An 8B Llama3-like model config.
