554c75730590024c22cee66098b121aebe358d05
Will Merrill <willm@allenai.org>
2025-12-01 18:14:13 -0800

    Revert to remove 1 head

--- src/scripts/train/linear-rnns/OLMo3.1-7B-hybrid.py
+++ src/scripts/train/linear-rnns/OLMo3.1-7B-hybrid.py
@@ -53,29 +53,29 @@ def build_model_config(common: CommonComponents) -> TransformerConfig:
         attn_backend=AttentionBackendName.flash_2,
     )
 
-    # # Remove heads (and scale down d_model) to compensate for extra params.
-    # config.d_model -= REMOVE_HEADS * 128
-    # config.block.attention.n_heads -= REMOVE_HEADS
-    # assert config.d_model / config.block.attention.n_heads == 128
+    # Remove heads (and scale down d_model) to compensate for extra params.
+    config.d_model -= REMOVE_HEADS * 128
+    config.block.attention.n_heads -= REMOVE_HEADS
+    assert config.d_model / config.block.attention.n_heads == 128
 
-    # ### Copied below from hybrid/gated_deltanet_0_25_rnn_first.py ###
+    ### Copied below from hybrid/gated_deltanet_0_25_rnn_first.py ###
 
-    # # Update the config to use an FLA block.
-    # config.block.name = TransformerBlockType.fla_hybrid
-    # assert config.n_layers % 4 == 0, "Current logic assumes n_layers is multiple of 4"
-    # config.block.fla_hybrid_attention_indices = [i for i in range(config.n_layers) if i % 4 == 3]
+    # Update the config to use an FLA block.
+    config.block.name = TransformerBlockType.fla_hybrid
+    assert config.n_layers % 4 == 0, "Current logic assumes n_layers is multiple of 4"
+    config.block.fla_hybrid_attention_indices = [i for i in range(config.n_layers) if i % 4 == 3]
 
-    # # Configure the non-attention part of the block to be a DeltaNet.
-    # config.block.fla = FLAConfig(
-    #     name="GatedDeltaNet",
-    #     dtype=config.dtype,
-    #     fla_layer_kwargs={
-    #         # FLA repo says num_heads * head_dim = 0.75 * hidden_size
-    #         "head_dim": int(0.75 * config.d_model / config.block.attention.n_heads),
-    #         "use_gate": True,
-    #         "allow_neg_eigval": True,
-    #     },
-    # )
+    # Configure the non-attention part of the block to be a DeltaNet.
+    config.block.fla = FLAConfig(
+        name="GatedDeltaNet",
+        dtype=config.dtype,
+        fla_layer_kwargs={
+            # FLA repo says num_heads * head_dim = 0.75 * hidden_size
+            "head_dim": int(0.75 * config.d_model / config.block.attention.n_heads),
+            "use_gate": True,
+            "allow_neg_eigval": True,
+        },
+    )
 
     return config
 
