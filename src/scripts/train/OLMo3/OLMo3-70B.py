af3611e607f6c1e7935580dfabb08abddbbade21
epwalsh <petew@allenai.org>
2025-12-16 13:10:03 -0800

    updates

--- src/scripts/train/OLMo3/OLMo3-70B.py
+++ src/scripts/train/OLMo3/OLMo3-70B.py
@@ -137,7 +137,7 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
         TrainerConfig(
             save_folder=f"gs://ai2-llm/checkpoints/{common.run_name}/",
             save_overwrite=True,
-            metrics_collect_interval=50,
+            metrics_collect_interval=10,
             cancel_check_interval=cancel_check_interval,
             max_duration=Duration.epochs(1),
         )
