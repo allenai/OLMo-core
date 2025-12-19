10174ae66ae584e10836eeebfa0ec5027f465bba
Tianhua Tao <taotianhua@outlook.com>
2025-12-19 06:31:33 +0000

    t

--- src/scripts/inspect_pt.py
+++ src/scripts/inspect_pt.py
@@ -1,10 +1,12 @@
 import torch
 import transformers
-
+import pickle
 input_path = "/home/tth/input_ids_rank195_mbx0.pt"
+input_path = "/workspace/OLMo-core/23526.id"
 
-data = torch.load(input_path)
-
+data = pickle.load(open(input_path, "rb"))
+# data = torch.load(input_path)
+data = data['input_ids']
 print(data)
 
 
@@ -13,4 +15,8 @@ tokenizer = transformers.AutoTokenizer.from_pretrained("allenai/dolma2-tokenizer
 decoded = tokenizer.batch_decode(data, )
 
 print('-----------------------')
-print(decoded[0])
+# print(decoded[0])
+
+with open('/workspace/decoded.txt', 'w') as f:
+    for i, line in enumerate(decoded):
+        f.write(line + f'\n-----------SEPARATOR {i}-----------\n')
