0f3d043e651680482d2894db71a58ada466c1077
epwalsh <petew@allenai.org>
2025-12-24 15:56:24 -0800

    fix

--- src/scripts/lambda/utils.sh
+++ src/scripts/lambda/utils.sh
@@ -72,7 +72,7 @@ function with_retries {
 }
 
 function get_job_name {
-    sacct -j "$1" --format=JobName%60 --noheader | head -n 1 | sed 's/^ *//'
+    sacct -j "$1" --format=JobName%60 --noheader | head -n 1 | xargs
 }
 
 function job_pending {
