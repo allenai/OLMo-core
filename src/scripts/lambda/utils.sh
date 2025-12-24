4ea3e3f20989bac573bc6e0de430457ab6624aae
epwalsh <petew@allenai.org>
2025-12-24 12:53:57 -0800

    add script name

--- src/scripts/lambda/utils.sh
+++ src/scripts/lambda/utils.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 function log_debug {
-    echo -e "\e[36m[$(date '+%Y-%m-%d %H:%M:%S')]\e[0m \e[90mDEBUG  \e[0m [$(hostname), node=${SLURM_NODEID:-?}] $1"
+    script_name=$(basename "$0")
+    echo -e "\e[36m[$(date '+%Y-%m-%d %H:%M:%S')]\e[0m \e[90mDEBUG  \e[0m [$script_name, $(hostname), node=${SLURM_NODEID:-?}] $1"
 }
 
 function log_info {
-    echo -e "\e[36m[$(date '+%Y-%m-%d %H:%M:%S')]\e[0m \e[34mINFO   \e[0m [$(hostname), node=${SLURM_NODEID:-?}] $1"
+    echo -e "\e[36m[$(date '+%Y-%m-%d %H:%M:%S')]\e[0m \e[34mINFO   \e[0m [$script_name, $(hostname), node=${SLURM_NODEID:-?}] $1"
 }
 
 function log_warning {
-    echo -e >&2 "\e[36m[$(date '+%Y-%m-%d %H:%M:%S')]\e[0m \e[33mWARNING\e[0m [$(hostname), node=${SLURM_NODEID:-?}] $1"
+    echo -e >&2 "\e[36m[$(date '+%Y-%m-%d %H:%M:%S')]\e[0m \e[33mWARNING\e[0m [$script_name, $(hostname), node=${SLURM_NODEID:-?}] $1"
 }
 
 function log_error {
-    echo -e >&2 "\e[36m[$(date '+%Y-%m-%d %H:%M:%S')]\e[0m \e[31mERROR  \e[0m [$(hostname), node=${SLURM_NODEID:-?}] $1"
+    echo -e >&2 "\e[36m[$(date '+%Y-%m-%d %H:%M:%S')]\e[0m \e[31mERROR  \e[0m [$script_name, $(hostname), node=${SLURM_NODEID:-?}] $1"
 }
 
 function die {
