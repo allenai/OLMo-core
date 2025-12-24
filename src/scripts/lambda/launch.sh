6733d7210fe262a4639140fb3cf35b150472bb28
epwalsh <petew@allenai.org>
2025-12-24 07:48:32 -0800

    capture interrupt in launch script

--- src/scripts/lambda/launch.sh
+++ src/scripts/lambda/launch.sh
@@ -76,5 +76,22 @@ while [ ! -f "$LOG_FILE" ]; do
     sleep 1
 done
 
+# On keyboard interrupt, print some useful information before exiting.
+control_c() {
+    echo "Caught keyboard interrupt!"
+    echo "You can check the job status with:"
+    echo "  squeue -j $JOB_ID"
+    echo ""
+    echo "Or get detailed information about the job status with:"
+    echo "  scontrol show job $JOB_ID"
+    echo ""
+    echo "Or cancel the job with:"
+    echo "  scancel $JOB_ID"
+    echo ""
+    echo "The main log file is located at '$LOG_FILE'. Use this command to grep through it:"
+    echo "  cat $LOG_FILE | less -R"
+    exit 1
+}
+
 # Stream the log file from the first task.
 tail -n +1 -f "$LOG_FILE"
