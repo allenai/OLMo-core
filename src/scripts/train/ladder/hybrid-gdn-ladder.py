20707157df127590bb1d204d9ce7a16127360f92
Tyler Romero <tylerr@allenai.org>
2025-12-27 13:52:41 -0800

    .

---
+++ src/scripts/train/ladder/hybrid-gdn-ladder.py
@@ -0,0 +1,133 @@
+import argparse
+import logging
+import math
+
+import olmo_core.io as io
+from olmo_core.data import DataMix, TokenizerConfig
+from olmo_core.data.composable import *
+from olmo_core.internal.common import get_gpu_type, get_root_dir
+from olmo_core.internal.ladder import main
+from olmo_core.model_ladder import *
+from olmo_core.model_ladder.utils import format_count
+from olmo_core.nn.attention import AttentionBackendName
+from olmo_core.nn.fla.layer import FLAConfig
+from olmo_core.nn.transformer import TransformerBlockType, TransformerConfig
+from olmo_core.utils import ensure_multiple_of, warn_once
+
+log = logging.getLogger(__name__)
+
+
+class HybridGDNTransformerModelConfigurator(TransformerModelConfigurator):
+    def configure_model(
+        self,
+        *,
+        size_spec: str,
+        sequence_length: int,
+        tokenizer: TokenizerConfig,
+        device_type: str,
+    ) -> TransformerConfig:
+        # TODO: configure context-parallelism if needed.
+        device_type = device_type.lower()
+        assert "h100" in device_type or "b200" in device_type
+        assert sequence_length in {2048, 4096, 8192}
+        size_spec = TransformerSize(size_spec)
+        vocab_size = tokenizer.padded_vocab_size()
+        kwargs = dict(attn_backend=AttentionBackendName.flash_3)
+
+        model: TransformerConfig
+        if size_spec == TransformerSize.size_190M:
+            model = TransformerConfig.olmo3_190M(vocab_size, **kwargs)
+        elif size_spec == TransformerSize.size_370M:
+            model = TransformerConfig.olmo3_370M(vocab_size, **kwargs)
+        elif size_spec == TransformerSize.size_600M:
+            model = TransformerConfig.olmo3_600M(vocab_size, **kwargs)
+        elif size_spec == TransformerSize.size_760M:
+            model = TransformerConfig.olmo3_760M(vocab_size, **kwargs)
+        elif size_spec == TransformerSize.size_1B:
+            model = TransformerConfig.olmo3_1B(vocab_size, **kwargs)
+        elif size_spec == TransformerSize.size_3B:
+            model = TransformerConfig.olmo3_3B(vocab_size, **kwargs)
+        elif size_spec == TransformerSize.size_7B:
+            model = TransformerConfig.olmo3_7B(vocab_size, **kwargs)
+        elif size_spec == TransformerSize.size_13B:
+            model = TransformerConfig.olmo3_13B(vocab_size, **kwargs)
+        else:
+            raise OLMoConfigurationError(f"Unsupported model size '{size_spec}'")
+
+        ### Copied below from hybrid/gated_deltanet_0_25_rnn_first.py ###
+
+        # Update the config to use an FLA block.
+        model.block.name = TransformerBlockType.fla_hybrid
+
+        # Every 4th layer is a quadratic attention layer.
+        attention_indices = [i for i in range(model.n_layers) if i % 4 == 3]
+        # Force full attention on the last layer
+        if model.n_layers - 1 not in attention_indices:
+            attention_indices.append(model.n_layers - 1)
+        model.block.fla_hybrid_attention_indices = sorted(attention_indices)
+
+        # Configure the non-attention part of the block to be a DeltaNet.
+        model.block.fla = FLAConfig(
+            name="GatedDeltaNet",
+            dtype=model.dtype,
+            fla_layer_kwargs={
+                # FLA repo says num_heads * head_dim = 0.75 * hidden_size
+                "head_dim": ensure_multiple_of(
+                    int(0.75 * model.d_model / model.block.attention.n_heads), 128
+                ),
+                "use_gate": True,
+                "allow_neg_eigval": True,
+            },
+        )
+
+        # Make sure actual number of params is close to target number.
+        if (
+            pct_diff := (
+                math.fabs(model.num_non_embedding_params - size_spec.approx_num_params)
+                / size_spec.approx_num_params
+            )
+        ) > 0.05:
+            warn_once(
+                f"Configured model has {format_count(model.num_non_embedding_params)} (non-embedding) parameters, "
+                f"which differs from target of {size_spec} by ~{100 * pct_diff:.1f}%.",
+                UserWarning,
+            )
+        return model
+
+
+def configure_ladder(args: argparse.Namespace) -> ModelLadder:
+    tokenizer = TokenizerConfig.dolma2()
+    instance_sources: list[InstanceSourceConfig] = [
+        ConcatAndChunkInstanceSourceConfig(
+            sources=[
+                NumpyDocumentSourceMixConfig(
+                    tokenizer=tokenizer,
+                    mix=DataMix.OLMo_mix_0925,
+                    mix_base_dir=get_root_dir(args.cluster),
+                )
+            ],
+            sequence_length=args.sequence_length,
+        ),
+    ]
+    ladder = ModelLadder(
+        name=args.name,
+        dir=str(io.join_path(get_root_dir(args.cluster), "model-ladders", args.name)),
+        sizes=list(TransformerSize),
+        max_devices=args.max_gpus,
+        device_type=get_gpu_type(args.cluster),
+        model_configurator=TransformerModelConfigurator(),
+        run_configurator=WSDSChinchillaRunConfigurator(
+            chinchilla_multiple=args.chinchilla_multiple
+        ),
+        sequence_length=args.sequence_length,
+        tokenizer=tokenizer,
+        instance_sources=instance_sources,
+        data_loader=ComposableDataLoaderConfig(
+            num_workers=8, instance_filter_config=InstanceFilterConfig()
+        ),
+    )
+    return ladder
+
+
+if __name__ == "__main__":
+    main(configure_ladder)
