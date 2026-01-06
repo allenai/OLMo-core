9a3161eee0889f87bd0df6636ed3cce108be0c79
Tyler Romero <tylerr@allenai.org>
2026-01-06 14:55:28 -0800

    flash 2

--- src/scripts/train/OLMo3/OLMo-3-190M.py
+++ src/scripts/train/OLMo3/OLMo-3-190M.py
@@ -71,11 +71,12 @@ def build_experiment_config(cli_context: CliContext) -> ExperimentConfig:
 
     tokenizer_config = TokenizerConfig.dolma2()
 
+    # attn_backend = (
+    #     AttentionBackendName.flash_2 if "B200" in gpu_type else AttentionBackendName.flash_3
+    # )
+    attn_backend = AttentionBackendName.flash_2
     model_config = TransformerConfig.olmo3_190M(
-        vocab_size=tokenizer_config.padded_vocab_size(),
-        attn_backend=AttentionBackendName.flash_2
-        if "B200" in gpu_type
-        else AttentionBackendName.flash_3,
+        vocab_size=tokenizer_config.padded_vocab_size(), attn_backend=attn_backend
     )
 
     train_module_config = TransformerTrainModuleConfig(
