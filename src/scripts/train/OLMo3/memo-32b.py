5bdf4a7fbb13b3569de9bca8abc1e0aae5f5a921
Luca Soldaini <lucas@allenai.org>
2026-01-08 16:17:28 -0800

    this is rigth weka?

--- src/scripts/train/OLMo3/memo-32b.py
+++ src/scripts/train/OLMo3/memo-32b.py
@@ -96,7 +96,7 @@ def build_experiment_config(cli_context: CliContext) -> ExperimentConfig:
 
     trainer_config = cookbook.configure_trainer(
         # load_path="gs://ai2-llm/checkpoints/stego32-highlr-filter3/step656000",
-        load_path="/weka/oe-training-default/ai2-llm/checkpoints/stego32-highlr-filter3/step656000",
+        load_path="weka://oe-training-default/ai2-llm/checkpoints/stego32-highlr-filter3/step656000",
         load_trainer_state=False,
         load_optim_state=True,
         max_duration=Duration.tokens(MAX_TOKENS),
