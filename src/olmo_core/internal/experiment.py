64fd7f502600522c884761966593b3182a51456f
Finbarr Timbers <finbarrtimbers@gmail.com>
2026-01-14 12:00:36 -0700

    cleaned up PR.

--- src/olmo_core/internal/experiment.py
+++ src/olmo_core/internal/experiment.py
@@ -429,7 +429,7 @@ def launch(config: ExperimentConfig):
     config.launch.launch(
         follow=True,
         slack_notifications=slack_enabled,
-        launch_timeout=86400 * 7,  # 1 week timeout for testing
+        launch_timeout=5 * 60,
         #  step_timeout=30 * 60,  # hard timeout kills the job
         step_soft_timeout=10 * 60,  # soft timeout only sends slack warning
     )
