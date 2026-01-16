bf18a4931edd11f5dbeb493286737c2d697c6795
Tyler Romero <tylerr@allenai.org>
2026-01-16 13:45:14 -0800

    .

--- src/test/nn/transformer/model_test.py
+++ src/test/nn/transformer/model_test.py
@@ -1,5 +1,6 @@
 import logging
 from dataclasses import replace
+from test.nn.attention_test import BF16_ATOL, BF16_RTOL
 from typing import Optional, cast
 
 import pytest
@@ -54,7 +55,6 @@ from olmo_core.testing import (
     run_distributed_test,
 )
 from olmo_core.utils import get_default_device, seed_all
-from test.nn.attention_test import BF16_ATOL, BF16_RTOL
 
 log = logging.getLogger(__name__)
 
