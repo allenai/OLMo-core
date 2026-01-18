8a0fd99058ab6d0740c28ae8ba1461f473177f47
Luca Soldaini <luca@soldaini.net>
2026-01-17 18:52:58 -0800

    official pretrain and midtrain data, microanneals

---
+++ src/scripts/train/code_ablations/olmo3_7b_microanneal_official_midtrain_mix.py
@@ -0,0 +1,153 @@
+import sys
+from datetime import datetime
+from pathlib import Path
+
+from olmo_core.data import NumpyDataLoaderConfig, NumpyFSLDatasetConfig, TokenizerConfig
+from olmo_core.data.source_mixture import (
+    SourceMixtureConfig,
+    SourceMixtureDatasetConfig,
+    SourceMixtureList,
+)
+from olmo_core.internal import cookbook
+from olmo_core.internal.common import build_launch_config, get_root_dir, get_work_dir
+from olmo_core.internal.experiment import (
+    CliContext,
+    ExperimentConfig,
+    get_beaker_username,
+)
+from olmo_core.internal.experiment import (
+    main as olmo_core_main,
+)
+from olmo_core.launch.beaker import BeakerLaunchConfig
+from olmo_core.nn.transformer import TransformerConfig
+from olmo_core.optim.scheduler import LinearWithWarmup, SchedulerUnits
+from olmo_core.train import Duration
+from olmo_core.train.train_module import TransformerTrainModuleConfig
+
+# Change these to match the config you want to use
+SEQ_LENGTH = 8192
+GLOBAL_BATCH_SIZE = 2**21  # ~2M tokens
+MAX_TOKENS = 10_000_000_000  # 10B
+LR = 0.00020712352850360292 / 2  # halfing the LR
+SEED = 1337
+TOKENIZER_CONFIG = TokenizerConfig.dolma2()
+PRIORITY = "high"
+WORKSPACE = "ai2/olmo4"
+BUDGET = "ai2/oe-base"
+NUM_NODES = 2
+LOAD_PATH = "gs://ai2-llm/checkpoints/OLMo25/step1413814"
+BASE_SAVE_DIR = "s3://ai2-llm/checkpoints"
+
+
+MODEL_CONFIG = TransformerConfig.olmo3_7B(vocab_size=TOKENIZER_CONFIG.padded_vocab_size())
+
+DATASET_CONFIG = SourceMixtureList.from_yaml(
+    path="src/olmo_core/data/source_mixtures/OLMo3-32B-midtraining-modelnamefilter.yaml"
+)
+
+
+def build_experiment_config(cli_context: CliContext) -> ExperimentConfig:
+    run_name_with_ts = (
+        f"{cli_context.run_name}-{datetime.now().astimezone().strftime('%Y%m%dT%H%M%S%z')}"
+    )
+    root_dir = get_root_dir(cli_context.cluster)
+    work_dir = get_work_dir(root_dir)
+    save_dir = f"{BASE_SAVE_DIR}/{get_beaker_username()}/{cli_context.run_name}"
+
+    beaker_launch_config: BeakerLaunchConfig | None = build_launch_config(
+        name=cli_context.run_name,
+        cmd=cli_context.remote_cmd,
+        cluster=cli_context.cluster,
+        root_dir=root_dir,
+        workspace=WORKSPACE,
+        num_nodes=NUM_NODES,
+        nccl_debug=True,
+    )
+    beaker_launch_config.priority = PRIORITY
+
+    train_module_config: TransformerTrainModuleConfig = cookbook.configure_train_module(
+        max_sequence_length=SEQ_LENGTH,
+        rank_microbatch_size=SEQ_LENGTH * 2,
+        learning_rate=LR,
+        scheduler=LinearWithWarmup(units=SchedulerUnits.steps, warmup=200, alpha_f=0.0),
+        activation_memory_budget=0.5,
+    )
+
+    DATASET_CONFIG.validate()
+    dataset_config = NumpyFSLDatasetConfig.from_src_mix(
+        src_mix=SourceMixtureDatasetConfig(
+            source_list=DATASET_CONFIG,
+            requested_tokens=MAX_TOKENS,
+            global_batch_size=GLOBAL_BATCH_SIZE,
+            processes=16,
+            seed=SEED,
+        ),
+        tokenizer=TOKENIZER_CONFIG,
+        work_dir=work_dir,
+        sequence_length=SEQ_LENGTH,
+    )
+
+    data_loader_config = NumpyDataLoaderConfig(
+        global_batch_size=GLOBAL_BATCH_SIZE, seed=SEED, num_workers=4
+    )
+
+    trainer_config = cookbook.configure_trainer(
+        load_path=LOAD_PATH,
+        load_trainer_state=False,
+        load_optim_state=True,
+        max_duration=Duration.tokens(MAX_TOKENS),
+        checkpoint_dir=save_dir,
+        work_dir=work_dir,
+    ).with_callbacks(
+        cookbook.configure_default_callbacks(
+            run_name=run_name_with_ts,
+            wandb_group_name=cli_context.run_name,
+            checkpoint_save_interval=None,
+            ephemeral_checkpoint_save_interval=250,
+        )
+    )
+
+    experiment_config = ExperimentConfig(
+        run_name=cli_context.run_name,
+        launch=beaker_launch_config,
+        model=MODEL_CONFIG,
+        train_module=train_module_config,
+        trainer=trainer_config,
+        dataset=dataset_config,
+        data_loader=data_loader_config,
+        init_seed=SEED,
+    )
+    experiment_config = experiment_config.merge(cli_context.overrides)
+    return experiment_config
+
+
+def main():
+    # TWO CASES:
+    # 1. len(sys.argv) < 4: definitely i have to add the run name here
+    # 2. len(sys.argv) >= 4: depends on whether the first 4 all start NOT
+    #                        with --: if they do, then i already have the
+    #                        run name. but if any of the first 4 start
+    #                        with --, then it means that some are already
+    #                        overrides, so i need to add the run name in.
+    if len(sys.argv) < 4 or any(arg.startswith("--") for arg in sys.argv[:4]):
+        sys.argv.insert(2, Path(__file__).stem)
+
+    # now i can just call the main function
+    return olmo_core_main(config_builder=build_experiment_config)
+
+
+if __name__ == "__main__":
+    """
+    Invoke this script directly to access the internal experiment CLI, which
+    supports launch, train, dry_run, and other subcommands.
+
+    Examples:
+        To render the config and exit:
+            uv run src/.../50web_alldressed_v2_50stack_edu_python.py dry_run
+
+        To launch a training run on Augusta w/ 8 nodes:
+            uv run src/.../50web_alldressed_v2_50stack_edu_python.py launch ai2/augusta \
+                --launch.num_nodes=8 \
+                --launch.priority=high
+    """
+    main()
