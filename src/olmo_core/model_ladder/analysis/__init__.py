d8ef97dadfca0479f032a2d20c4cd7cdf40b806b
Tyler Romero <tylerr@allenai.org>
2026-01-20 11:15:24 -0800

    .

--- src/olmo_core/model_ladder/analysis/__init__.py
+++ src/olmo_core/model_ladder/analysis/__init__.py
@@ -3,12 +3,10 @@ from .eval import (
     RolloutSplit,
     ScalingLawRollout,
     SplitEvaluation,
-    distance_weights,
     evaluate_rollout,
     evaluate_split,
     perplexity_ratio,
     relative_bpb_error,
-    rollout_distance,
 )
 from .plotting import plot_scaling_law_3d, plot_scaling_law_3d_comparison
 from .scaling_laws import (
@@ -19,25 +17,18 @@ from .scaling_laws import (
 )
 
 __all__ = [
-    # Rollout cross-validation
     "ScalingLawRollout",
     "RolloutSplit",
-    # Evaluation
     "evaluate_rollout",
     "evaluate_split",
     "RolloutEvaluation",
     "SplitEvaluation",
-    # Error metrics
     "perplexity_ratio",
     "relative_bpb_error",
-    "rollout_distance",
-    "distance_weights",
-    # Scaling laws
     "chinchilla_parametric_scaling_law",
     "ChinchillaParams",
     "ChinchillaParametricFit",
     "ChinchillaParametricBootstrappedFit",
-    # Plotting
     "plot_scaling_law_3d",
     "plot_scaling_law_3d_comparison",
 ]
