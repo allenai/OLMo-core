2324b812df28dd119c677b9585e63c386abbc2d4
epwalsh <petew@allenai.org>
2026-01-24 17:06:11 -0800

    fix

--- src/olmo_core/internal/experiment.py
+++ src/olmo_core/internal/experiment.py
@@ -428,7 +428,6 @@ def launch(config: ExperimentConfig):
     config.launch.launch(
         follow=True,
         slack_notifications=slack_enabled,
-        launch_timeout=5 * 60,
         #  step_timeout=30 * 60,  # hard timeout kills the job
         step_soft_timeout=10 * 60,  # soft timeout only sends slack warning
     )
