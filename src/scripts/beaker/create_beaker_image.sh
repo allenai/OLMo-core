4e00b8f1ff62395eb5bafd2f1c393441e6d7050d
Pete Walsh <petew@allenai.org>
2026-01-27 11:17:46 -0800

    Integrate QuACK kernels, minor improvements to Docker build (#569)
    - Adds the option to install QuACK in our Docker images.
    - Adds a new Beaker image with torch 2.9.1, CUDA 12.9.1, QuACK, and all
    other optional dependencies: `petew/olmo-core-tch291cu129-2026-01-24`.
    - Adds a CuTe-based RMSNorm from the
    [QuACK](https://github.com/Dao-AILab/quack) library. I didn't see any
    improvement at the 7B scale, but I'm curious to try with some larger
    models soon. And there's a bunch of other kernels in QuACK that could be
    useful.

--- src/scripts/beaker/create_beaker_image.sh
+++ src/scripts/beaker/create_beaker_image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -exuo pipefail
+set -euo pipefail
 
 source_image=$1
 beaker_image=$2
@@ -9,5 +9,5 @@ timestamp=$(date "+%Y%m%d%H%M%S")
 
 beaker_user=$(beaker account whoami --format=json | jq -r '.[0].name')
 beaker image create "${source_image}" --name "${beaker_image}-tmp" --workspace "${beaker_workspace}"
-beaker image rename "${beaker_user}/${beaker_image}" "${beaker_image}-${timestamp}" || true
+beaker image rename "${beaker_user}/${beaker_image}" "${beaker_image}-${timestamp}" 2>/dev/null || true
 beaker image rename "${beaker_user}/${beaker_image}-tmp" "${beaker_image}"
