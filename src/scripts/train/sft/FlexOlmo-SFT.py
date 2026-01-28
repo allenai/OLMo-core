416661287cd4b6c7a91977351041a624193fa16b
jacob-morrison <jacobm@allenai.org>
2026-01-28 15:57:30 -0800

    new seed?

--- src/scripts/train/sft/FlexOlmo-SFT.py
+++ src/scripts/train/sft/FlexOlmo-SFT.py
@@ -287,7 +287,7 @@ class SFTRouterConfig(Config):
     data_loader: NumpyDataLoaderConfig
     train_module: TransformerTrainModuleConfig
     trainer: TrainerConfig
-    init_seed: int = 53184
+    init_seed: int = 531842
 
     @classmethod
     def build(
@@ -520,7 +520,7 @@ class SFTRouterConfig(Config):
             model=model,
             dataset=None,
             data_loader=NumpyDataLoaderConfig(
-                global_batch_size=bs_config.global_batch_size_tokens, seed=34521, num_workers=4
+                global_batch_size=bs_config.global_batch_size_tokens, seed=3452100, num_workers=4
             ),
             # train_module=TransformerTrainModuleConfig(
             #     rank_microbatch_size=2 * 4096,
