203a35bc9f5e01d444967dfa89c44f5d2c9e6547
Nathan Lambert <nathanl@allenai.org>
2026-01-29 10:40:23 -0800

    convesion script

---
+++ src/scripts/train/sft/launch/convert_smoketest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+gantry run --cluster ai2/saturn-cirrascale --timeout -1 -y --budget ai2/oe-adapt --workspace ai2/olmo-instruct \
+    --install "curl -LsSf https://astral.sh/uv/install.sh | sh && /root/.local/bin/uv sync --all-extras" \
+    --weka=oe-adapt-default:/weka/oe-adapt-default \
+    --weka=oe-training-default:/weka/oe-training-default \
+    --priority urgent \
+    --gpus 1 \
+    -- /root/.local/bin/uv run python src/examples/huggingface/convert_checkpoint_to_hf.py \
+        -i /weka/oe-training-default/ai2-llm/checkpoints/nathanl/olmo-sft/HYBRID_SFT_SMOKETEST/step4 \
+        -o /weka/oe-adapt-default/nathanl/checkpoints/HYBRID_SFT_SMOKETEST/step4-hf \
+        --max-sequence-length 32768
