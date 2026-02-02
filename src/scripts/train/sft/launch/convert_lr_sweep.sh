fb165c2851a5ff7ff94d5fecc7eb78c3d30caa05
Nathan Lambert <nathanl@allenai.org>
2026-02-02 08:09:44 -0800

    up

--- src/scripts/train/sft/launch/convert_lr_sweep.sh
+++ src/scripts/train/sft/launch/convert_lr_sweep.sh
@@ -46,6 +46,20 @@
 #         --skip-validation
 
 # LR 4.5e-5
+# gantry run --cluster ai2/saturn-cirrascale --timeout -1 -y --budget ai2/oe-adapt --workspace ai2/olmo-instruct \
+#     --beaker-image tylerr/olmo-core-tch291cu128-2025-11-25 \
+#     --install "/opt/conda/bin/pip install -e '.[fla,transformers]'" \
+#     --weka=oe-adapt-default:/weka/oe-adapt-default \
+#     --weka=oe-training-default:/weka/oe-training-default \
+#     --priority urgent \
+#     --gpus 1 \
+#     -- /opt/conda/bin/python src/examples/huggingface/convert_checkpoint_to_hf_hybrid.py \
+#         -i /weka/oe-training-default/ai2-llm/checkpoints/nathanl/olmo-sft/TEST_HYBRIC_SFT_LARGER_LR4.5e-5_seed42/step46412 \
+#         -o /weka/oe-adapt-default/nathanl/checkpoints/TEST_HYBRIC_SFT_LARGER_LR4.5e-5_seed42/step46412-hf \
+#         --max-sequence-length 32768 \
+#         --skip-validation
+
+# LR 1e-5
 gantry run --cluster ai2/saturn-cirrascale --timeout -1 -y --budget ai2/oe-adapt --workspace ai2/olmo-instruct \
     --beaker-image tylerr/olmo-core-tch291cu128-2025-11-25 \
     --install "/opt/conda/bin/pip install -e '.[fla,transformers]'" \
@@ -54,7 +68,7 @@ gantry run --cluster ai2/saturn-cirrascale --timeout -1 -y --budget ai2/oe-adapt
     --priority urgent \
     --gpus 1 \
     -- /opt/conda/bin/python src/examples/huggingface/convert_checkpoint_to_hf_hybrid.py \
-        -i /weka/oe-training-default/ai2-llm/checkpoints/nathanl/olmo-sft/TEST_HYBRIC_SFT_LARGER_LR4.5e-5_seed42/step46412 \
-        -o /weka/oe-adapt-default/nathanl/checkpoints/TEST_HYBRIC_SFT_LARGER_LR4.5e-5_seed42/step46412-hf \
+        -i /weka/oe-training-default/ai2-llm/checkpoints/nathanl/olmo-sft/TEST_HYBRIC_SFT_LARGER_LR41e-5/step46412 \
+        -o /weka/oe-adapt-default/nathanl/checkpoints/TEST_HYBRIC_SFT_LARGER_LR1e-5/step46412-hf \
         --max-sequence-length 32768 \
         --skip-validation
