f2088a2e53bc4cada49362f51b47b4095190b4a4
Anej Svete <anej.svete@protonmail.com>
2026-02-02 04:25:39 +0100

    Changed mbz.

--- src/scripts/train/ladder/hybrid-mamba-ladder.py
+++ src/scripts/train/ladder/hybrid-mamba-ladder.py
@@ -97,7 +97,7 @@ class HybridMamba2TransformerModelConfigurator(TransformerModelConfigurator):
         if num_params <= 100e6:
             mbz = 2 * 4096
         elif num_params <= 190e6:
-            mbz = 4 * 4096
+            mbz = 2 * 4096
         elif num_params <= 370e6:
             mbz = 2 * 4096
         elif num_params <= 760e6:
