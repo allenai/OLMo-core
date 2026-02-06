44f01c3da3a2a26d0058171dfd91246890092a68
epwalsh <petew@allenai.org>
2026-02-06 10:53:22 -0800

    add failing test

--- src/test/data/composable/sampling_token_source_test.py
+++ src/test/data/composable/sampling_token_source_test.py
@@ -19,27 +19,59 @@ def test_sampling_token_source(tmp_path: Path):
 
 
 def test_sampling_token_source_with_mixing_source(tmp_path: Path):
-    source = SamplingTokenSource(
-        MixingTokenSource(
-            MixingTokenSource.Spec(
-                source=InMemoryTokenSource(list(range(12)), work_dir=tmp_path),
-                ratio=0.25,
-            ),
-            MixingTokenSource.Spec(
-                source=InMemoryTokenSource(list(range(12, 24)), work_dir=tmp_path),
-                ratio=0.75,
-            ),
-            work_dir=tmp_path,
-            num_tokens=16,
-            seed=None,
+    # Down-sampled mixing source.
+    mixing_source = MixingTokenSource(
+        MixingTokenSource.Spec(
+            source=InMemoryTokenSource(list(range(12)), work_dir=tmp_path),
+            ratio=0.25,
         ),
+        MixingTokenSource.Spec(
+            source=InMemoryTokenSource(list(range(12, 24)), work_dir=tmp_path),
+            ratio=0.75,
+        ),
+        work_dir=tmp_path,
+        num_tokens=16,
+        seed=None,
+    )
+
+    # Then down-sample again.
+    source = SamplingTokenSource(
+        mixing_source,
         max_tokens=8,
         work_dir=tmp_path,
         seed=None,
     )
-    # Should unwind the mix's sources and maintain the sampling ratios.
     assert source.num_tokens == 8
     assert list(source[:]["input_ids"]) == [0, 1, 12, 13, 14, 15, 16, 17]
+
+    # Or up-sample.
+    source = SamplingTokenSource(
+        mixing_source,
+        max_tokens=32,
+        work_dir=tmp_path,
+        seed=None,
+    )
+    assert source.num_tokens == 32
+    assert list(source[:]["input_ids"]) == list(range(4)) * 2 + list(range(12, 24)) * 2
+
+
+def test_sampling_token_source_with_sampling_token_source(tmp_path):
+    # Create an up-sampled source, 8->12 tokens.
+    up_sampled_source = InMemoryTokenSource(list(range(8)), work_dir=tmp_path).resize(
+        1.5, seed=None
+    )
+    assert len(up_sampled_source) == 12
+    assert list(up_sampled_source[:]["input_ids"]) == list(range(8)) + list(range(4))
+
+    # Down-sample from the up-sampled source.
+    source = SamplingTokenSource(
+        up_sampled_source,
+        max_tokens=8,
+        work_dir=tmp_path,
+        seed=None,
+    )
+    assert source.num_tokens == 8
+    assert list(source[:]["input_ids"]) == (list(range(6)) + list(range(2)))
 
 
 def test_sampling_token_source_with_repetition(tmp_path: Path):
