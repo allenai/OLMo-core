145e8ca6d9957beed9b071b6b7187f3d0348438c
Nathan Lambert <nathanl@allenai.org>
2026-02-11 18:07:47 -0800

    up

--- src/scripts/train/sft/launch/convert_lr_sweep.sh
+++ src/scripts/train/sft/launch/convert_lr_sweep.sh
@@ -75,18 +75,18 @@
 
 # ablation models
 # LR 5e-5
-gantry run --cluster ai2/saturn-cirrascale --timeout -1 -y --budget ai2/oe-adapt --workspace ai2/olmo-instruct \
-    --beaker-image tylerr/olmo-core-tch291cu128-2025-11-25 \
-    --install "/opt/conda/bin/pip install -e '.[fla,transformers]'" \
-    --weka=oe-adapt-default:/weka/oe-adapt-default \
-    --weka=oe-training-default:/weka/oe-training-default \
-    --priority urgent \
-    --gpus 1 \
-    -- /opt/conda/bin/python src/examples/huggingface/convert_checkpoint_to_hf_hybrid.py \
-        -i /weka/oe-training-default/ai2-llm/checkpoints/nathanl/olmo-sft/ABLATE_HYBRID_THINK_SFT_0210_5e-5/step43110 \
-        -o /weka/oe-adapt-default/nathanl/checkpoints/ABLATE_HYBRID_THINK_SFT_0210_5e-5/step43110-hf \
-        --max-sequence-length 32768 \
-        --skip-validation
+# gantry run --cluster ai2/saturn-cirrascale --timeout -1 -y --budget ai2/oe-adapt --workspace ai2/olmo-instruct \
+#     --beaker-image tylerr/olmo-core-tch291cu128-2025-11-25 \
+#     --install "/opt/conda/bin/pip install -e '.[fla,transformers]'" \
+#     --weka=oe-adapt-default:/weka/oe-adapt-default \
+#     --weka=oe-training-default:/weka/oe-training-default \
+#     --priority urgent \
+#     --gpus 1 \
+#     -- /opt/conda/bin/python src/examples/huggingface/convert_checkpoint_to_hf_hybrid.py \
+#         -i /weka/oe-training-default/ai2-llm/checkpoints/nathanl/olmo-sft/ABLATE_HYBRID_THINK_SFT_0210_5e-5/step43110 \
+#         -o /weka/oe-adapt-default/nathanl/checkpoints/ABLATE_HYBRID_THINK_SFT_0210_5e-5/step43110-hf \
+#         --max-sequence-length 32768 \
+#         --skip-validation
 
 # LR 2.5e-5
 gantry run --cluster ai2/saturn-cirrascale --timeout -1 -y --budget ai2/oe-adapt --workspace ai2/olmo-instruct \
