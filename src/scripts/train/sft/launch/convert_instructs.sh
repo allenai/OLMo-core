0914bbe477866d2cbad0505e991fde41ad7a8b69
Nathan Lambert <nathanl@allenai.org>
2026-02-18 17:55:30 -0800

    conversion scripts

--- src/scripts/train/sft/launch/convert_instructs.sh
+++ src/scripts/train/sft/launch/convert_instructs.sh
@@ -6,11 +6,10 @@ STEP=step3256
 TOKENIZER=allenai/olmo-3-tokenizer-instruct-dev
 BEAKER_IMAGE=tylerr/olmo-core-tch291cu128-2025-11-25
 
-# LRS=(1e-4 9e-5 8e-5 6e-5 5e-5 3e-5 2.5e-5 1.5e-5)
-LRS=(8e-5 1e-4 6e-5)
+LRS=(1e-4 9e-5 8e-5 6e-5 5e-5 2.5e-5)
 
 for LR in "${LRS[@]}"; do
-    RUN_NAME="HYBRID_INSTRUCT_SFT_0217_${LR}"
+    RUN_NAME="HYBRID_INSTRUCT_SFT_0218_${LR}"
     INPUT="/weka/oe-training-default/ai2-llm/checkpoints/nathanl/olmo-sft/${RUN_NAME}/${STEP}"
     OUTPUT="/weka/oe-adapt-default/nathanl/checkpoints/${RUN_NAME}/${STEP}-hf"
 
