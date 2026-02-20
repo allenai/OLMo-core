0ae52971095ee806c4eaf9ca949472464e7aba3a
Bailey Kuehl <67339479+baileykuehl@users.noreply.github.com>
2026-02-19 20:57:03 -0800

    fix to actual pre sa tokens

--- src/scripts/train/assaisonnees_ablations/olmo3_1b_5xc_assaisonnees_sa-pre.py
+++ src/scripts/train/assaisonnees_ablations/olmo3_1b_5xc_assaisonnees_sa-pre.py
@@ -40,7 +40,7 @@ DATASET_CONFIG = SourceMixtureList(
             source_name="pre_sa",
             target_ratio=1.0,
             paths=[
-                f"s3://ai2-llm/preprocessed/cc_all_dressed/all_dressed_v5/ablation_pools_decon/suffix_arrays/full_pool_resharded/pre_sa_tokens/*.npy"
+                f"s3://ai2-llm/preprocessed/cc_all_dressed/all_dressed_v5/ablation_pools_decon/suffix_arrays/full_pool_resharded/pre_sa_tokens_actually/*.npy"
             ],
         )
     ]
