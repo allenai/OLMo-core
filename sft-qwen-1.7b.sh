16fd33263ecff5a11b527cc1f98ad83b48882ffc
Jacob Morrison <jacob1morrison@gmail.com>
2026-04-02 16:03:08 -0700

    test

--- sft-qwen-1.7b.sh
+++ sft-qwen-1.7b.sh
@@ -1,6 +1,6 @@
 RUN_NAME=qwen3-1.7b-sft-10k
 BASE_CKPT=/weka/oe-adapt-default/jacobm/repos/cse-579/checkpoints/Qwen3-1.7B-Base-oc/model_and_optim
-CLUSTER=ai2/saturn
+CLUSTER=ai2/jupiter
 DATASET_PATH=/weka/oe-adapt-default/jacobm/repos/cse-579/datasets/Dolci-Think-SFT-32B-qwen3-olmo-thinker-10k
 
 uv run python src/scripts/train/sft/Qwen3-1.7B-SFT.py launch \
