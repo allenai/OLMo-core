e0eb1c693c5869f350a41ef839140cd078d0b5f9
Jacob Morrison <jacob1morrison@gmail.com>
2026-04-03 10:45:46 -0700

    push

--- convert_to_hf.sh
+++ convert_to_hf.sh
@@ -1,3 +1,5 @@
+MODEL_NAME="/weka/oe-training-default/ai2-llm/checkpoints/jacobm/olmo-sft/qwen3-4b-sft-100k/step1476"
+HF_MODEL_ID="Qwen/Qwen3-4B-Base"
 uv run gantry run --cluster ai2/saturn-cirrascale --timeout -1 -y --budget ai2/oe-adapt --workspace ai2/flex2 \
         --install "curl -LsSf https://astral.sh/uv/install.sh | sh && /root/.local/bin/uv sync --all-extras" \
         --weka=oe-adapt-default:/weka/oe-adapt-default \
@@ -5,7 +7,7 @@ uv run gantry run --cluster ai2/saturn-cirrascale --timeout -1 -y --budget ai2/o
         --priority urgent \
         --gpus 1 \
         -- /root/.local/bin/uv run python src/scripts/train/sft/convert_qwen3_to_hf.py \
-            -i "/weka/oe-training-default/ai2-llm/checkpoints/jacobm/olmo-sft/qwen3-1.7b-sft-10k/step1476" \
-            -o /weka/oe-training-default/ai2-llm/checkpoints/jacobm/olmo-sft/qwen3-1.7b-sft-10k/step1476-hf \
-            --hf-model-id Qwen/Qwen3-1.7B-Base \
+            -i $MODEL_NAME \
+            -o $MODEL_NAME-hf \
+            --hf-model-id $HF_MODEL_ID \
             --max-sequence-length 32768
