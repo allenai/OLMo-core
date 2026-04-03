6464b3718380f0e4d4138b4577aadca53b4f6797
Jacob Morrison <jacob1morrison@gmail.com>
2026-04-02 19:54:09 -0700

    push

--- sft-qwen-1.7b.sh
+++ sft-qwen-1.7b.sh
@@ -1,7 +1,7 @@
 RUN_NAME=qwen3-1.7b-sft-10k
 BASE_CKPT=/weka/oe-adapt-default/jacobm/repos/cse-579/checkpoints/Qwen3-1.7B-Base-oc/model_and_optim
 CLUSTER=ai2/jupiter
-DATASET_PATH=/weka/oe-adapt-default/jacobm/repos/cse-579/datasets/Dolci-Think-SFT-32B-qwen3-olmo-thinker-10k
+DATASET_PATH=/weka/oe-adapt-default/jacobm/repos/cse-579/datasets/Dolci-Think-SFT-32B-qwen3-olmo-thinker-100k
 
 uv run python src/scripts/train/sft/Qwen3-1.7B-SFT.py launch \
     ${RUN_NAME} \
@@ -10,6 +10,6 @@ uv run python src/scripts/train/sft/Qwen3-1.7B-SFT.py launch \
     --dataset_path=${DATASET_PATH} \
     --budget=ai2/oe-adapt \
     --workspace=ai2/olmo-instruct \
-    --num_nodes=1 \
+    --num_nodes=2 \
     --launch.priority=urgent \
     --launch.preemptible=true
