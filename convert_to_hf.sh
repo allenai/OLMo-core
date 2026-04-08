5aa42f11c907d9bcc913cd5687f91fbb720d1d9b
Jacob Morrison <jacob1morrison@gmail.com>
2026-04-08 10:14:05 -0700

    push

--- convert_to_hf.sh
+++ convert_to_hf.sh
@@ -1,5 +1,6 @@
-MODEL_NAME="/weka/oe-training-default/ai2-llm/checkpoints/jacobm/olmo-sft/qwen3-4b-sft-100k/step1476"
+# MODEL_NAME="/weka/oe-training-default/ai2-llm/checkpoints/jacobm/olmo-sft/qwen3-4b-sft-100k/step1476"
 HF_MODEL_ID="Qwen/Qwen3-4B-Base"
+MODEL_NAME="/weka/oe-training-default/ai2-llm/checkpoints/jacobm/olmo-sft/qwen3-4b-sft-full/step44438"
 uv run gantry run --cluster ai2/saturn-cirrascale --timeout -1 -y --budget ai2/oe-adapt --workspace ai2/flex2 \
         --install "curl -LsSf https://astral.sh/uv/install.sh | sh && /root/.local/bin/uv sync --all-extras" \
         --weka=oe-adapt-default:/weka/oe-adapt-default \
