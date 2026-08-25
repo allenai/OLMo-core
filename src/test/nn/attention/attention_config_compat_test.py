c9dca5625ea306f89d63eb5a7fa59a2d7020e630
Akshita Bhagia <akshita23bhagia@gmail.com>
2026-08-24 19:59:44 -0700

    Fix scalable softmax attention config dispatch

--- src/test/nn/attention/attention_config_compat_test.py
+++ src/test/nn/attention/attention_config_compat_test.py
@@ -1,3 +1,6 @@
+import pytest
+
+from olmo_core.exceptions import OLMoConfigurationError
 from olmo_core.nn.attention import AttentionConfig, AttentionType
 
 
@@ -14,3 +17,22 @@ def test_legacy_d_attn_sets_head_dim_and_does_not_reach_module():
     attention = config.build(d_model=2048, layer_idx=0, n_layers=1)
     assert attention.head_dim == 128
     assert attention.w_qkv.out_features == 6144
+
+
+@pytest.mark.parametrize("attention_type", [AttentionType.fused_v2, AttentionType.normalized])
+def test_disabled_scalable_softmax_does_not_reach_unsupported_modules(attention_type):
+    config = AttentionConfig(name=attention_type, n_heads=8, scalable_softmax=False)
+
+    config.build(d_model=64, layer_idx=0, n_layers=1)
+
+
+@pytest.mark.parametrize(
+    "attention_type", [AttentionType.fused, AttentionType.fused_v2, AttentionType.normalized]
+)
+def test_enabled_scalable_softmax_rejects_unsupported_modules(attention_type):
+    config = AttentionConfig(name=attention_type, n_heads=8, scalable_softmax=True)
+
+    with pytest.raises(
+        OLMoConfigurationError, match="scalable_softmax is only supported by default attention"
+    ):
+        config.build(d_model=64, layer_idx=0, n_layers=1)
